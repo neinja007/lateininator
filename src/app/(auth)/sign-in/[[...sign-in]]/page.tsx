f9d4047b5de2fbee8c1af055baa55f843e25b673
neinja007 <anton.siligan@gmail.com>
2024-06-16 13:06:32 +0200

    made simpler

--- src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import Button from '@/components/ui/Button';
 import { Connection, Field, FieldError, Icon, Input, Label } from '@clerk/elements/common';
 import { Action, Root, SafeIdentifier, Step, Strategy, SupportedStrategy } from '@clerk/elements/sign-in';
 import Link from 'next/link';
@@ -14,35 +13,28 @@ export default function SignInPage() {
 				<p>
 					Noch kein Konto? <Link href={'/sign-up'}>Erstellen Sie hier eines.</Link>
 				</p>
-				<Connection name='google' asChild className='mb-4'>
-					<Button
-						className='w-full'
-						label={
-							<span className='inline-flex w-full justify-center'>
-								<Icon className='mr-2' /> Mit Google anmelden
-							</span>
-						}
-					/>
+				<Connection name='google' className='wide-button'>
+					<span className='inline-flex justify-center'>
+						<Icon className='mr-2' /> Mit Google anmelden
+					</span>
 				</Connection>
 				<br />
-				<Field name='identifier' className='mb-4'>
+				<Field name='identifier'>
 					<Label>E-Mail Adresse</Label>
 					<Input className='w-full' placeholder='max.mustermann@duck.com' />
 					<FieldError className='text-red-500'>
-						{({ code }) => (fieldErrorCodeMapper.email.hasOwnProperty(code) ? fieldErrorCodeMapper.email[code] : code)}
+						{({ code }) => (fieldErrorCodeMapper.email[code] ? fieldErrorCodeMapper.email[code] : code)}
 					</FieldError>
 				</Field>
 
-				<Action submit asChild>
-					<Button label='Weiter' />
-				</Action>
+				<Action submit>Weiter</Action>
 			</Step>
 
 			<Step name='verifications'>
 				<Strategy name='password'>
 					<h1>Geben Sie Ihr Passwort ein</h1>
 
-					<Field name='password' className='mb-4'>
+					<Field name='password'>
 						<Label>Passwort</Label>
 						<Action navigate='forgot-password' className='float-end link'>
 							<a>Passwort vergessen?</a>
@@ -50,18 +42,14 @@ export default function SignInPage() {
 						<br />
 						<Input className='w-full' placeholder='●●●●●●●●●●●●' />
 						<FieldError className='text-red-500'>
-							{({ code }) =>
-								fieldErrorCodeMapper.password.hasOwnProperty(code) ? fieldErrorCodeMapper.password[code] : code
-							}
+							{({ code }) => (fieldErrorCodeMapper.password[code] ? fieldErrorCodeMapper.password[code] : code)}
 						</FieldError>
 					</Field>
 
-					<Action submit asChild>
-						<Button label='Anmelden' />
-					</Action>
+					<Action submit>Anmelden</Action>
 
-					<Action navigate='start' asChild>
-						<Button label='Abbrechen' className='float-end' />
+					<Action navigate='start' className='float-end'>
+						Abbrechen
 					</Action>
 				</Strategy>
 
@@ -75,20 +63,18 @@ export default function SignInPage() {
 						gesendet.
 					</p>
 
-					<Field name='code' className='mb-4'>
+					<Field name='code'>
 						<Label>Bestätigungscode</Label>
 						<Input className='w-full' placeholder='123456' />
 						<FieldError className='text-red-500'>
-							{({ code }) => (fieldErrorCodeMapper.code.hasOwnProperty(code) ? fieldErrorCodeMapper.code[code] : code)}
+							{({ code }) => (fieldErrorCodeMapper.code[code] ? fieldErrorCodeMapper.code[code] : code)}
 						</FieldError>
 					</Field>
 
-					<Action submit asChild>
-						<Button label='Weiter' />
-					</Action>
+					<Action submit>Weiter</Action>
 
-					<Action navigate='start' asChild>
-						<Button label='Abbrechen' className='float-end' />
+					<Action navigate='start' className='float-end'>
+						Abbrechen
 					</Action>
 				</Strategy>
 			</Step>
@@ -96,44 +82,36 @@ export default function SignInPage() {
 			<Step name='forgot-password'>
 				<h1>Passwort vergessen?</h1>
 
-				<SupportedStrategy name='reset_password_email_code' asChild>
-					<Button label='Passwort zurücksetzen' className='mb-4' />
-				</SupportedStrategy>
+				<SupportedStrategy name='reset_password_email_code'>Passwort zurücksetzen</SupportedStrategy>
 				<br />
-				<Action navigate='previous' asChild>
-					<Button label='Zurück' className='float-end' />
+				<Action navigate='previous' className='float-end'>
+					Zurück
 				</Action>
 			</Step>
 
 			<Step name='reset-password'>
 				<h1>Setzen Sie Ihr Passwort zurück</h1>
 
-				<Field name='password' className='mb-4'>
+				<Field name='password'>
 					<Label>Neues Passwort</Label>
 					<Input className='w-full' />
 					<FieldError className='text-red-500'>
-						{({ code }) =>
-							fieldErrorCodeMapper.password.hasOwnProperty(code) ? fieldErrorCodeMapper.password[code] : code
-						}
+						{({ code }) => (fieldErrorCodeMapper.password[code] ? fieldErrorCodeMapper.password[code] : code)}
 					</FieldError>
 				</Field>
 
-				<Field name='confirmPassword' className='mb-4'>
+				<Field name='confirmPassword'>
 					<Label>Passwort bestätigen</Label>
 					<Input className='w-full' />
 					<FieldError className='text-red-500'>
-						{({ code }) =>
-							fieldErrorCodeMapper.password.hasOwnProperty(code) ? fieldErrorCodeMapper.password[code] : code
-						}
+						{({ code }) => (fieldErrorCodeMapper.password[code] ? fieldErrorCodeMapper.password[code] : code)}
 					</FieldError>
 				</Field>
 
-				<Action submit asChild>
-					<Button label={'Passwort zurücksetzen'} />
-				</Action>
+				<Action submit>Passwort zurücksetzen</Action>
 
-				<Action navigate='start' asChild>
-					<Button label='Abbrechen' className='float-end' />
+				<Action navigate='start' className='float-end'>
+					Abbrechen
 				</Action>
 			</Step>
 		</Root>
--- src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,8 +1,7 @@
 'use client';
 
-import Button from '@/components/ui/Button';
 import { Connection, Field, FieldError, Icon, Input, Label } from '@clerk/elements/common';
-import { Action, Captcha, Root, Step, Strategy } from '@clerk/elements/sign-up';
+import { Action, Captcha, Root, Step } from '@clerk/elements/sign-up';
 import Link from 'next/link';
 import { fieldErrorCodeMapper } from '@/data/authMapper';
 
@@ -15,84 +14,69 @@ export default function SignUpPage() {
 					Sie haben schon ein Konto? <Link href={'/sign-in'}>Melden Sie sich hier an.</Link>
 				</p>
 
-				<Connection name='google' asChild className='mb-4'>
-					<Button
-						className='w-full'
-						label={
-							<span className='inline-flex w-full justify-center'>
-								<Icon className='mr-2' /> Konto mit Google erstellen
-							</span>
-						}
-					/>
+				<Connection name='google' className='wide-button'>
+					<span className='inline-flex justify-center'>
+						<Icon className='mr-2' /> Konto mit Google erstellen
+					</span>
 				</Connection>
 				<br />
-				<Field name='emailAddress' className='mb-4'>
+				<Field name='emailAddress'>
 					<Label>E-Mail Adresse</Label>
 					<Input className='w-full' required placeholder='max.mustermann@duck.com' />
 					<FieldError className='text-red-500'>
-						{({ code }) => (fieldErrorCodeMapper.email.hasOwnProperty(code) ? fieldErrorCodeMapper.email[code] : code)}
+						{({ code }) => (fieldErrorCodeMapper.email[code] ? fieldErrorCodeMapper.email[code] : code)}
 					</FieldError>
 				</Field>
 
-				<Field name='password' className='mb-4'>
+				<Field name='password'>
 					<Label>Password</Label>
 					<Input className='w-full' placeholder='●●●●●●●●●●●●' />
 					<FieldError className='text-red-500'>
-						{({ code }) =>
-							fieldErrorCodeMapper.password.hasOwnProperty(code) ? fieldErrorCodeMapper.password[code] : code
-						}
+						{({ code }) => (fieldErrorCodeMapper.password[code] ? fieldErrorCodeMapper.password[code] : code)}
 					</FieldError>
 				</Field>
 
 				<Captcha />
 
-				<Action submit asChild>
-					<Button label={'Weiter'} />
-				</Action>
+				<Action submit>Weiter</Action>
 			</Step>
 
 			<Step name='continue'>
 				<h1>Geben Sie Bitte Ihren Namen an</h1>
 
-				<Field name='firstName' className='mb-4'>
+				<Field name='firstName'>
 					<Label>Vorname</Label>
 					<Input className='w-full' placeholder='Max' />
 					<FieldError className='text-red-500'>
-						{({ code }) => (fieldErrorCodeMapper.name.hasOwnProperty(code) ? fieldErrorCodeMapper.name[code] : code)}
+						{({ code }) => (fieldErrorCodeMapper.name[code] ? fieldErrorCodeMapper.name[code] : code)}
 					</FieldError>
 				</Field>
 
-				<Field name='lastName' className='mb-4'>
+				<Field name='lastName'>
 					<Label>Nachname</Label>
 					<Input className='w-full' placeholder='Mustermann' />
 					<FieldError className='text-red-500'>
-						{({ code }) => (fieldErrorCodeMapper.name.hasOwnProperty(code) ? fieldErrorCodeMapper.name[code] : code)}
+						{({ code }) => (fieldErrorCodeMapper.name[code] ? fieldErrorCodeMapper.name[code] : code)}
 					</FieldError>
 				</Field>
 
-				<Action submit asChild>
-					<Button label='Weiter' />
-				</Action>
+				<Action submit>Weiter</Action>
 			</Step>
 
 			<Step name='verifications'>
 				<h1>Überprüfen Sie Ihre E-Mails</h1>
 
-				<Field name='code' className='mb-4'>
+				<Field name='code'>
 					<Label>Bestätigungscode</Label>
 					<Input className='w-full' placeholder='123456' />
 					<FieldError className='text-red-500'>
-						{({ code }) => (fieldErrorCodeMapper.code.hasOwnProperty(code) ? fieldErrorCodeMapper.code[code] : code)}
+						{({ code }) => (fieldErrorCodeMapper.code[code] ? fieldErrorCodeMapper.code[code] : code)}
 					</FieldError>
 				</Field>
 
-				<Action submit asChild>
-					<Button label='Konto Erstellen' />
-				</Action>
+				<Action submit>Konto Erstellen</Action>
 
-				<Action navigate='start' asChild>
-					<Button label='Abbrechen' className='float-end' />
-				</Action>
+				<Action navigate='start'>Abbrechen</Action>
 			</Step>
 		</Root>
 	);
