43225798b8eab486d7a7f4cfe88235f4efc6be7a
neinja007 <anton.siligan@gmail.com>
2024-06-16 13:06:57 +0200

    restyle

--- src/components/navbar/NavbarDropdownLink.tsx
+++ src/components/navbar/NavbarDropdownLink.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import Link from 'next/link';
-import React from 'react';
 
 type NavbarDropdownLinkProps = {
 	label: string;
@@ -12,7 +11,7 @@ type NavbarDropdownLinkProps = {
 function NavbarDropdownLink({ label, href, active }: NavbarDropdownLinkProps) {
 	return (
 		<Link
-			className={`p-2 px-4 my-auto text-inherit font-semibold last:border-b-inherit border-b rounded-none block first:rounded-t-md last:rounded-b-md ${
+			className={`p-2 px-4 my-auto text-inherit font-semibold border-b last:border-b-none rounded-none block first:rounded-t-md last:rounded-b-md ${
 				active ? 'bg-gray-200' : 'bg-white hover:bg-gray-100'
 			}`}
 			href={href}
