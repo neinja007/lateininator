59a55080743e839ac46355f286d0d03c090f643c
neinja007 <anton.siligan@gmail.com>
2024-06-16 07:33:27 +0200

    redirect to /dashboard

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,12 @@
 import { clerkMiddleware } from '@clerk/nextjs/server';
+import { NextRequest, NextResponse } from 'next/server';
 
-export default clerkMiddleware();
+export default function middleware(request: NextRequest) {
+	if (request.nextUrl.pathname === '/') {
+		return NextResponse.redirect(new URL('/dashboard', request.url));
+	}
+	clerkMiddleware();
+}
 
 export const config = {
 	matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)']
