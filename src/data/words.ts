21388cf6b92d8072063c02eda73208570df0e985
neinja007 <anton.siligan@gmail.com>
2024-06-19 06:15:33 +0200

    template test data

--- src/data/words.ts
+++ src/data/words.ts
@@ -1,5 +1,16 @@
 import { Words } from '@/data/types';
 
 export const words: Words = [
+	{
+		id: 1,
+		type: 'noun',
+		declension: 'a',
+		gender: 'f',
+		genitive: 'scholae',
+		word: 'schola',
+		translation: ['Schule', 'Uni']
+	},
+	{ id: 1, type: 'noun', declension: 'a', gender: 'f', genitive: 'scholae', word: 'schola', translation: ['Schule'] },
+	{ id: 1, type: 'noun', declension: 'a', gender: 'f', genitive: 'scholae', word: 'schola', translation: ['Schule'] },
 	{ id: 1, type: 'noun', declension: 'a', gender: 'f', genitive: 'scholae', word: 'schola', translation: ['Schule'] }
 ];
