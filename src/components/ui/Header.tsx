397288ded6366c3bcefa67bd7c7a58757d46be8c
neinja007 <anton.siligan@gmail.com>
2024-06-22 06:18:34 +0200

    header adjust

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,20 +1,11 @@
 type HeaderProps = {
 	className?: React.CSSProperties;
-} & (
-	| {
-			children?: never;
-			label: string;
-	  }
-	| {
-			children: React.ReactNode;
-			label?: never;
-	  }
-) &
-	React.ComponentProps<'h1'>;
+	children: React.ReactNode;
+} & React.ComponentProps<'h1'>;
 
 function Header({ children, className, ...props }: HeaderProps) {
 	return (
-		<h1 {...props} className={`${className} font-bold text-2xl text-theme`}>
+		<h1 {...props} className={`${className} font-bold text-3xl text-theme`}>
 			{children}
 		</h1>
 	);
