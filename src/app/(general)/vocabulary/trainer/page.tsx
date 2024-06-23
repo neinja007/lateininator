1c5d9e86f83851d62d0b24a7c51f517fb6796ed3
neinja007 <anton.siligan@gmail.com>
2024-06-23 13:50:58 +0200

    trainer dev

--- src/app/(general)/vocabulary/trainer/page.tsx
+++ src/app/(general)/vocabulary/trainer/page.tsx
@@ -1,7 +1,43 @@
+'use client';
+
+import SelectButton from '@/components/SelectButton';
+import H1 from '@/components/navbar/ui/H1';
+import { lists } from '@/data/lists';
+import { Words, List } from '@/data/types';
+import { useState } from 'react';
+
 function Page() {
+	const [stage, setStage] = useState<'settings' | 'testing' | 'results'>('settings');
+	const [words, setWords] = useState<Words>([]);
+	const [selectedLists, setSelectedLists] = useState<Array<List>>([]);
+
+	function toggleList(list: List) {
+		if (selectedLists.includes(list)) {
+			setSelectedLists((prevSelectedLists) => prevSelectedLists.filter((prevList) => prevList.name !== list.name));
+		} else {
+			setSelectedLists((prevSelectedLists) => [...prevSelectedLists, list]);
+		}
+	}
+
 	return (
 		<div>
-			<h1>Page</h1>
+			<H1>Vokabeltrainer</H1>
+			{stage === 'settings' && (
+				<>
+					<p>Wähle aus, welche Wörter du lernen möchtest:</p>
+					<div className='flex space-x-3 justify-center'>
+						{lists.map((list, i) => (
+							<SelectButton
+								key={i}
+								list={list}
+								active={selectedLists.includes(list)}
+								handleClick={() => toggleList(list)}
+							/>
+						))}
+					</div>
+				</>
+			)}
+			{stage === 'testing' && <div></div>}
 		</div>
 	);
 }
