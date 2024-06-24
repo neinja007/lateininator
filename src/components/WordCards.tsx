a32cbef68cc9394add9ec1002d9c12673b60804a
neinja007 <anton.siligan@gmail.com>
2024-06-24 06:05:21 +0200

    adjustments to w change

--- src/components/WordCards.tsx
+++ src/components/WordCards.tsx
@@ -5,7 +5,7 @@ type WordCardsProps = { results: Words; query: string };
 
 function WordCards({ results, query }: WordCardsProps) {
 	return (
-		<div className='inline-grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3 w-full'>
+		<div className='inline-grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 w-full'>
 			{results.map((word, i) => (
 				<WordCard key={i} word={word} query={query} />
 			))}
