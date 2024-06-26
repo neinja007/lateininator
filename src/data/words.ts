b50ca84844e7a295402bf8b98a68fa9f8550a76d
neinja007 <anton.siligan@gmail.com>
2024-06-26 06:44:13 +0200

    testing purposes

--- src/data/words.ts
+++ src/data/words.ts
@@ -10,306 +10,306 @@ export const words: Words = [
 		genitive: 'scholae',
 		gender: 'f',
 		declension: 'a'
-	},
-	{
-		id: 2,
-		word: 'et',
-		type: 'other',
-		translation: ['und']
-	},
-	{
-		id: 3,
-		word: 'amicus',
-		type: 'noun',
-		translation: ['freund'],
-
-		genitive: 'amici',
-		gender: 'm',
-		declension: 'o'
-	},
-	{
-		id: 4,
-		word: 'sed',
-		type: 'other',
-		translation: ['aber', 'sondern']
-	},
-	{
-		id: 5,
-		word: 'discipulus',
-		type: 'noun',
-		translation: ['schüler'],
-
-		genitive: 'discipuli',
-		gender: 'm',
-		declension: 'o'
-	},
-	{
-		id: 6,
-		word: 'malus',
-		type: 'adjective',
-		translation: ['schlecht'],
-
-		declension: 'a_o',
-		forms: {
-			femininum: 'mala',
-			neutrum: 'malum'
-		}
-	},
-	{
-		id: 7,
-		word: 'laetus',
-		type: 'adjective',
-		translation: ['froh', 'fröhlich'],
-
-		declension: 'a_o',
-		forms: {
-			femininum: 'laeta',
-			neutrum: 'laetum'
-		}
-	},
-	{
-		id: 8,
-		word: 'saepe',
-		type: 'adverb',
-		translation: ['oft']
-	},
-	{
-		id: 9,
-		word: 'ridere',
-		type: 'verb',
-		translation: ['lachen'],
-
-		conjugation: 'e',
-		forms: {
-			present: 'rideo',
-			perfect: 'risi',
-			participle: 'risum'
-		}
-	},
-	{
-		id: 10,
-		word: 'cur?',
-		type: 'other',
-		translation: ['warum', 'warum?']
-	},
-	{
-		id: 11,
-		word: 'nunc',
-		type: 'adverb',
-		translation: ['nun', 'jetzt']
-	},
-	{
-		id: 12,
-		word: 'hic',
-		type: 'adverb',
-		translation: ['hier'],
-		info: 'Adverb'
-	},
-	{
-		id: 13,
-		word: 'non',
-		type: 'other',
-		translation: ['nicht']
-	},
-	{
-		id: 14,
-		word: 'pensum',
-		type: 'noun',
-		translation: ['aufgabe'],
-
-		genitive: 'pensi',
-		gender: 'n',
-		declension: 'o'
-	},
-	{
-		id: 15,
-		word: 'magnus',
-		type: 'adjective',
-		translation: ['groß'],
-
-		declension: 'a_o',
-		forms: {
-			femininum: 'magna',
-			neutrum: 'magnum'
-		}
-	},
-	{
-		id: 16,
-		word: 'itaque',
-		type: 'other',
-		translation: ['deshalb', 'daher']
-	},
-	{
-		id: 17,
-		word: 'gaudere',
-		type: 'verb',
-		translation: ['freuen', 'sich freuen'],
-
-		conjugation: 'e',
-		forms: {
-			present: 'gaudeo',
-			perfect: '-',
-			participle: '-'
-		}
-	},
-	{
-		id: 18,
-		word: 'diu',
-		type: 'adverb',
-		translation: ['lang', 'lange']
-	},
-	{
-		id: 19,
-		word: 'laborare',
-		type: 'verb',
-		translation: ['arbeiten'],
-
-		conjugation: 'a',
-		forms: {
-			present: 'laboro',
-			perfect: 'laboravi',
-			participle: 'laboratum'
-		}
-	},
-	{
-		id: 20,
-		word: 'libenter',
-		type: 'adverb',
-		translation: ['gern', 'gerne']
-	},
-	{
-		id: 21,
-		word: 'interrogare',
-		type: 'verb',
-		translation: ['fragen'],
-
-		conjugation: 'a',
-		forms: {
-			present: 'interrogo',
-			perfect: 'interrogavi',
-			participle: 'interrogatum'
-		}
-	},
-	{
-		id: 22,
-		word: 'respondere',
-		type: 'verb',
-		translation: ['antworten'],
-
-		conjugation: 'e',
-		forms: {
-			present: 'respondeo',
-			perfect: 'respondi',
-			participle: 'responsum'
-		}
-	},
-	{
-		id: 23,
-		word: 'sedere',
-		type: 'verb',
-		translation: ['sitzen'],
-
-		conjugation: 'e',
-		forms: {
-			present: 'sedeo',
-			perfect: 'sedi',
-			participle: 'sessum'
-		}
-	},
-	{
-		id: 24,
-		word: 'tum',
-		type: 'other',
-		translation: ['dann', 'damals'],
-		info: '= tunc'
-	},
-	{
-		id: 25,
-		word: 'clamare',
-		type: 'verb',
-		translation: ['rufen', 'schreien'],
-
-		conjugation: 'a',
-		forms: {
-			present: 'clamo',
-			perfect: 'clamavi',
-			participle: 'clamatum'
-		}
-	},
-	{
-		id: 26,
-		word: 'laudare',
-		type: 'verb',
-		translation: ['loben'],
-
-		conjugation: 'a',
-		forms: {
-			present: 'laudo',
-			perfect: 'laudavi',
-			participle: 'laudatum'
-		}
-	},
-	{
-		id: 27,
-		word: 'amica',
-		type: 'noun',
-		translation: ['freundin'],
-
-		genitive: 'amicae',
-		gender: 'f',
-		declension: 'a'
-	},
-	{
-		id: 28,
-		word: 'meus/a/um',
-		type: 'pronoun',
-		translation: ['mein']
-	},
-	{
-		id: 29,
-		word: 'esse',
-		type: 'irregular_verb',
-		translation: ['sein', 'zu sein'],
-		info: 'irreguläres Verb',
-		forms: {
-			present: 'sum',
-			perfect: 'fui',
-			participle: '-'
-		}
-	},
-	{
-		id: 30,
-		word: 'discipula',
-		type: 'noun',
-		translation: ['schülerin'],
-
-		genitive: 'discipulae',
-		gender: 'f',
-		declension: 'a'
-	},
-	{
-		id: 31,
-		word: 'bonus',
-		type: 'adjective',
-		translation: ['gut'],
-
-		declension: 'a_o',
-		forms: {
-			femininum: 'bona',
-			neutrum: 'bonum'
-		}
-	},
-	{
-		id: 32,
-		word: 'semper',
-		type: 'adverb',
-		translation: ['immer']
-	},
-	{
-		id: 33,
-		word: 'sunt',
-		type: 'other',
-		translation: ['sie sind'],
-		derivative: 29
 	}
+	// {
+	// 	id: 2,
+	// 	word: 'et',
+	// 	type: 'other',
+	// 	translation: ['und']
+	// },
+	// {
+	// 	id: 3,
+	// 	word: 'amicus',
+	// 	type: 'noun',
+	// 	translation: ['freund'],
+
+	// 	genitive: 'amici',
+	// 	gender: 'm',
+	// 	declension: 'o'
+	// },
+	// {
+	// 	id: 4,
+	// 	word: 'sed',
+	// 	type: 'other',
+	// 	translation: ['aber', 'sondern']
+	// },
+	// {
+	// 	id: 5,
+	// 	word: 'discipulus',
+	// 	type: 'noun',
+	// 	translation: ['schüler'],
+
+	// 	genitive: 'discipuli',
+	// 	gender: 'm',
+	// 	declension: 'o'
+	// },
+	// {
+	// 	id: 6,
+	// 	word: 'malus',
+	// 	type: 'adjective',
+	// 	translation: ['schlecht'],
+
+	// 	declension: 'a_o',
+	// 	forms: {
+	// 		femininum: 'mala',
+	// 		neutrum: 'malum'
+	// 	}
+	// },
+	// {
+	// 	id: 7,
+	// 	word: 'laetus',
+	// 	type: 'adjective',
+	// 	translation: ['froh', 'fröhlich'],
+
+	// 	declension: 'a_o',
+	// 	forms: {
+	// 		femininum: 'laeta',
+	// 		neutrum: 'laetum'
+	// 	}
+	// },
+	// {
+	// 	id: 8,
+	// 	word: 'saepe',
+	// 	type: 'adverb',
+	// 	translation: ['oft']
+	// },
+	// {
+	// 	id: 9,
+	// 	word: 'ridere',
+	// 	type: 'verb',
+	// 	translation: ['lachen'],
+
+	// 	conjugation: 'e',
+	// 	forms: {
+	// 		present: 'rideo',
+	// 		perfect: 'risi',
+	// 		participle: 'risum'
+	// 	}
+	// },
+	// {
+	// 	id: 10,
+	// 	word: 'cur?',
+	// 	type: 'other',
+	// 	translation: ['warum', 'warum?']
+	// },
+	// {
+	// 	id: 11,
+	// 	word: 'nunc',
+	// 	type: 'adverb',
+	// 	translation: ['nun', 'jetzt']
+	// },
+	// {
+	// 	id: 12,
+	// 	word: 'hic',
+	// 	type: 'adverb',
+	// 	translation: ['hier'],
+	// 	info: 'Adverb'
+	// },
+	// {
+	// 	id: 13,
+	// 	word: 'non',
+	// 	type: 'other',
+	// 	translation: ['nicht']
+	// },
+	// {
+	// 	id: 14,
+	// 	word: 'pensum',
+	// 	type: 'noun',
+	// 	translation: ['aufgabe'],
+
+	// 	genitive: 'pensi',
+	// 	gender: 'n',
+	// 	declension: 'o'
+	// },
+	// {
+	// 	id: 15,
+	// 	word: 'magnus',
+	// 	type: 'adjective',
+	// 	translation: ['groß'],
+
+	// 	declension: 'a_o',
+	// 	forms: {
+	// 		femininum: 'magna',
+	// 		neutrum: 'magnum'
+	// 	}
+	// },
+	// {
+	// 	id: 16,
+	// 	word: 'itaque',
+	// 	type: 'other',
+	// 	translation: ['deshalb', 'daher']
+	// },
+	// {
+	// 	id: 17,
+	// 	word: 'gaudere',
+	// 	type: 'verb',
+	// 	translation: ['freuen', 'sich freuen'],
+
+	// 	conjugation: 'e',
+	// 	forms: {
+	// 		present: 'gaudeo',
+	// 		perfect: '-',
+	// 		participle: '-'
+	// 	}
+	// },
+	// {
+	// 	id: 18,
+	// 	word: 'diu',
+	// 	type: 'adverb',
+	// 	translation: ['lang', 'lange']
+	// },
+	// {
+	// 	id: 19,
+	// 	word: 'laborare',
+	// 	type: 'verb',
+	// 	translation: ['arbeiten'],
+
+	// 	conjugation: 'a',
+	// 	forms: {
+	// 		present: 'laboro',
+	// 		perfect: 'laboravi',
+	// 		participle: 'laboratum'
+	// 	}
+	// },
+	// {
+	// 	id: 20,
+	// 	word: 'libenter',
+	// 	type: 'adverb',
+	// 	translation: ['gern', 'gerne']
+	// },
+	// {
+	// 	id: 21,
+	// 	word: 'interrogare',
+	// 	type: 'verb',
+	// 	translation: ['fragen'],
+
+	// 	conjugation: 'a',
+	// 	forms: {
+	// 		present: 'interrogo',
+	// 		perfect: 'interrogavi',
+	// 		participle: 'interrogatum'
+	// 	}
+	// },
+	// {
+	// 	id: 22,
+	// 	word: 'respondere',
+	// 	type: 'verb',
+	// 	translation: ['antworten'],
+
+	// 	conjugation: 'e',
+	// 	forms: {
+	// 		present: 'respondeo',
+	// 		perfect: 'respondi',
+	// 		participle: 'responsum'
+	// 	}
+	// },
+	// {
+	// 	id: 23,
+	// 	word: 'sedere',
+	// 	type: 'verb',
+	// 	translation: ['sitzen'],
+
+	// 	conjugation: 'e',
+	// 	forms: {
+	// 		present: 'sedeo',
+	// 		perfect: 'sedi',
+	// 		participle: 'sessum'
+	// 	}
+	// },
+	// {
+	// 	id: 24,
+	// 	word: 'tum',
+	// 	type: 'other',
+	// 	translation: ['dann', 'damals'],
+	// 	info: '= tunc'
+	// },
+	// {
+	// 	id: 25,
+	// 	word: 'clamare',
+	// 	type: 'verb',
+	// 	translation: ['rufen', 'schreien'],
+
+	// 	conjugation: 'a',
+	// 	forms: {
+	// 		present: 'clamo',
+	// 		perfect: 'clamavi',
+	// 		participle: 'clamatum'
+	// 	}
+	// },
+	// {
+	// 	id: 26,
+	// 	word: 'laudare',
+	// 	type: 'verb',
+	// 	translation: ['loben'],
+
+	// 	conjugation: 'a',
+	// 	forms: {
+	// 		present: 'laudo',
+	// 		perfect: 'laudavi',
+	// 		participle: 'laudatum'
+	// 	}
+	// },
+	// {
+	// 	id: 27,
+	// 	word: 'amica',
+	// 	type: 'noun',
+	// 	translation: ['freundin'],
+
+	// 	genitive: 'amicae',
+	// 	gender: 'f',
+	// 	declension: 'a'
+	// },
+	// {
+	// 	id: 28,
+	// 	word: 'meus/a/um',
+	// 	type: 'pronoun',
+	// 	translation: ['mein']
+	// },
+	// {
+	// 	id: 29,
+	// 	word: 'esse',
+	// 	type: 'irregular_verb',
+	// 	translation: ['sein', 'zu sein'],
+	// 	info: 'irreguläres Verb',
+	// 	forms: {
+	// 		present: 'sum',
+	// 		perfect: 'fui',
+	// 		participle: '-'
+	// 	}
+	// },
+	// {
+	// 	id: 30,
+	// 	word: 'discipula',
+	// 	type: 'noun',
+	// 	translation: ['schülerin'],
+
+	// 	genitive: 'discipulae',
+	// 	gender: 'f',
+	// 	declension: 'a'
+	// },
+	// {
+	// 	id: 31,
+	// 	word: 'bonus',
+	// 	type: 'adjective',
+	// 	translation: ['gut'],
+
+	// 	declension: 'a_o',
+	// 	forms: {
+	// 		femininum: 'bona',
+	// 		neutrum: 'bonum'
+	// 	}
+	// },
+	// {
+	// 	id: 32,
+	// 	word: 'semper',
+	// 	type: 'adverb',
+	// 	translation: ['immer']
+	// },
+	// {
+	// 	id: 33,
+	// 	word: 'sunt',
+	// 	type: 'other',
+	// 	translation: ['sie sind'],
+	// 	derivative: 29
+	// }
 ];
