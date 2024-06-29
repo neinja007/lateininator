d060099abe0e0d3e82a26cf8cd9a312c6040cc75
neinja007 <anton.siligan@gmail.com>
2024-06-29 09:53:20 +0200

    refactor: Generate sequential numbers for lists dynamically

--- src/data/lists.ts
+++ src/data/lists.ts
@@ -1,11 +1,20 @@
 import { List } from './types';
 
+function generateSequentialNumbers(start: number, end: number): number[] {
+	return Array.from({ length: end - start + 1 }, (_, i) => i + start);
+}
+
 export const lists: Array<List> = [
 	{
 		name: 'Lektion 1',
-		words: [
-			1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
-			32, 33
-		]
+		words: generateSequentialNumbers(1, 33)
+	},
+	{
+		name: 'Lektion 2',
+		words: generateSequentialNumbers(34, 64)
+	},
+	{
+		name: 'Lektion 3',
+		words: generateSequentialNumbers(65, 91)
 	}
 ];
