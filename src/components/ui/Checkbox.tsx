c86ef1f4bbf23eacc83a0a71aeec8367c4457246
neinja007 <anton.siligan@gmail.com>
2024-07-02 06:13:01 +0200

    styling

--- src/components/ui/Checkbox.tsx
+++ src/components/ui/Checkbox.tsx
@@ -1,20 +1,26 @@
 import { Dispatch, SetStateAction, useId } from 'react';
 
-type CheckboxProps = { checked: boolean; handleChange: Dispatch<SetStateAction<boolean>>; label: string };
+type CheckboxProps = {
+	checked: boolean;
+	disabled?: boolean;
+	handleChange: Dispatch<SetStateAction<boolean>>;
+	label: string;
+};
 
-function Checkbox({ checked, handleChange, label }: CheckboxProps) {
+function Checkbox({ checked, disabled, handleChange, label }: CheckboxProps) {
 	const id = useId();
 
 	return (
 		<div>
 			<input
+				disabled={disabled}
 				id={id}
 				type='checkbox'
 				checked={checked}
 				onChange={(e) => handleChange(e.target.checked)}
 				className='mr-1'
 			/>
-			<label htmlFor={id} className={checked ? 'text-black' : 'text-gray-500'}>
+			<label htmlFor={id} className={checked ? 'text-blue-700' : disabled ? 'text-gray-400' : 'text-gray-500'}>
 				{label}
 			</label>
 		</div>
