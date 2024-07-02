c7b1cc1761e5a61d3d0bda8951587b8cf977f519
neinja007 <anton.siligan@gmail.com>
2024-07-02 05:03:42 +0200

    chore: Update SelectButton styling for better alignment and color consistency

--- src/components/SelectButton.tsx
+++ src/components/SelectButton.tsx
@@ -7,7 +7,7 @@ function SelectButton({ list, active, handleClick }: SelectButtonProps) {
 	return (
 		<button
 			onClick={() => handleClick(list)}
-			className={`p-1 px-3 rounded-lg border-2 font-medium shadow ${
+			className={`p-1 px-3 rounded-lg border shadow ${
 				active ? 'text-green-700 bg-green-200 border-green-400' : 'text-gray-700 bg-gray-200 border-gray-400'
 			}`}
 		>
