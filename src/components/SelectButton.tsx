b111aa2de394be037c8815eb60c83092801ef3fd
neinja007 <anton.siligan@gmail.com>
2024-07-02 14:08:18 +0200

    styling

--- src/components/SelectButton.tsx
+++ src/components/SelectButton.tsx
@@ -9,9 +9,14 @@ type SelectButtonProps = {
 
 function SelectButton({ label, active, handleClick }: SelectButtonProps) {
 	return (
-		<Button onClick={handleClick} className={`${active ? 'text-sky-100 bg-blue-600 border-blue-800' : ''}`}>
+		<button
+			onClick={handleClick}
+			className={`h-9 p-1 px-3 border rounded-lg shadow ${
+				active ? 'text-sky-100 bg-blue-600 border-blue-800' : 'border-gray-400'
+			} transition-colors`}
+		>
 			{label}
-		</Button>
+		</button>
 	);
 }
 
