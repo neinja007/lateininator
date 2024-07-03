4f3c777945411057874593646e29fe710afebab7
neinja007 <anton.siligan@gmail.com>
2024-07-03 06:14:59 +0200

    extract reusable functions

---
+++ src/utils/inputUtils.ts
@@ -0,0 +1,28 @@
+export const compareValues = (input: string, correctInput: string | Array<string>, translation?: boolean): boolean => {
+	if (translation && typeof correctInput === 'object') {
+		return input.split(',').some((translation) => {
+			return correctInput.some((correctInput) => {
+				return compareValues(translation, correctInput);
+			});
+		});
+	} else if (typeof correctInput === 'string') {
+		return input.trim().toLowerCase() === correctInput.trim().toLowerCase();
+	} else {
+		throw new Error('Error: compareValues() called with incorrect arguments.');
+	}
+};
+
+export const getInputWithCorrectValue = (
+	input: string,
+	correctValue: string | Array<string>,
+	translation?: true
+): string => {
+	const formattedValue = typeof correctValue === 'object' ? correctValue.join(', ') : correctValue;
+	if (compareValues(input, correctValue, translation) && !translation && typeof correctValue === 'string') {
+		return correctValue;
+	} else if (input.trim() === '') {
+		return `(${formattedValue})`;
+	} else {
+		return `${input} (${formattedValue})`;
+	}
+};
