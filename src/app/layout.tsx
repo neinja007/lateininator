319c39ce6dbb68338f80a8f3e217ec92f13a4ddc
neinja007 <anton.siligan@gmail.com>
2024-07-03 06:14:07 +0200

    fix navbar hiding content

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
 		<ClerkProvider localization={deDE}>
 			<html lang='en'>
 				<body className={rubik.className}>
-					<div className='min-h-screen px-4'>
+					<div className='min-h-screen px-4 pt-24'>
 						<Navbar />
 						{children}
 					</div>
