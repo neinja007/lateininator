67fc887d30412976be1a6e44050a1b11e6999206
neinja007 <anton.siligan@gmail.com>
2024-07-04 06:19:28 +0200

    it turns out the implicit type thing does not work for properties. but i do believe i understand why.

--- src/data/properties.ts
+++ src/data/properties.ts
@@ -1,44 +1,43 @@
-// import {
-// 	Comparison,
-// 	Case,
-// 	Conjugation,
-// 	Gender,
-// 	Modus,
-// 	Declension,
-// 	Numerus,
-// 	Person,
-// 	Tense,
-// 	Voice,
-// 	ComparisonDegree,
-// 	Type
-// } from './types';
+import {
+	Comparison,
+	Case,
+	Conjugation,
+	Gender,
+	Modus,
+	Declension,
+	Numerus,
+	Person,
+	Tense,
+	Voice,
+	ComparisonDegree,
+	Type
+} from './types';
 
-// export const properties: {
-// 	case: Array<Case>;
-// 	person: Array<Person>;
-// 	numerus: Array<Numerus>;
-// 	declension: Array<Declension>;
-// 	gender: Array<Gender>;
-// 	modus: Array<Modus>;
-// 	voice: Array<Voice>;
-// 	indTense: Array<Tense>;
-// 	konTense: Array<Exclude<Tense, 'fut1'>>;
-// 	wordKeys: {
-// 		noun: ['declension', 'genitive', 'gender'];
-// 		verb: ['conjugation', 'present', 'perfect', 'participle'];
-// 		irregular_verb: ['present', 'participle', 'perfect'];
-// 		adjective: ['comparison', 'femininum', 'neutrum'];
-// 		other: [];
-// 		adverb: [];
-// 		pronoun: [];
-// 	};
-// 	conjugation: Array<Conjugation>;
-// 	adjectiveDeclension: Array<Comparison>;
-// 	types: Array<Type>;
-// 	comparisonDegree: Array<ComparisonDegree>;
-// } = {
-
-export const properties = {
+export const properties: {
+	case: Array<Case>;
+	person: Array<Person>;
+	numerus: Array<Numerus>;
+	declension: Array<Declension>;
+	gender: Array<Gender>;
+	modus: Array<Modus>;
+	voice: Array<Voice>;
+	indTense: Array<Tense>;
+	konTense: Array<Exclude<Tense, 'fut1'>>;
+	wordKeys: {
+		noun: ['declension', 'genitive', 'gender'];
+		verb: ['conjugation', 'present', 'perfect', 'participle'];
+		irregular_verb: ['present', 'participle', 'perfect'];
+		adjective: ['comparison', 'femininum', 'neutrum'];
+		other: [];
+		adverb: [];
+		pronoun: [];
+	};
+	conjugation: Array<Conjugation>;
+	adjectiveDeclension: Array<Comparison>;
+	types: Array<Type>;
+	comparisonDegree: Array<ComparisonDegree>;
+} = {
+	// export const properties = {
 	case: ['1', '2', '3', '4', '5', '6'],
 	person: ['1', '2', '3'],
 	numerus: ['sin', 'plu'],
