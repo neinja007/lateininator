b4367abf560b237093baae45425a6d98fa5d14a3
neinja007 <anton.siligan@gmail.com>
2024-07-04 06:07:11 +0200

    add adjective tables and improve design

--- src/app/(general)/vocabulary/dictionary/[id]/page.tsx
+++ src/app/(general)/vocabulary/dictionary/[id]/page.tsx
@@ -6,6 +6,8 @@ import { words } from '@/data/words';
 import { getForm, getLexicalForm } from '@/utils/wordUtils';
 import Link from 'next/link';
 import { ArrowLeft } from 'react-feather';
+import { Fragment } from 'react';
+import { capitalizeFirstLetter } from '@/utils/inputUtils';
 
 type PageProps = { params: { id: string } };
 
@@ -29,93 +31,135 @@ const Page = ({ params: { id } }: PageProps) => {
 							<TypeIndicator type={word.type} />
 						</div>
 					</div>
-					<p>{word.translation?.join(', ')}</p>
-					{(word.type === 'noun' && (
+					<p>{word.translation?.map((translation) => capitalizeFirstLetter(translation)).join(', ')}</p>
+					{word.type === 'noun' ? (
 						<>
 							<p>
 								{mapper.extended.declension[word.declension]}; {mapper.extended.gender[word.gender]}
 							</p>
-							<div>
-								<table className='w-full rounded-lg table-fixed overflow-hidden shadow'>
-									<thead className='bg-gray-100'>
-										<tr>
-											<th />
-											{properties.numerus.map((numerus, i) => {
-												return (
-													<th key={i} className='px-3 py-1'>
-														{mapper.extended.numerus[numerus]}
-													</th>
-												);
-											})}
-										</tr>
-									</thead>
-									<tbody>
-										{properties.case.map((wordCase, i) => {
+							<hr />
+							<table className='w-full rounded-lg table-fixed overflow-hidden shadow'>
+								<thead className='bg-gray-100'>
+									<tr>
+										<th />
+										{properties.numerus.map((numerus, i) => {
 											return (
-												<tr key={i} className='border-t'>
-													<th key={i} className='px-3 py-1 bg-gray-100'>
-														{mapper.extended.case[wordCase]}
-													</th>
-													{properties.numerus.map((numerus, i) => {
-														return (
-															<td key={i} className='px-3 py-1'>
-																{getForm(word, { wordCase: wordCase, numerus: numerus })}
-															</td>
-														);
-													})}
-												</tr>
+												<th key={i} className='px-3 py-1'>
+													{mapper.extended.numerus[numerus]}
+												</th>
 											);
 										})}
-									</tbody>
-								</table>
+									</tr>
+								</thead>
+								<tbody>
+									{properties.case.map((wordCase, i) => {
+										return (
+											<tr key={i} className='border-t'>
+												<th key={i} className='px-3 py-1 bg-gray-100'>
+													{mapper.extended.case[wordCase]}
+												</th>
+												{properties.numerus.map((numerus, i) => {
+													return (
+														<td key={i} className='px-3 py-1'>
+															{getForm(word, { wordCase: wordCase, numerus: numerus })}
+														</td>
+													);
+												})}
+											</tr>
+										);
+									})}
+								</tbody>
+							</table>
+						</>
+					) : word.type === 'verb' ? (
+						<>
+							<p>{mapper.extended.conjugation[word.conjugation]}</p>
+							<hr />
+							<div className='space-y-2'>
+								{properties.modus.map((modus) =>
+									properties.voice.map((voice, i) => (
+										<Fragment key={i}>
+											<p>
+												{mapper.extended.modus[modus]} {mapper.extended.voice[voice]}
+											</p>
+											<table className='w-full rounded-lg table-fixed overflow-hidden shadow'>
+												<thead className='bg-gray-100'>
+													<tr>
+														<th />
+														{properties[modus === 'ind' ? 'indTense' : 'konTense'].map((tense, i) => (
+															<th key={i} className='px-3 py-1'>
+																{mapper.extended.tense[tense]}
+															</th>
+														))}
+													</tr>
+												</thead>
+												<tbody>
+													{properties.numerus.map((numerus) =>
+														properties.person.map((person, i) => (
+															<tr key={i} className='border-t'>
+																<th className='px-3 py-1 bg-gray-100'>
+																	{mapper.short.person[person]} {mapper.extended.numerus[numerus]}
+																</th>
+																{properties[modus === 'ind' ? 'indTense' : 'konTense'].map((tense, i) => (
+																	<td key={i} className='px-3 py-1'>
+																		{getForm(word, { modus, numerus, person, tense, voice })}
+																	</td>
+																))}
+															</tr>
+														))
+													)}
+												</tbody>
+											</table>
+											<br />
+										</Fragment>
+									))
+								)}
 							</div>
 						</>
-					)) ||
-						(word.type === 'verb' && (
-							<>
-								<p>{mapper.extended.conjugation[word.conjugation]}</p>
-								<div className='space-y-2'>
-									{properties.modus.map((modus) =>
-										properties.voice.map((voice, i) => (
-											<>
-												<p>
-													{mapper.extended.modus[modus]} {mapper.extended.voice[voice]}
-												</p>
-												<table key={i} className='w-full rounded-lg table-fixed overflow-hidden shadow'>
-													<thead className='bg-gray-100'>
-														<tr>
-															<th />
-															{properties[modus === 'ind' ? 'indTense' : 'konTense'].map((tense, i) => (
-																<th key={i} className='px-3 py-1'>
-																	{mapper.extended.tense[tense]}
-																</th>
+					) : word.type === 'adjective' ? (
+						<>
+							<p>{mapper.extended.comparison[word.comparison]}</p>
+							<hr />
+							<div className='space-y-2'>
+								{properties.comparisonDegree.map((comparisonDegree, i) => (
+									<Fragment key={i}>
+										<p>{mapper.extended.comparisonDegree[comparisonDegree]}</p>
+										<table key={i} className='w-full rounded-lg table-fixed overflow-hidden shadow'>
+											<thead className='bg-gray-100'>
+												<tr>
+													<th />
+													{properties.gender.map((gender, i) => (
+														<th key={i} className='px-3 py-1'>
+															{mapper.extended.gender[gender]}
+														</th>
+													))}
+												</tr>
+											</thead>
+											<tbody>
+												{properties.numerus.map((numerus) =>
+													properties.case.map((wordCase, i) => (
+														<tr key={i} className='border-t'>
+															<th className='px-3 py-1 bg-gray-100'>
+																{mapper.extended.case[wordCase]} {mapper.extended.numerus[numerus]}
+															</th>
+															{properties.gender.map((gender, i) => (
+																<td key={i} className='px-3 py-1'>
+																	{getForm(word, { comparisonDegree, gender, numerus, wordCase })}
+																</td>
 															))}
 														</tr>
-													</thead>
-													<tbody>
-														{properties.numerus.map((numerus) =>
-															properties.person.map((person, i) => (
-																<tr key={i} className='border-t'>
-																	<th className='px-3 py-1 bg-gray-100'>
-																		{mapper.short.person[person]} {mapper.extended.numerus[numerus]}
-																	</th>
-																	{properties[modus === 'ind' ? 'indTense' : 'konTense'].map((tense, i) => (
-																		<td key={i} className='px-3 py-1'>
-																			{getForm(word, { modus, numerus, person, tense, voice })}
-																		</td>
-																	))}
-																</tr>
-															))
-														)}
-													</tbody>
-												</table>
-												<br />
-											</>
-										))
-									)}
-								</div>
-							</>
-						))}
+													))
+												)}
+											</tbody>
+										</table>
+										<br />
+									</Fragment>
+								))}
+							</div>
+						</>
+					) : (
+						false
+					)}
 				</>
 			) : (
 				<span>
