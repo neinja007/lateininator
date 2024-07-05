fe9bd306a9dd8d86f0c643f1445eda0c5da43c39
neinja007 <anton.siligan@gmail.com>
2024-07-05 06:38:28 +0200

    lists

--- src/data/lists.ts
+++ src/data/lists.ts
@@ -16,5 +16,29 @@ export const lists: Array<List> = [
 	{
 		name: 'Lektion 3',
 		words: generateSequentialNumbers(65, 91)
+	},
+	{
+		name: 'Lektion 4',
+		words: generateSequentialNumbers(92, 120)
+	},
+	{
+		name: 'Lektion 5',
+		words: generateSequentialNumbers(121, 161)
+	},
+	{
+		name: 'Lektion 6',
+		words: generateSequentialNumbers(162, 186)
+	},
+	{
+		name: 'Lektion 7',
+		words: generateSequentialNumbers(187, 215)
+	},
+	{
+		name: 'Lektion 8',
+		words: generateSequentialNumbers(216, 237)
+	},
+	{
+		name: 'Lektion 9',
+		words: generateSequentialNumbers(238, 265)
 	}
 ];
