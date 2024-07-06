6b493460b27bb238b1f494ca3cc211f6e2359aca
neinja007 <anton.siligan@gmail.com>
2024-07-06 05:19:58 +0200

    removed spacing

--- src/app/(general)/layout.tsx
+++ src/app/(general)/layout.tsx
@@ -7,7 +7,7 @@ type MainLayoutProps = {
 const MainLayout = ({ children }: MainLayoutProps) => {
 	return (
 		<div>
-			<div className='container max-w-[1024px] mx-auto *:space-y-5'>{children}</div>
+			<div className='container max-w-[1024px] mx-auto'>{children}</div>
 		</div>
 	);
 };
