b96604862008fa983c4cc129ed020b9b90bdacbe
neinja007 <anton.siligan@gmail.com>
2024-07-07 07:54:30 +0200

    cleanup

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,10 +1,10 @@
-type ButtonProps = { children: React.ReactNode; className?: React.CSSProperties } & React.ComponentProps<'button'>;
+type ButtonProps = { children: React.ReactNode } & React.ComponentProps<'button'>;
 
-const Button = ({ children, className, ...props }: ButtonProps) => {
+const Button = ({ children, ...props }: ButtonProps) => {
 	return (
 		<button
 			{...props}
-			className={`h-9 p-1 px-3 border border-gray-400 rounded-lg active:border-gray-700 shadow ${className}`}
+			className={`h-9 p-1 px-3 border border-gray-400 rounded-lg active:border-gray-700 shadow ${props.className}`}
 		>
 			{children}
 		</button>
