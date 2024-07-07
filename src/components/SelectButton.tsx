5d9a71fb51d8c5bfaa36f70e856907e86e064fd5
neinja007 <anton.siligan@gmail.com>
2024-07-07 07:54:20 +0200

    select button accepts classname

--- src/components/SelectButton.tsx
+++ src/components/SelectButton.tsx
@@ -1,19 +1,23 @@
 import { Dispatch, SetStateAction } from 'react';
-import Button from './ui/Button';
+import clsx from 'clsx';
 
 type SelectButtonProps = {
 	label: string;
 	active: boolean;
 	handleClick: Dispatch<SetStateAction<any>>;
+	className?: React.CSSProperties & string;
 };
 
-const SelectButton = ({ label, active, handleClick }: SelectButtonProps) => {
+const SelectButton = ({ label, active, handleClick, className }: SelectButtonProps) => {
 	return (
 		<button
 			onClick={handleClick}
-			className={`h-9 p-1 px-3 border rounded-lg shadow ${
-				active ? 'text-sky-100 bg-blue-600 border-blue-800' : 'border-gray-400'
-			} transition-colors`}
+			className={clsx(
+				`h-9 p-1 px-3 border rounded-lg shadow ${
+					active ? 'text-sky-100 bg-blue-600 border-blue-800' : 'border-gray-400'
+				} transition-colors`,
+				className
+			)}
 		>
 			{label}
 		</button>
