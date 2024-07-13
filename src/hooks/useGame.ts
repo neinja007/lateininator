d83de5d55083ee4352419a8d954cc709141fd39f
neinja007 <anton.siligan@gmail.com>
2024-07-13 16:25:53 +0200

    merge useActiveWord and useStage into useGame

--- src/hooks/useGame.ts
+++ src/hooks/useGame.ts
@@ -1,19 +1,25 @@
-import { Word } from '@/types';
+import { Stage, Word } from '@/types';
+import { stat } from 'fs';
 import { useCallback, useEffect, useState } from 'react';
 
-export const useActiveWord = (
-	staticPossibleWords: boolean
+export const useGame = (
+	staticPossibleWords: boolean,
+	resetInputs: () => void,
+	canContinue?: (word: Word) => boolean
 ): {
 	activeWord: Word | undefined;
 	remainingWords: number;
 	maxWords: number;
 	updateActiveWord: () => void;
 	updateWords: (arg?: Word[]) => void;
+	handleContinue: (arg?: Stage) => void;
+	stage: Stage;
 } => {
 	const [possibleWords, setPossibleWords] = useState<Word[]>([]);
 	const [activeWord, setActiveWord] = useState<Word>();
 	const [remainingWords, setRemainingWords] = useState<number>(0);
 	const [maxWords, setMaxWords] = useState<number>(0);
+	const [stage, setStage] = useState<Stage>('settings');
 
 	const updateActiveWord = useCallback(() => {
 		setActiveWord(possibleWords[Math.floor(Math.random() * possibleWords.length)]);
@@ -24,6 +30,31 @@ export const useActiveWord = (
 			setRemainingWords(possibleWords.length);
 		}
 	}, [possibleWords, staticPossibleWords]);
+
+	const handleContinue = (newStage?: Stage) => {
+		if (newStage) setStage(newStage);
+		else newStage = stage;
+		if (newStage === 'test') {
+			if (!activeWord) {
+				throw new Error('activeWord is undefined');
+			}
+
+			setStage('review');
+
+			if (staticPossibleWords || canContinue) {
+				updateWords();
+			}
+		} else {
+			if (remainingWords === 0) {
+				setStage('results');
+				return;
+			}
+			setStage('test');
+
+			updateActiveWord();
+			resetInputs();
+		}
+	};
 
 	const updateWords = useCallback(
 		(words?: Word[]) => {
@@ -48,7 +79,9 @@ export const useActiveWord = (
 		activeWord,
 		remainingWords,
 		maxWords,
+		stage,
 		updateActiveWord,
-		updateWords
+		updateWords,
+		handleContinue
 	};
 };
