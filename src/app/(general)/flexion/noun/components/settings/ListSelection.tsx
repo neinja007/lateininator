ad28f8bab7107c5fa6076b992d1539187f98e722
neinja007 <anton.siligan@gmail.com>
2024-07-23 08:44:12 +0200

    feat: Add AdjectiveListSelection component for noun settings page

---
+++ src/app/(general)/flexion/noun/components/settings/ListSelection.tsx
@@ -0,0 +1,33 @@
+import Select from '@/components/Select';
+import { lists } from '@/data/lists';
+import { Word } from '@/types';
+
+type AdjectiveListSelectionProps = {
+  maxUnit: number;
+  setMaxUnit: (arg: number) => void;
+  validWords: Word[];
+};
+
+const AdjectiveListSelection = ({ maxUnit, setMaxUnit, validWords }: AdjectiveListSelectionProps) => {
+  return (
+    <>
+      <p>Wähle eine Lektion aus. Wörter zur Abfrage werden von dieser und von vorherigen Lektionen ausgewählt.</p>
+      <div className='flex'>
+        <Select
+          label='Lektion'
+          value={maxUnit}
+          handleChange={setMaxUnit}
+          options={lists.reduce((acc: any, list) => {
+            acc[list.id] = list.name;
+            return acc;
+          }, {})}
+        />
+        <span className='mb-1.5 ml-5 mt-auto'>
+          Du hast <b className='text-blue-500'>{validWords.length} Nomen</b> ausgewählt.
+        </span>
+      </div>
+    </>
+  );
+};
+
+export default AdjectiveListSelection;
