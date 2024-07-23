69dac441c1a1c733c1f129afa76c317f2e2ec85e
neinja007 <anton.siligan@gmail.com>
2024-07-23 09:34:15 +0200

    refactor: Add TestingFormSelection component for verb flexion settings page

---
+++ src/app/(general)/flexion/verb/components/settings/TestingFormSelection.tsx
@@ -0,0 +1,65 @@
+import CheckboxList from '@/components/CheckboxList';
+import { WORD_CONSTANTS } from '@/constants';
+import { Conjugation, Modus, Tense, Voice } from '@/types';
+import { MAPPER } from '@/utils/mapper';
+import { Dispatch, SetStateAction } from 'react';
+
+type TestingFormSelectionProps = {
+  voices: Voice[];
+  setVoices: Dispatch<SetStateAction<Voice[]>>;
+  modi: Modus[];
+  setModi: Dispatch<SetStateAction<Modus[]>>;
+  tenses: Tense[];
+  setTenses: Dispatch<SetStateAction<Tense[]>>;
+  conjugations: Conjugation[];
+  setConjugations: Dispatch<SetStateAction<Conjugation[]>>;
+};
+
+const TestingFormSelection = ({
+  modi,
+  setModi,
+  setTenses,
+  setVoices,
+  tenses,
+  voices,
+  conjugations,
+  setConjugations
+}: TestingFormSelectionProps) => {
+  return (
+    <>
+      <p>Wähle aus, was abgefragt werden soll:</p>
+      <div className='grid grid-cols-4'>
+        <CheckboxList
+          options={[...WORD_CONSTANTS.conjugation]}
+          selected={conjugations}
+          setSelected={setConjugations}
+          label='Konjugation'
+          mapper={MAPPER.extended.conjugation}
+        />
+        <CheckboxList
+          options={[...WORD_CONSTANTS.modus]}
+          selected={modi}
+          setSelected={setModi}
+          label='Modus'
+          mapper={MAPPER.extended.modus}
+        />
+        <CheckboxList
+          options={[...WORD_CONSTANTS.tense]}
+          selected={tenses}
+          setSelected={setTenses}
+          label='Zeitformen'
+          mapper={MAPPER.extended.tense}
+        />
+        <CheckboxList
+          options={[...WORD_CONSTANTS.voice]}
+          selected={voices}
+          setSelected={setVoices}
+          label='Aktiv / Passiv'
+          mapper={MAPPER.extended.voice}
+        />
+      </div>
+    </>
+  );
+};
+
+export default TestingFormSelection;
