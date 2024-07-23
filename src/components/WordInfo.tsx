6b4d539cec27e3f27f1df15ea41895ea25cc7927
neinja007 <anton.siligan@gmail.com>
2024-07-23 08:43:37 +0200

    chore: Update WordInfo component to use black text color

--- src/components/WordInfo.tsx
+++ src/components/WordInfo.tsx
@@ -1,7 +1,7 @@
 type WordInfoProps = { info: string };
 
 const WordInfo = ({ info }: WordInfoProps) => {
-  return <p className='float-end rounded-lg border border-gray-600 bg-gray-100 p-1 px-3'>{info}</p>;
+  return <p className='float-end rounded-lg border border-gray-600 bg-gray-100 p-1 px-3 text-black'>{info}</p>;
 };
 
 export default WordInfo;
