839361337d78ab34799b6d364cde94a8c7cc442b
neinja007 <anton.siligan@gmail.com>
2024-07-23 08:43:49 +0200

    refactor: Update TestingFormSelection component to use CheckboxList for improved code organization and reusability

--- src/app/(general)/flexion/adjective/components/settings/TestingFormSelection.tsx
+++ src/app/(general)/flexion/adjective/components/settings/TestingFormSelection.tsx
@@ -1,3 +1,4 @@
+import CheckboxList from '@/components/CheckboxList';
 import CheckboxWithLabel from '@/components/CheckboxWithLabel';
 import { WORD_CONSTANTS } from '@/constants';
 import { Comparison, ComparisonDegree, Gender } from '@/types';
@@ -36,47 +37,27 @@ const TestingFormSelection = ({
         />
       </div>
       <div className='grid grid-cols-3'>
-        <div>
-          Deklination:
-          {WORD_CONSTANTS.comparison.map((comparison) => (
-            <CheckboxWithLabel
-              key={comparison}
-              checked={comparisons.includes(comparison)}
-              handleChange={(checked) =>
-                setComparisons((prev) => (checked ? [...prev, comparison] : prev.filter((p) => p !== comparison)))
-              }
-              label={MAPPER.extended.comparison[comparison]}
-            />
-          ))}
-        </div>
-        <div>
-          Steigerungsform:
-          {WORD_CONSTANTS.comparisonDegree.map((comparisonDegree) => (
-            <CheckboxWithLabel
-              key={comparisonDegree}
-              checked={comparisonDegrees.includes(comparisonDegree)}
-              handleChange={(checked) =>
-                setComparisonDegrees((prev) =>
-                  checked ? [...prev, comparisonDegree] : prev.filter((p) => p !== comparisonDegree)
-                )
-              }
-              label={MAPPER.extended.comparisonDegree[comparisonDegree]}
-            />
-          ))}
-        </div>
-        <div>
-          Geschlecht:
-          {WORD_CONSTANTS.gender.map((gender) => (
-            <CheckboxWithLabel
-              key={gender}
-              checked={genders.includes(gender)}
-              handleChange={(checked) =>
-                setGenders((prev) => (checked ? [...prev, gender] : prev.filter((p) => p !== gender)))
-              }
-              label={MAPPER.extended.gender[gender]}
-            />
-          ))}
-        </div>
+        <CheckboxList
+          options={[...WORD_CONSTANTS.comparison]}
+          selected={comparisons}
+          setSelected={setComparisons}
+          label='Deklination'
+          mapper={MAPPER.extended.comparison}
+        />
+        <CheckboxList
+          options={[...WORD_CONSTANTS.comparisonDegree]}
+          selected={comparisonDegrees}
+          setSelected={setComparisonDegrees}
+          label='Steigerungsform'
+          mapper={MAPPER.extended.comparisonDegree}
+        />
+        <CheckboxList
+          options={[...WORD_CONSTANTS.gender]}
+          selected={genders}
+          setSelected={setGenders}
+          label='Geschlecht'
+          mapper={MAPPER.extended.gender}
+        />
       </div>
     </>
   );
