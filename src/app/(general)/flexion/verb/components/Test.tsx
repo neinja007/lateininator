2866616187b53c532ba1ccc73141f9cfb812f6b8
neinja007 <anton.siligan@gmail.com>
2024-07-23 09:34:09 +0200

    refactor: Add Test component for verb flexion page

---
+++ src/app/(general)/flexion/verb/components/Test.tsx
@@ -0,0 +1,117 @@
+import ActionBar from '@/components/ActionBar';
+import WordDisplay from '@/components/WordDisplay';
+import { Conjugation, Modus, Numerus, Person, Tense, Verb, Voice } from '@/types';
+import { Dispatch, SetStateAction, useEffect, useState } from 'react';
+import IndividualInput from './test/IndividualInput';
+import TableInput from './test/TableInput';
+import { getRandomItem } from '@/utils/propertyUtils';
+import { WORD_CONSTANTS } from '@/constants';
+
+type TestProps = {
+  activeWord: Verb;
+  testingType: 'table' | 'individual';
+  stage: 'test' | 'review';
+  tableInputValues: Record<Tense, Record<Numerus, Record<Exclude<Person, '4'>, string>>>;
+  setTableInputValues: Dispatch<SetStateAction<Record<Tense, Record<Numerus, Record<Exclude<Person, '4'>, string>>>>>;
+  maxWords: number;
+  remainingWords: number;
+  handleContinue: () => void;
+  modi: Modus[];
+  voices: Voice[];
+  tenses: Tense[];
+  individualInputValue: string;
+  setIndividualInputValue: Dispatch<SetStateAction<string>>;
+};
+
+const Test = ({
+  activeWord,
+  testingType,
+  stage,
+  tableInputValues,
+  setTableInputValues,
+  maxWords,
+  remainingWords,
+  handleContinue,
+  modi,
+  voices,
+  tenses,
+  individualInputValue,
+  setIndividualInputValue
+}: TestProps) => {
+  const [individualInputForm, setIndividualInputForm] = useState<{
+    conjugation: Conjugation;
+    voice: Voice;
+    modus: Modus;
+    tense: Tense;
+    numerus: Numerus;
+    person: Person;
+  }>({
+    conjugation: activeWord.conjugation as Conjugation,
+    voice: getRandomItem(voices),
+    modus: getRandomItem(modi),
+    tense: getRandomItem(tenses),
+    numerus: getRandomItem([...WORD_CONSTANTS.numerus]),
+    person: getRandomItem([...WORD_CONSTANTS.person])
+  });
+
+  const [tableInputForm, setTableInputForm] = useState<{
+    voice: Voice;
+    modus: Modus;
+  }>({
+    voice: getRandomItem(voices),
+    modus: getRandomItem(modi)
+  });
+
+  useEffect(() => {
+    if (!activeWord || activeWord.type !== 'verb') return;
+    if (testingType === 'individual') {
+      activeWord.type === 'verb' &&
+        setIndividualInputForm({
+          conjugation: activeWord.conjugation as Conjugation,
+          voice: getRandomItem(voices),
+          modus: getRandomItem(modi),
+          tense: getRandomItem(tenses),
+          numerus: getRandomItem([...WORD_CONSTANTS.numerus]),
+          person: getRandomItem([...WORD_CONSTANTS.person.filter((person) => person !== '4')])
+        });
+    } else {
+      setTableInputForm({
+        voice: getRandomItem(voices),
+        modus: getRandomItem(modi)
+      });
+    }
+  }, [activeWord, modi, tenses, testingType, voices]);
+
+  return (
+    <>
+      <WordDisplay word={activeWord} />
+      <hr className='border-gray-500' />
+      <div>
+        {individualInputForm && testingType === 'individual' ? (
+          <IndividualInput
+            individualInputForm={individualInputForm}
+            individualInputValue={individualInputValue}
+            setIndividualInputValue={setIndividualInputValue}
+            stage={stage}
+            activeWord={activeWord}
+          />
+        ) : (
+          tableInputForm &&
+          testingType === 'table' && (
+            <TableInput
+              tableInputForm={tableInputForm}
+              tableInputValues={tableInputValues}
+              setTableInputValues={setTableInputValues}
+              stage={stage}
+              activeWord={activeWord}
+            />
+          )
+        )}
+      </div>
+      <hr className='border-gray-500' />
+      <ActionBar handleContinue={handleContinue} progressPercentage={((maxWords - remainingWords) / maxWords) * 100} />
+    </>
+  );
+};
+
+export default Test;
