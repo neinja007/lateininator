458d39e459c45d1af741d84cbc12bb5848a83323
neinja007 <anton.siligan@gmail.com>
2024-07-26 07:44:35 +0200

    refactor: Update WordInfo component with dark mode styles for background and text color

--- src/components/WordInfo.tsx
+++ src/components/WordInfo.tsx
@@ -1,7 +1,11 @@
 type WordInfoProps = { info: string };
 
 const WordInfo = ({ info }: WordInfoProps) => {
-  return <p className='float-end rounded-lg border border-gray-600 bg-gray-100 p-1 px-3 text-black'>{info}</p>;
+  return (
+    <p className='float-end rounded-lg border border-gray-600 bg-gray-100 p-1 px-3 text-black dark:bg-gray-800 dark:text-white'>
+      {info}
+    </p>
+  );
 };
 
 export default WordInfo;
