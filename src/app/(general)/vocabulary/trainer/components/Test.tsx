6b5b0aa0d74b336af447f2c7cbbd35d11ffb2d0a
neinja007 <anton.siligan@gmail.com>
2024-08-04 09:03:26 +0200

    test form implementation in vocabulary trainer

--- src/app/(general)/vocabulary/trainer/components/Test.tsx
+++ src/app/(general)/vocabulary/trainer/components/Test.tsx
@@ -1,9 +1,10 @@
-import ActionBar from '@/components/ActionBar';
 import WordDisplay from '@/components/WordDisplay';
 import { Stage, Word, WordProperty } from '@/types';
 import { Dispatch, SetStateAction } from 'react';
 import TranslationInput from './test/TranslationInput';
 import PropertyInputs from './test/PropertyInputs';
+import ActionBar from '@/components/ActionBar';
+import { useTestForm } from '@/hooks/useTestForm';
 
 type TestProps = {
   stage: Stage;
@@ -26,29 +27,33 @@ const Test = ({
   handleContinue,
   progressPercentage
 }: TestProps) => {
+  const { submit } = useTestForm(handleContinue);
+
   return (
     activeWord && (
       <>
         <WordDisplay word={activeWord} />
         <hr className='dark:border-gray-500' />
-        <TranslationInput
-          checkTranslation={checkTranslation}
-          activeWord={activeWord}
-          stage={stage}
-          inputValues={inputValues}
-          setInputValues={setInputValues}
-        />
-        {validKeysToCheck.length > 0 && (
-          <PropertyInputs
-            validKeysToCheck={validKeysToCheck}
+        <form onSubmit={submit} className='space-y-5'>
+          <TranslationInput
+            checkTranslation={checkTranslation}
             activeWord={activeWord}
+            stage={stage}
             inputValues={inputValues}
             setInputValues={setInputValues}
-            stage={stage}
           />
-        )}
-        <hr className='dark:border-gray-500' />
-        <ActionBar handleContinue={handleContinue} progressPercentage={progressPercentage} />
+          {validKeysToCheck.length > 0 && (
+            <PropertyInputs
+              validKeysToCheck={validKeysToCheck}
+              activeWord={activeWord}
+              inputValues={inputValues}
+              setInputValues={setInputValues}
+              stage={stage}
+            />
+          )}
+          <hr className='dark:border-gray-500' />
+          <ActionBar form handleContinue={handleContinue} progressPercentage={progressPercentage} />
+        </form>
       </>
     )
   );
