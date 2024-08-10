8ef3607f90eaf6ead663cd270bdd164212b072ac
neinja007 <anton.siligan@gmail.com>
2024-08-10 09:59:47 +0200

    add disabledOptions to checkboxList

--- src/components/CheckboxList.tsx
+++ src/components/CheckboxList.tsx
@@ -4,20 +4,29 @@ import { Dispatch, SetStateAction } from 'react';
 type CheckboxListProps = {
   label: string;
   disabled?: boolean;
+  disabledOptions?: string[];
   options: string[];
   mapper: { [key: string]: string };
   selected: string[];
   setSelected: Dispatch<SetStateAction<any[]>>;
 };
 
-const CheckboxList = ({ label, options, selected, disabled, setSelected, mapper }: CheckboxListProps) => {
+const CheckboxList = ({
+  label,
+  options,
+  selected,
+  disabled,
+  setSelected,
+  mapper,
+  disabledOptions
+}: CheckboxListProps) => {
   return (
     <div>
       <span className={disabled ? 'text-gray-500' : ''}>{label}:</span>
       {options.map((option) => (
         <CheckboxWithLabel
           key={option}
-          disabled={disabled}
+          disabled={disabled || disabledOptions?.includes(option)}
           checked={selected.includes(option)}
           handleChange={(checked) =>
             setSelected((prev) => (checked ? [...prev, option] : prev.filter((p) => p !== option)))
