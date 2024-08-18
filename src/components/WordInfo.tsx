ca7e3ac6116a50c22aff4d2c962423b90717f4cb
neinja007 <anton.siligan@gmail.com>
2024-08-18 16:30:01 +0200

    dim down word info

--- src/components/WordInfo.tsx
+++ src/components/WordInfo.tsx
@@ -2,7 +2,7 @@ type WordInfoProps = { info: string };
 
 const WordInfo = ({ info }: WordInfoProps) => {
   return (
-    <p className='float-end rounded-lg border border-gray-600 bg-gray-100 p-1 px-3 text-black dark:bg-gray-800 dark:text-white'>
+    <p className='float-end rounded-lg border border-gray-600 bg-gray-100 p-1 px-3 text-black dark:bg-gray-800 dark:text-gray-400'>
       {info}
     </p>
   );
