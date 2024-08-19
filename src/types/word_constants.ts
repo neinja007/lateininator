575da05fc4b715edb37bbc1a94186f236e648bea
neinja007 <anton.siligan@gmail.com>
2024-08-19 09:11:52 +0200

    export optional types

--- src/types/word_constants.ts
+++ src/types/word_constants.ts
@@ -6,8 +6,14 @@ export type Numerus = (typeof WORD_CONSTANTS.numerus)[number];
 export type Modus = (typeof WORD_CONSTANTS.modus)[number];
 export type Voice = (typeof WORD_CONSTANTS.voice)[number];
 export type Tense = (typeof WORD_CONSTANTS.tense)[number];
+export type ComparisonDegree = (typeof WORD_CONSTANTS.comparisonDegree)[number];
+
 export type Declension = (typeof WORD_CONSTANTS.declension)[number];
 export type Gender = (typeof WORD_CONSTANTS.gender)[number];
 export type Conjugation = (typeof WORD_CONSTANTS.conjugation)[number];
 export type Comparison = (typeof WORD_CONSTANTS.comparison)[number];
-export type ComparisonDegree = (typeof WORD_CONSTANTS.comparisonDegree)[number];
+
+export type OptionalDeclension = (typeof WORD_CONSTANTS.optionalDeclension)[number];
+export type OptionalGender = (typeof WORD_CONSTANTS.optionalGender)[number];
+export type OptionalConjugation = (typeof WORD_CONSTANTS.optionalConjugation)[number];
+export type OptionalComparison = (typeof WORD_CONSTANTS.optionalComparison)[number];
