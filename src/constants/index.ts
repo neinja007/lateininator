eba28d1a3f82a941bf0a4e10230472b9e2c60978
neinja007 <anton.siligan@gmail.com>
2024-08-19 09:10:26 +0200

    added optional word types

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -6,11 +6,17 @@ const numerus = ['sin', 'plu'] as const;
 const modus = ['ind', 'kon'] as const;
 const voice = ['act', 'pas'] as const;
 const tense = ['pres', 'perf', 'plus', 'fut1', 'impe'] as const;
+const comparisonDegree = ['pos', 'comp', 'sup'] as const;
+
 const declension = ['a', 'e', 'i', 'k', 'm', 'o', 'u'] as const;
 const gender = ['m', 'f', 'n'] as const;
 const conjugation = ['a', 'e', 'i', 'k', 'm'] as const;
 const comparison = ['a_o', 'kon'] as const;
-const comparisonDegree = ['pos', 'comp', 'sup'] as const;
+
+const optionalDeclension = ['-', ...declension] as const;
+const optionalGender = ['-', ...gender] as const;
+const optionalConjugation = ['-', ...conjugation] as const;
+const optionalComparison = ['-', ...comparison] as const;
 
 export const WORD_CONSTANTS = {
   wordCase,
@@ -23,7 +29,11 @@ export const WORD_CONSTANTS = {
   gender,
   conjugation,
   comparison,
-  comparisonDegree
+  comparisonDegree,
+  optionalDeclension,
+  optionalGender,
+  optionalConjugation,
+  optionalComparison
 } as const;
 
 const wordProperties = {
