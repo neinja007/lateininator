ee2f06e9dac7e63d6f9fec29dd65bff081cbcb90
neinja007 <anton.siligan@gmail.com>
2024-08-19 10:03:36 +0200

    improve property input resilience and prop clarity

--- src/app/(general)/vocabulary/trainer/components/test/PropertyInput.tsx
+++ src/app/(general)/vocabulary/trainer/components/test/PropertyInput.tsx
@@ -3,46 +3,55 @@ import { WORD_CONSTANTS } from '@/constants';
 import { MAPPER } from '@/utils/other/mapper';
 import clsx from 'clsx';
 import ui from '@/styles/ui.module.css';
-import TrainerInput from './TrainerInput';
+import TrainerInput from '../../../../../../components/TrainerInput';
 import { isWordPropertiesUsingSelectInput } from '@/utils/typeguards/isWordPropertiesUsingSelectInput';
 import { WordProperty } from '@/types/app_constants';
 
 type PropertyInputProps = {
   property: WordProperty;
-  value: string;
-  appendedString?: string;
+  inputValue: string;
   handleChange: (key: WordProperty, value: string) => void;
-  correct?: boolean;
+  correctValue: string;
+  isInputCorrect: boolean;
+  stage: 'test' | 'review';
 };
 
-const PropertyInput = ({ correct, property, handleChange, value, appendedString }: PropertyInputProps) => {
+const PropertyInput = ({
+  stage,
+  correctValue,
+  property,
+  handleChange,
+  inputValue,
+  isInputCorrect
+}: PropertyInputProps) => {
   const options = isWordPropertiesUsingSelectInput(property)
-    ? WORD_CONSTANTS[property].reduce((object: { [key: string]: string }, element) => {
+    ? WORD_CONSTANTS.optional[property].reduce((object: { [key: string]: string }, element) => {
         object[element] = (MAPPER.extended[property] as { [key: string]: string })[element];
         return object;
       }, {})
     : {};
-  const correctValueIndicator = correct !== undefined && (correct ? ui.correct : ui.incorrect);
+  const correctValueIndicatorClasses = isInputCorrect ? ui.correct : ui.incorrect;
 
   if (isWordPropertiesUsingSelectInput(property)) {
     return (
       <Select
         label={MAPPER.extended.property[property]}
         options={options}
-        className={clsx('w-full', correctValueIndicator)}
-        value={value}
-        appendString={correct === false ? appendedString : undefined}
+        className={clsx('w-full', stage === 'review' && correctValueIndicatorClasses)}
+        value={inputValue}
+        appendString={stage === 'review' && !isInputCorrect ? correctValue : undefined}
         handleChange={(value) => handleChange(property, value)}
-        disabled={correct !== undefined}
+        disabled={stage === 'review'}
       />
     );
   } else {
     return (
       <TrainerInput
         label={MAPPER.extended.property[property]}
-        correctValue={appendedString}
-        value={value}
+        correctValue={correctValue}
+        value={inputValue}
         handleChange={(value) => handleChange(property, value)}
+        stage={stage}
       />
     );
   }
