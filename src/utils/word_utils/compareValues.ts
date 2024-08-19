e6eda9e749932f85dcd1d75582feb7788704b87d
neinja007 <anton.siligan@gmail.com>
2024-08-19 10:08:49 +0200

    support empty translations array

--- src/utils/word_utils/compareValues.ts
+++ src/utils/word_utils/compareValues.ts
@@ -1,5 +1,6 @@
 export const compareValues = (input: string, correctInput: string | string[], translation?: boolean): boolean => {
   if (translation && typeof correctInput === 'object') {
+    if (correctInput.length === 0 && !input.trim()) return true;
     return input.split(',').some((translation) => {
       return correctInput.some((correctInput) => {
         return compareValues(translation, correctInput);
