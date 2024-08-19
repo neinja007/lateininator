a153f96a8ea3888746b0c3157f21461c776351b3
neinja007 <anton.siligan@gmail.com>
2024-08-19 09:11:35 +0200

    support optinal types in mapper

--- src/types/mapper.ts
+++ src/types/mapper.ts
@@ -1,26 +1,26 @@
 import { WordType, WordProperty, OtherProperty } from './app_constants';
 import {
-  Declension,
-  Comparison,
   WordCase,
-  Gender,
   Numerus,
-  Conjugation,
   Tense,
   Person,
   Modus,
   Voice,
-  ComparisonDegree
+  ComparisonDegree,
+  OptionalDeclension,
+  OptionalComparison,
+  OptionalGender,
+  OptionalConjugation
 } from './word_constants';
 
 export type MapperKeys = {
   type: Record<WordType, string>;
-  declension: Record<Declension, string>;
-  comparison: Record<Comparison, string>;
+  declension: Record<OptionalDeclension, string>;
+  comparison: Record<OptionalComparison, string>;
   wordCase: Record<WordCase, string>;
-  gender: Record<Gender, string>;
+  gender: Record<OptionalGender, string>;
   numerus: Record<Numerus, string>;
-  conjugation: Record<Conjugation, string>;
+  conjugation: Record<OptionalConjugation, string>;
   tense: Record<Tense, string>;
   person: Record<Person, string>;
   modus: Record<Modus, string>;
