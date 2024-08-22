034c702df9aaba8250819e736adc2518420af10f
neinja007 <anton.siligan@gmail.com>
2024-08-22 11:07:59 +0200

    add basic overview page

--- src/app/(general)/dashboard/page.tsx
+++ src/app/(general)/dashboard/page.tsx
@@ -1,7 +1,38 @@
+import Heading from '@/components/Heading';
+import { Route, routes } from '@/data/routes';
+import { SignedOut } from '@clerk/nextjs';
+import { Fragment } from 'react';
+import RouteLink from './components/RouteLink';
+
 const Page = () => {
   return (
     <div>
-      <h1>Page</h1>
+      <Heading className='mb-3'>Übersicht</Heading>
+      <p className='mb-2 text-center text-lg font-bold'>Latein Lernen</p>
+      <div className='grid grid-cols-4 items-center gap-y-2'>
+        {routes
+          .filter((route) => !!route.children)
+          .map((route) => (
+            <Fragment key={route.label}>
+              <div>{route.label}:</div>
+              <div className='col-span-3 flex items-center justify-between gap-2'>
+                {route.children
+                  .map((childRoute) => ({ ...childRoute, href: route.href + childRoute.href }))
+                  .map((route, i) => {
+                    return <RouteLink key={i} route={route} />;
+                  })}
+              </div>
+            </Fragment>
+          ))}
+      </div>
+      <p className='mb-2 mt-10 text-center text-lg font-bold'>Sonstiges</p>
+      <div className='col-span-3 flex items-center justify-between gap-2'>
+        <RouteLink route={routes.find((route) => route.label === 'Kompetenz') as Route} />
+        <SignedOut>
+          <RouteLink route={{ href: '/sign-in', label: 'Anmelden' }} />
+        </SignedOut>
+        {/* <RouteLink route={{ href: '/premium', label: 'Premium' }} /> */}
+      </div>
     </div>
   );
 };
