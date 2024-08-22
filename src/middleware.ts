5f59580a31d836e66a327dc05b0c2a94180c517d
neinja007 <anton.siligan@gmail.com>
2024-08-22 11:07:47 +0200

    fix clerk middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,8 @@
 import { clerkMiddleware } from '@clerk/nextjs/server';
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 
-const middleware = (request: NextRequest) => {
+export default clerkMiddleware((auth, request) => {
   if (request.nextUrl.pathname === '/') {
     return NextResponse.redirect(new URL('/dashboard', request.url));
   }
-
-  clerkMiddleware();
-};
-
-export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)']
-};
-
-export default middleware;
+});
