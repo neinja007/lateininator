4361118fefdbdcdff219373b4ccbfe26c5e6515a
neinja007 <anton.siligan@gmail.com>
2024-08-22 10:06:31 +0200

    add footer for future editing

--- src/app/(general)/layout.tsx
+++ src/app/(general)/layout.tsx
@@ -7,7 +7,14 @@ type MainLayoutProps = {
 const MainLayout = ({ children }: MainLayoutProps) => {
   return (
     <div>
-      <div className='container mx-auto max-w-[1024px] pb-16'>{children}</div>
+      <div className='container mx-auto max-w-[1024px] pb-16'>
+        {children}
+        {/* <Hr className='my-5' />
+        <div className='grid grid-cols-5'>
+          <span className='col-span-2'>&copy; 2024 Anton Siligan</span>
+          <div className='text-center text-red-500 underline'>Fehler melden</div>
+        </div> */}
+      </div>
     </div>
   );
 };
