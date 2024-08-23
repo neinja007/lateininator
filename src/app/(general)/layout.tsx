67c87e06b3b41cf40bb3c45dbd4a5c3f94632f72
neinja007 <anton.siligan@gmail.com>
2024-08-23 09:07:26 +0200

    add footer

--- src/app/(general)/layout.tsx
+++ src/app/(general)/layout.tsx
@@ -1,4 +1,6 @@
 import '@/app/globals.css';
+import Hr from '@/components/Hr';
+import Link from 'next/link';
 
 type MainLayoutProps = {
   children: React.ReactNode;
@@ -9,11 +11,16 @@ const MainLayout = ({ children }: MainLayoutProps) => {
     <div>
       <div className='container mx-auto max-w-[1024px] pb-16'>
         {children}
-        {/* <Hr className='my-5' />
-        <div className='grid grid-cols-5'>
-          <span className='col-span-2'>&copy; 2024 Anton Siligan</span>
-          <div className='text-center text-red-500 underline'>Fehler melden</div>
-        </div> */}
+        <Hr className='my-5' />
+        <div className='flex justify-between'>
+          <span>&copy; 2024 Anton Siligan</span>
+          <div>
+            Fehler, Vorschläge, oder Feedback:{' '}
+            <Link href={'mailto:support@lateininator.com'} className='text-blue-500 hover:underline'>
+              support@lateininator.com
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
