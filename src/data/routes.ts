ddfd131359166f6d53ec9dae39ba7ca455afb3d9
neinja007 <anton.siligan@gmail.com>
2024-08-23 09:05:18 +0200

    add icons to routes

--- src/data/routes.ts
+++ src/data/routes.ts
@@ -1,52 +1,65 @@
 import { Color } from '@/types/other';
+import {
+  BookCheck,
+  BookMarked,
+  BookOpenText,
+  CircleDot,
+  Crosshair,
+  LayoutDashboard,
+  Newspaper,
+  PencilLine,
+  Replace,
+  SpellCheck,
+  X
+} from 'lucide-react';
 
 export type Route = {
   label: string;
   href: string;
+  icon: typeof X;
   color?: Color;
-  children?: never;
+  children?: Route[];
 };
 
-export type Dropdown = {
-  label: string;
-  href: string;
-  children: Route[];
-};
-
-export const routes: (Route | Dropdown)[] = [
-  { label: 'Übersicht', href: '/dashboard' },
+export const routes: Route[] = [
+  { label: 'Übersicht', href: '/dashboard', icon: LayoutDashboard },
   {
     label: 'Vokabular',
     href: '/vocabulary',
+    icon: BookMarked,
     children: [
       {
         label: 'Trainer',
         href: '/trainer',
-        color: 'yellow'
+        color: 'yellow',
+        icon: BookCheck
       },
-      { label: 'Wörterbuch', href: '/dictionary', color: 'purple' }
+      { label: 'Wörterbuch', href: '/dictionary', color: 'purple', icon: BookOpenText }
     ]
   },
   {
     label: 'Flexion',
     href: '/flexion',
+    icon: Replace,
     children: [
-      { label: 'Nomen', href: '/noun', color: 'blue' },
-      { label: 'Verben', href: '/verb', color: 'red' },
-      { label: 'Adjektive', href: '/adjective', color: 'green' }
+      { label: 'Nomen', href: '/noun', color: 'blue', icon: CircleDot },
+      { label: 'Verben', href: '/verb', color: 'red', icon: CircleDot },
+      { label: 'Adjektive', href: '/adjective', color: 'green', icon: CircleDot }
     ]
   },
   {
     label: 'Grammatik',
     href: '/grammar',
+    icon: SpellCheck,
     children: [
-      { label: 'Zusammenfassungen', href: '/summaries', color: 'purple' },
+      { label: 'Zusammenfassungen', href: '/summaries', color: 'purple', icon: Newspaper },
       {
         label: 'Übungen',
         href: '/exercises',
-        color: 'yellow'
+        color: 'yellow',
+        icon: PencilLine
       }
     ]
   },
-  { label: 'Kompetenz', href: '/competence', color: 'orange' }
+  { label: 'Kompetenz', href: '/competence', color: 'orange', icon: Crosshair }
 ];
