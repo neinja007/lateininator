a022ef433b46fe9b9d6c4dd430dda8c33d7c75ed
neinja007 <anton.siligan@gmail.com>
2024-08-23 17:38:48 +0200

    add responsiveness to word information

--- src/app/(general)/vocabulary/dictionary/[id]/components/WordInformation.tsx
+++ src/app/(general)/vocabulary/dictionary/[id]/components/WordInformation.tsx
@@ -8,12 +8,12 @@ type WordInformationProps = { word: Word };
 
 const WordInformation = ({ word }: WordInformationProps) => {
   return (
-    <div className='grid grid-cols-3'>
+    <div className='grid space-y-3 md:grid-cols-3 md:space-y-0'>
       <p>
         {word.translation.length === 0 ? null : word.translation.length === 1 ? 'Übersetzung:' : 'Übersetzungen:'}{' '}
         <b>{word.translation.length > 0 ? word.translation.join(', ') : 'Keine Übersetzung'}</b>
       </p>
-      <p className='text-center'>
+      <p className='md:text-center'>
         {word.derivative && (
           <span>
             Abwandlung von{' '}
