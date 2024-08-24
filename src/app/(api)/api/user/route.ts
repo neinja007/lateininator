98cf090844f402775618ce414facee4238e5f383
neinja007 <anton.siligan@gmail.com>
2024-08-24 09:42:25 +0200

    add user creation route (POST)

---
+++ src/app/(api)/api/user/route.ts
@@ -0,0 +1,41 @@
+import { prisma } from '@/utils/other/client';
+import { currentUser } from '@clerk/nextjs/server';
+import { NextRequest, NextResponse } from 'next/server';
+
+export const POST = async (req: NextRequest) => {
+  const user = await currentUser();
+  if (!user) {
+    console.error('Unauthorized user');
+    return NextResponse.json({ status: 401, body: { message: 'Unauthorized' } });
+  }
+
+  try {
+    const emailAdress = user.primaryEmailAddress?.emailAddress || user.emailAddresses[0].emailAddress;
+    const username = user.username || user.fullName || user.firstName + ' ' + user.lastName;
+
+    if (!emailAdress || !username) {
+      console.error('Email or username not found');
+      return NextResponse.json({ status: 400, body: { message: 'Email or username not found' } });
+    }
+
+    const userExists = await prisma.user.findUnique({
+      where: { email: emailAdress }
+    });
+
+    if (userExists) {
+      return NextResponse.json(userExists, { status: 200 });
+    }
+
+    const newUser = await prisma.user.create({
+      data: {
+        email: emailAdress,
+        name: username
+      }
+    });
+
+    return NextResponse.json(newUser, { status: 201 });
+  } catch (error: any) {
+    console.error(error);
+    return NextResponse.json({ status: 500, body: { message: error } });
+  }
+};
