e54c7c90ea043514159130f87f1e61ef1eb91755
neinja007 <anton.siligan@gmail.com>
2024-08-24 10:33:07 +0200

    add pink to constatns

--- src/types/other.ts
+++ src/types/other.ts
@@ -1,6 +1,6 @@
 export type List = { id: number; name: string; words: number[] };
 
-export type Color = 'gray' | 'red' | 'orange' | 'yellow' | 'green' | 'blue' | 'purple' | 'default';
+export type Color = 'gray' | 'red' | 'orange' | 'yellow' | 'green' | 'blue' | 'purple' | 'default' | 'pink';
 
 export type Stage = 'settings' | 'test' | 'review' | 'results';
 
