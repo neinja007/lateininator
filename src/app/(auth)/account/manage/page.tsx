0004c2b4bb6ca8ff12c20eb487c8ae2926a31d03
neinja007 <anton.siligan@gmail.com>
2024-08-24 08:17:15 +0200

    add basic management page with signout button

---
+++ src/app/(auth)/account/manage/page.tsx
@@ -0,0 +1,12 @@
+import { SignOutButton } from '@clerk/nextjs';
+
+const Page = () => {
+  return (
+    <div>
+      {' '}
+      <SignOutButton>Abmelden</SignOutButton>{' '}
+    </div>
+  );
+};
+
+export default Page;
