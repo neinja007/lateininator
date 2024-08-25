1d891cf28e1d08f715385b7244a3cea731ae3d78
neinja007 <anton.siligan@gmail.com>
2024-08-25 07:53:50 +0200

    add successful payment page

---
+++ src/app/(general)/premium/success/page.tsx
@@ -0,0 +1,14 @@
+import Heading from '@/components/Heading';
+
+const Page = () => {
+  return (
+    <div>
+      <Heading>Lateininator Premium</Heading>
+      <p className='mt-5 text-center'>
+        Vielen Dank für deinen Einkauf! Du kannst von nun an alle Premium-Funktionen nutzen.
+      </p>
+    </div>
+  );
+};
+
+export default Page;
