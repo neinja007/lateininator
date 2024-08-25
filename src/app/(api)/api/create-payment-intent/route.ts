0c22708c5b18bbf1808ea2772f4320afd91022cd
neinja007 <anton.siligan@gmail.com>
2024-08-25 07:53:19 +0200

    add payment intent route for stripe

---
+++ src/app/(api)/api/create-payment-intent/route.ts
@@ -0,0 +1,29 @@
+import { monthlyPrice } from '@/constants/other';
+import { NextRequest, NextResponse } from 'next/server';
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+export async function POST(request: NextRequest) {
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: monthlyPrice * 100,
+      currency: 'eur',
+      automatic_payment_methods: { enabled: true },
+      setup_future_usage: 'off_session'
+    });
+
+    return NextResponse.json(
+      {
+        clientSecret: paymentIntent.client_secret
+      },
+      { status: 200 }
+    );
+  } catch (error: any) {
+    console.error(error);
+    return NextResponse.json(
+      {
+        error: error.message
+      },
+      { status: 500 }
+    );
+  }
+}
