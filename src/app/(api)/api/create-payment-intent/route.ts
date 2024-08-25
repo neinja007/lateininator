b4e26b454c0ae83a25e1f094b803886c21b2c98c
neinja007 <anton.siligan@gmail.com>
2024-08-25 11:30:37 +0200

    pass userId as metadata

--- src/app/(api)/api/create-payment-intent/route.ts
+++ src/app/(api)/api/create-payment-intent/route.ts
@@ -1,14 +1,23 @@
 import { monthlyPrice } from '@/constants/other';
+import { currentUser } from '@clerk/nextjs/server';
 import { NextResponse } from 'next/server';
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 export async function POST() {
+  const user = await currentUser();
+  if (!user) {
+    return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+  }
+
   try {
     const paymentIntent = await stripe.paymentIntents.create({
       amount: monthlyPrice * 100,
       currency: 'eur',
       automatic_payment_methods: { enabled: true },
-      setup_future_usage: 'off_session'
+      setup_future_usage: 'off_session',
+      metadata: {
+        userId: user.id
+      }
     });
 
     return NextResponse.json(
