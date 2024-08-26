0f841fb2290c20a4d1676e8e3a0bd3618bc0e609
neinja007 <anton.siligan@gmail.com>
2024-08-26 12:36:56 +0200

    remove forSignedIn users

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,8 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { clerkMiddleware } from '@clerk/nextjs/server';
 import { NextResponse } from 'next/server';
 
 export default clerkMiddleware((auth, request) => {
-  if (auth().userId === null) {
-    if (forSignedInUsers(request)) {
-      return NextResponse.redirect(new URL('/account/sign-in', request.url));
-    }
-  }
-
   if (request.nextUrl.pathname === '/') {
     return NextResponse.redirect(new URL('/dashboard', request.url));
   }
 });
-
-const forSignedInUsers = createRouteMatcher(['/account/redirect', '/account/manage']);
