31685dbe8d5aa1f7bfce7393a510657b2168f727
neinja007 <anton.siligan@gmail.com>
2024-08-26 14:17:39 +0200

    execute resizeHandler on hook mount

--- src/hooks/useWidth.ts
+++ src/hooks/useWidth.ts
@@ -1,5 +1,5 @@
 import { BREAKPOINTS } from '@/constants/other';
-import { useEffect } from 'react';
+import { useCallback, useEffect } from 'react';
 
 export const useWidth = (
   width: 'sm' | 'md' | 'lg' | 'xl',
@@ -7,18 +7,21 @@ export const useWidth = (
   onExit?: () => void,
   max?: boolean
 ): void => {
+  const handleResize = useCallback(() => {
+    if (max ? window.innerWidth > BREAKPOINTS[width] : window.innerWidth < BREAKPOINTS[width]) {
+      onReachWidth();
+    } else {
+      onExit && onExit();
+    }
+  }, [max, onExit, onReachWidth, width]);
+
   useEffect(() => {
-    const handleResize = () => {
-      if (max ? window.innerWidth > BREAKPOINTS[width] : window.innerWidth < BREAKPOINTS[width]) {
-        onReachWidth();
-      } else {
-        onExit && onExit();
-      }
-    };
     addEventListener('resize', handleResize);
+
+    handleResize();
 
     return () => {
       removeEventListener('resize', handleResize);
     };
-  }, [width, onReachWidth, max, onExit]);
+  }, [handleResize]);
 };
