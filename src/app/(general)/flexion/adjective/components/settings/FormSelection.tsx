9cb742a395a241e44e852cfe45b9db706a35be3e
neinja007 <anton.siligan@gmail.com>
2024-08-26 13:55:49 +0200

    use formSelectionWrapper for adjective as well

--- src/app/(general)/flexion/adjective/components/settings/FormSelection.tsx
+++ src/app/(general)/flexion/adjective/components/settings/FormSelection.tsx
@@ -1,11 +1,10 @@
-import Button from '@/components/Button';
 import CheckboxList from '@/components/CheckboxList';
-import CheckboxWithLabel from '@/components/CheckboxWithLabel';
 import { WORD_CONSTANTS } from '@/constants/wordConstants';
 import { Adjective } from '@/types/word';
 import { Comparison, ComparisonDegree, Gender } from '@/types/wordConstants';
 import { MAPPER } from '@/utils/other/mapper';
 import { Dispatch, SetStateAction } from 'react';
+import FormSelectionWrapper from '../../../components/FormSelectionWrapper';
 
 type FormSelectionProps = {
   checkAdverb: boolean;
@@ -35,75 +34,58 @@ const FormSelection = ({
   });
 
   return (
-    <>
-      <div className='grid grid-cols-3'>
-        <p>Wähle aus, was abgefragt werden soll:</p>
-        <CheckboxWithLabel
-          checked={checkAdverb}
-          handleChange={() => setCheckAdverb((prev) => !prev)}
-          label={'Adverbien'}
-        />
-        <div className='grid grid-cols-2 gap-x-4'>
-          <Button
-            color={
-              comparisons.length === WORD_CONSTANTS.comparison.length &&
-              comparisonDegrees.length === WORD_CONSTANTS.comparisonDegree.length &&
-              genders.length === WORD_CONSTANTS.gender.length &&
-              checkAdverb
-                ? 'blue'
-                : 'default'
-            }
-            onClick={() => {
-              setComparisons([...WORD_CONSTANTS.comparison]);
-              setComparisonDegrees([...WORD_CONSTANTS.comparisonDegree]);
-              setGenders([...WORD_CONSTANTS.gender]);
-              setCheckAdverb(true);
-            }}
-          >
-            Alle auswählen
-          </Button>
-          <Button
-            color={
-              comparisons.length === 0 && comparisonDegrees.length === 0 && genders.length === 0 && !checkAdverb
-                ? 'blue'
-                : 'default'
-            }
-            onClick={() => {
-              setComparisons([]);
-              setComparisonDegrees([]);
-              setGenders([]);
-              setCheckAdverb(false);
-            }}
-          >
-            Alle abwählen
-          </Button>
-        </div>
-      </div>
-      <div className='grid grid-cols-3'>
-        <CheckboxList
-          options={[...WORD_CONSTANTS.comparison]}
-          disabledOptions={comparisonsNotToCheck}
-          selected={comparisons}
-          setSelected={setComparisons}
-          label='Komparation'
-          mapper={MAPPER.extended.comparison}
-        />
-        <CheckboxList
-          options={[...WORD_CONSTANTS.comparisonDegree]}
-          selected={comparisonDegrees}
-          setSelected={setComparisonDegrees}
-          label='Steigerungsform'
-          mapper={MAPPER.extended.comparisonDegree}
-        />
-        <CheckboxList
-          options={[...WORD_CONSTANTS.gender]}
-          selected={genders}
-          setSelected={setGenders}
-          label='Geschlecht'
-          mapper={MAPPER.extended.gender}
-        />
-      </div>
-    </>
+    <FormSelectionWrapper
+      titleOption={{
+        checked: checkAdverb,
+        handleChange: () => setCheckAdverb((prev) => !prev),
+        disabled: false,
+        label: 'Adverbien'
+      }}
+      selectAllActive={
+        comparisons.length === WORD_CONSTANTS.comparison.length &&
+        comparisonDegrees.length === WORD_CONSTANTS.comparisonDegree.length &&
+        genders.length === WORD_CONSTANTS.gender.length &&
+        checkAdverb
+      }
+      selectAll={() => {
+        setComparisons([...WORD_CONSTANTS.comparison]);
+        setComparisonDegrees([...WORD_CONSTANTS.comparisonDegree]);
+        setGenders([...WORD_CONSTANTS.gender]);
+        setCheckAdverb(true);
+      }}
+      selectNoneActive={
+        comparisons.length === 0 && comparisonDegrees.length === 0 && genders.length === 0 && !checkAdverb
+      }
+      selectNone={() => {
+        setComparisons([]);
+        setComparisonDegrees([]);
+        setGenders([]);
+        setCheckAdverb(false);
+      }}
+    >
+      <CheckboxList
+        options={[...WORD_CONSTANTS.comparison]}
+        disabledOptions={comparisonsNotToCheck}
+        selected={comparisons}
+        setSelected={setComparisons}
+        label='Komparation'
+        mapper={MAPPER.extended.comparison}
+      />
+      <CheckboxList
+        options={[...WORD_CONSTANTS.comparisonDegree]}
+        selected={comparisonDegrees}
+        setSelected={setComparisonDegrees}
+        label='Steigerungsform'
+        mapper={MAPPER.extended.comparisonDegree}
+      />
+      <CheckboxList
+        options={[...WORD_CONSTANTS.gender]}
+        selected={genders}
+        setSelected={setGenders}
+        label='Geschlecht'
+        mapper={MAPPER.extended.gender}
+      />
+    </FormSelectionWrapper>
   );
 };
 
