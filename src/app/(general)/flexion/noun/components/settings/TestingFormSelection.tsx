d97c209167c2723daf86fb606a672a6323312c0f
neinja007 <anton.siligan@gmail.com>
2024-08-26 13:52:32 +0200

    use formSelection component for noun flexion settings

--- src/app/(general)/flexion/noun/components/settings/TestingFormSelection.tsx
+++ src/app/(general)/flexion/noun/components/settings/TestingFormSelection.tsx
@@ -1,10 +1,10 @@
-import Button from '@/components/Button';
 import CheckboxList from '@/components/CheckboxList';
 import { WORD_CONSTANTS } from '@/constants/wordConstants';
 import { Noun } from '@/types/word';
 import { Gender, Declension } from '@/types/wordConstants';
 import { MAPPER } from '@/utils/other/mapper';
 import { Dispatch, SetStateAction } from 'react';
+import FormSelection from '../../../components/FormSelection';
 
 type TestingFormSelectionProps = {
   genders: Gender[];
@@ -28,53 +28,38 @@ const TestingFormSelection = ({
   const gendersNotToCheck = genders.filter((gender) => !validWords.some((word) => word.gender === gender));
 
   return (
-    <>
-      <div className='grid grid-cols-3'>
-        <p className='col-span-2'>Wähle aus, was abgefragt werden soll:</p>
-        <div className='grid grid-cols-2 gap-x-4'>
-          <Button
-            color={
-              declensions.length === WORD_CONSTANTS.declension.length && genders.length === WORD_CONSTANTS.gender.length
-                ? 'blue'
-                : 'default'
-            }
-            onClick={() => {
-              setDeclensions([...WORD_CONSTANTS.declension]);
-              setGenders([...WORD_CONSTANTS.gender]);
-            }}
-          >
-            Alle auswählen
-          </Button>
-          <Button
-            color={declensions.length === 0 && genders.length === 0 ? 'blue' : 'default'}
-            onClick={() => {
-              setDeclensions([]);
-              setGenders([]);
-            }}
-          >
-            Alle abwählen
-          </Button>
-        </div>
-      </div>
-      <div className='grid grid-cols-2'>
-        <CheckboxList
-          options={[...WORD_CONSTANTS.declension]}
-          disabledOptions={declensionsNotToCheck}
-          selected={declensions}
-          setSelected={setDeclensions}
-          label='Deklination'
-          mapper={MAPPER.extended.declension}
-        />
-        <CheckboxList
-          options={[...WORD_CONSTANTS.gender]}
-          disabledOptions={gendersNotToCheck}
-          selected={genders}
-          setSelected={setGenders}
-          label='Geschlecht'
-          mapper={MAPPER.extended.gender}
-        />
-      </div>
-    </>
+    <FormSelection
+      selectAll={() => {
+        setDeclensions([...WORD_CONSTANTS.declension]);
+        setGenders([...WORD_CONSTANTS.gender]);
+      }}
+      selectAllActive={
+        declensions.length === WORD_CONSTANTS.declension.length && genders.length === WORD_CONSTANTS.gender.length
+      }
+      selectNone={() => {
+        setDeclensions([]);
+        setGenders([]);
+      }}
+      selectNoneActive={declensions.length === 0 && genders.length === 0}
+      columns={2}
+    >
+      <CheckboxList
+        options={[...WORD_CONSTANTS.declension]}
+        disabledOptions={declensionsNotToCheck}
+        selected={declensions}
+        setSelected={setDeclensions}
+        label='Deklination'
+        mapper={MAPPER.extended.declension}
+      />
+      <CheckboxList
+        options={[...WORD_CONSTANTS.gender]}
+        disabledOptions={gendersNotToCheck}
+        selected={genders}
+        setSelected={setGenders}
+        label='Geschlecht'
+        mapper={MAPPER.extended.gender}
+      />
+    </FormSelection>
   );
 };
 
