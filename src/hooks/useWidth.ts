a9fb498ff4e239fad477ca0a49aa1d46e42cc608
neinja007 <anton.siligan@gmail.com>
2024-08-26 14:21:37 +0200

    allow undefined breakpoints

--- src/hooks/useWidth.ts
+++ src/hooks/useWidth.ts
@@ -1,17 +1,18 @@
 import { BREAKPOINTS } from '@/constants/other';
+import { Breakpoint } from '@/types/other';
 import { useCallback, useEffect } from 'react';
 
 export const useWidth = (
-  width: 'sm' | 'md' | 'lg' | 'xl',
+  width: Breakpoint | undefined,
   onReachWidth: () => void,
   onExit?: () => void,
   max?: boolean
 ): void => {
   const handleResize = useCallback(() => {
-    if (max ? window.innerWidth > BREAKPOINTS[width] : window.innerWidth < BREAKPOINTS[width]) {
+    if (width && (max ? window.innerWidth > BREAKPOINTS[width] : window.innerWidth < BREAKPOINTS[width])) {
       onReachWidth();
     } else {
-      onExit && onExit();
+      width && onExit && onExit();
     }
   }, [max, onExit, onReachWidth, width]);
 
