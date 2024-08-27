4d6b38fa50370468d2c7a36ce2fc963b5bebcae2
neinja007 <anton.siligan@gmail.com>
2024-08-27 10:08:07 +0200

    add not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,23 @@
+import Heading from '@/components/Heading';
+import LinkToSupportEmail from '@/components/LinkToSupportEmail';
+import Link from 'next/link';
+
+const Page = () => {
+  return (
+    <div>
+      <Heading>Diese Seite wurde nicht gefunden (Error 404).</Heading>
+      <p>
+        Wenn Sie glauben, dass dies ein Fehler ist, bitten wir Sie, unseren <LinkToSupportEmail /> zu kontaktieren.
+      </p>
+      <p className='mt-2'>
+        Ansonsten können Sie{' '}
+        <Link className='text-blue-500 hover:underline' href={'/dashboard'}>
+          hier zurück zur Homepage gelangen
+        </Link>
+        .
+      </p>
+    </div>
+  );
+};
+
+export default Page;
