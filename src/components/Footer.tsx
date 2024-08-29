e59182821e51d29e77fd7dd6053cdf756d89666f
neinja007 <anton.siligan@gmail.com>
2024-08-29 15:50:35 +0200

    edit copyright notice

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import LinkToSupportEmail from './LinkToSupportEmail';
 const Footer = () => {
   return (
     <div className='justify-between text-center md:flex'>
-      <span>&copy; 2024 Anton Siligan</span>
+      <span>&copy; 2024 Lateininator</span>
       <div className='mt-5 md:mt-0'>
         Fehler, Vorschläge, oder Feedback: <LinkToSupportEmail>support@lateininator.com</LinkToSupportEmail>
       </div>
