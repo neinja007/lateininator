efd8192cf6a0fc3c526c40a7302d6fe807c75d6f
neinja007 <anton.siligan@gmail.com>
2024-08-30 07:54:26 +0200

    create util function for wrapping auth status dependent elements

---
+++ src/utils/other/makeAuthStateDependent.tsx
@@ -0,0 +1,13 @@
+import { SignedIn, SignedOut } from '@clerk/nextjs';
+
+export const makeAuthStateDependent = (element: React.ReactNode, condition: 'signedIn' | 'signedOut' | undefined) => {
+  if (condition === 'signedIn') {
+    return <SignedIn>element</SignedIn>;
+  }
+
+  if (condition === 'signedOut') {
+    return <SignedOut>element</SignedOut>;
+  }
+
+  return element;
+};
