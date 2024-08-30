b4379a4754a67d0d34e32c57bf1eb39de4ceedd6
neinja007 <anton.siligan@gmail.com>
2024-08-30 08:37:32 +0200

    add basic settings page

---
+++ src/app/(general)/user/settings/page.tsx
@@ -0,0 +1,13 @@
+import Heading from '@/components/Heading';
+import Hr from '@/components/Hr';
+
+const Page = () => {
+  return (
+    <div>
+      <Heading>Einstellungen</Heading>
+      <Hr />
+    </div>
+  );
+};
+
+export default Page;
