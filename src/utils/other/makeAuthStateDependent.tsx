d7ddd418c70c7500daf54aef8831d63228b1c107
neinja007 <anton.siligan@gmail.com>
2024-08-30 08:14:17 +0200

    fix child element rendering

--- src/utils/other/makeAuthStateDependent.tsx
+++ src/utils/other/makeAuthStateDependent.tsx
@@ -1,12 +1,15 @@
 import { SignedIn, SignedOut } from '@clerk/nextjs';
 
-export const makeAuthStateDependent = (element: React.ReactNode, condition: 'signedIn' | 'signedOut' | undefined) => {
+export const makeAuthStateDependent = (
+  element: React.ReactNode,
+  condition: 'signedIn' | 'signedOut' | undefined
+): React.ReactNode => {
   if (condition === 'signedIn') {
-    return <SignedIn>element</SignedIn>;
+    return <SignedIn>{element}</SignedIn>;
   }
 
   if (condition === 'signedOut') {
-    return <SignedOut>element</SignedOut>;
+    return <SignedOut>{element}</SignedOut>;
   }
 
   return element;
