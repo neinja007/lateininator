0dd8f0994655c8de6b5b032014f37ccffa03bf43
neinja007 <anton.siligan@gmail.com>
2024-08-30 08:14:31 +0200

    adjust dashboard for new routes

--- src/app/(general)/dashboard/page.tsx
+++ src/app/(general)/dashboard/page.tsx
@@ -1,20 +1,20 @@
 import Heading from '@/components/Heading';
-import { Route, routes } from '@/data/routes';
-import { SignedIn, SignedOut } from '@clerk/nextjs';
+import { routes } from '@/data/routes';
 import RouteLink from './components/RouteLink';
-import { LogIn, User } from 'lucide-react';
+import { makeAuthStateDependent } from '@/utils/other/makeAuthStateDependent';
+import { Fragment } from 'react';
 
 const Page = () => {
   return (
     <div>
       <Heading className='mb-3 md:mb-6 lg:mb-12'>Übersicht</Heading>
-      <p className='mb-2 text-center text-lg font-bold'>Latein Lernen</p>
+      <p className='mb-2 text-center text-lg font-bold'>Lernen</p>
       {routes
         .filter((route) => !!route.children && !route.authStatus)
         .map((route) => (
           <div key={route.label} className='mb-5 grid-cols-4 items-center md:mb-2 md:grid'>
             <div className='flex items-center gap-x-2 font-bold text-gray-600 dark:text-gray-400'>
-              <route.icon className='w-5' /> {route.label}
+              <route.icon className='w-5' /> {route.label.replace('{name}', 'Profil')}
             </div>
             <div className='col-span-3 items-center justify-between gap-x-2 sm:flex'>
               {route.children &&
@@ -26,16 +26,18 @@ const Page = () => {
             </div>
           </div>
         ))}
-      <p className='mb-2 mt-10 text-center text-lg font-bold'>Sonstiges</p>
+      <p className='mb-2 mt-10 text-center text-lg font-bold'>Verwaltung</p>
       <div className='col-span-3 items-center justify-between gap-2 sm:flex'>
-        <RouteLink route={routes.find((route) => route.label === 'Kompetenz') as Route} />
-        <SignedOut>
-          <RouteLink route={{ href: '/account/sign-in', label: 'Anmelden', icon: LogIn, color: 'green' }} />
-        </SignedOut>
-        <SignedIn>
-          <RouteLink route={{ href: '/account/manage', label: 'Konto Verwalten', icon: User, color: 'green' }} />
-        </SignedIn>
-        <RouteLink route={routes.find((route) => route.label === 'Premium') as Route} />
+        {routes
+          .find((route) => route.label === '{name}')
+          ?.children?.map((child, i) => (
+            <Fragment key={i}>
+              {makeAuthStateDependent(
+                <RouteLink route={{ ...child, href: '/account' + child.href }} />,
+                child.authStatus
+              )}
+            </Fragment>
+          ))}
       </div>
     </div>
   );
