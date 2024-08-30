744819736e6ab1897a3c5e55126851851a894801
neinja007 <anton.siligan@gmail.com>
2024-08-30 08:57:03 +0200

    add basic statistics page

---
+++ src/app/(general)/user/statistics/page.tsx
@@ -0,0 +1,12 @@
+import Heading from '@/components/Heading';
+
+const Page = () => {
+  return (
+    <div>
+      <Heading>Wortschatz</Heading>
+      Dieses Feature ist noch nicht verfügbar.
+    </div>
+  );
+};
+
+export default Page;
