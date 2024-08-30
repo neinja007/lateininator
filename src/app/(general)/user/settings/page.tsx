11aa051ac823fc656dc18530024a30b61cfc6676
neinja007 <anton.siligan@gmail.com>
2024-08-30 08:44:17 +0200

    add links and placeholder text

--- src/app/(general)/user/settings/page.tsx
+++ src/app/(general)/user/settings/page.tsx
@@ -1,11 +1,31 @@
+import FancyLink from '@/components/FancyLink';
 import Heading from '@/components/Heading';
-import Hr from '@/components/Hr';
+import { LogOut, UserIcon } from 'lucide-react';
 
 const Page = () => {
   return (
     <div>
       <Heading>Einstellungen</Heading>
-      <Hr />
+      <div className='h-44'>Bis jetzt gibt es noch keine Einstellungen.</div>
+      <p className='mb-2 text-center text-lg font-bold'>Weitere Einstellungen</p>
+      <div className='items-center justify-between gap-x-2 sm:flex'>
+        <FancyLink
+          route={{
+            label: 'Konto Verwalten',
+            href: '/auth/profile',
+            icon: UserIcon,
+            color: 'blue'
+          }}
+        />
+        <FancyLink
+          route={{
+            label: 'Abmelden',
+            href: '/auth/sign-out',
+            icon: LogOut,
+            color: 'red'
+          }}
+        />
+      </div>
     </div>
   );
 };
