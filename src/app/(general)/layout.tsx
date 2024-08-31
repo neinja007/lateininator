ab7fa894635bd4567dfc528c2aa84907c447ec7a
neinja007 <anton.siligan@gmail.com>
2024-08-31 12:37:47 +0200

    initialize react query for 'general' route group

--- src/app/(general)/layout.tsx
+++ src/app/(general)/layout.tsx
@@ -1,5 +1,11 @@
+'use client';
+
 import Footer from '@/components/Footer';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Hr from '@/components/Hr';
+
+const queryClient = new QueryClient();
 
 type LayoutProps = {
   children: React.ReactNode;
@@ -7,11 +13,12 @@ type LayoutProps = {
 
 const Layout = ({ children }: LayoutProps) => {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       {children}
       <Hr className='my-4' />
       <Footer />
-    </>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
 
