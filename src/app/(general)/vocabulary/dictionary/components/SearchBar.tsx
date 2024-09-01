7cb299458c36ea635e2499ac8752a941364daeb2
neinja007 <anton.siligan@gmail.com>
2024-09-01 10:08:35 +0200

    add search button

--- src/app/(general)/vocabulary/dictionary/components/SearchBar.tsx
+++ src/app/(general)/vocabulary/dictionary/components/SearchBar.tsx
@@ -1,20 +1,27 @@
+import Button from '@/components/Button';
 import Input from '@/components/Input';
 
 type SearchBarProps = {
   query: string;
   setQuery: (query: string) => void;
+  onSearch?: () => void;
 };
 
-const SearchBar = ({ query, setQuery }: SearchBarProps) => {
+const SearchBar = ({ query, setQuery, onSearch }: SearchBarProps) => {
   return (
-    <div>
-      <Input
-        label='Suche'
-        placeholder='Wort oder Übersetzung eingeben'
-        className='mr-4 w-full'
-        value={query}
-        onChange={setQuery}
-      />
+    <div className='flex items-end justify-between'>
+      <div className='flex-grow'>
+        <Input
+          label='Suche'
+          placeholder='Wort oder Übersetzung eingeben'
+          className='mr-4 w-full'
+          value={query}
+          onChange={setQuery}
+        />
+      </div>
+      <Button className={'ml-4'} onClick={onSearch}>
+        Suchen
+      </Button>
     </div>
   );
 };
