c57642069708c12c833bf9c57900b84730d476ab
neinja007 <anton.siligan@gmail.com>
2024-09-01 08:34:14 +0200

    add adjective lexical form

--- src/utils/word/getLexicalForm.ts
+++ src/utils/word/getLexicalForm.ts
@@ -2,6 +2,7 @@ import { endings } from '@/constants/endings';
 import { Word } from '@/types/word';
 import { isNoun } from '../typeguards/isNoun';
 import { isVerb } from '../typeguards/isVerb';
+import { isAdjective } from '../typeguards/isAdjective';
 
 export const getLexicalForm = (word: Word) => {
   if (isNoun(word)) {
@@ -11,5 +12,8 @@ export const getLexicalForm = (word: Word) => {
   } else if (isVerb(word)) {
     if (word.verb.conjugation === 'NONE') return;
     return `-${endings.verb[word.verb.conjugation].ind.act.pres.sin[1]}, ${word.verb.conjugation}.`;
+  } else if (isAdjective(word)) {
+    if (word.adjective.comparison === 'NONE') return;
+    return `-${endings.adjective[word.adjective.comparison].M.pos.sin[2]}, ${word.adjective.comparison}.`;
   }
 };
