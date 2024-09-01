741eafe2f2ebcfe30d6b55845235c1d341d334df
neinja007 <anton.siligan@gmail.com>
2024-09-01 16:16:16 +0200

    support ids

--- src/app/(api)/api/words/route.ts
+++ src/app/(api)/api/words/route.ts
@@ -4,11 +4,18 @@ import { NextRequest, NextResponse } from 'next/server';
 export const GET = async (request: NextRequest) => {
   const searchParams = request.nextUrl.searchParams;
   const query = searchParams.get('query');
+  const id = searchParams.get('id');
 
-  // give me a regex that checks if a string is only alphabetical, but it also returns true if there are no characters (empty string)
+  if (query && id) {
+    return NextResponse.json({ error: 'Invalid searchParams (cannot read both id and query)' }, { status: 400 });
+  }
 
-  if (query !== null && !/^[a-zA-Z]*$/.test(query)) {
-    return NextResponse.json({ error: 'Invalid query' }, { status: 400 });
+  if (id !== null && !/^\d+$/.test(id)) {
+    return NextResponse.json({ error: 'Invalid id (must be number)' }, { status: 400 });
+  }
+
+  if (query !== null && !/^[a-zA-Z]+$/.test(query)) {
+    return NextResponse.json({ error: 'Invalid query (must be string)' }, { status: 400 });
   }
 
   if (query) {
@@ -23,11 +30,35 @@ export const GET = async (request: NextRequest) => {
         include: {
           adjective: true,
           noun: true,
-          verb: true
+          verb: true,
+          derivative: true
         }
       });
 
       return NextResponse.json(words);
+    } catch (error: any) {
+      console.error(error);
+      return NextResponse.json({ error: error.message }, { status: 500 });
+    }
+  } else if (id) {
+    try {
+      const word = await prisma.word.findUnique({
+        where: {
+          id: parseInt(id)
+        },
+        include: {
+          adjective: true,
+          noun: true,
+          verb: true,
+          derivative: true
+        }
+      });
+
+      if (!word) {
+        return NextResponse.json({ error: 'Word not found' }, { status: 404 });
+      }
+
+      return NextResponse.json(word);
     } catch (error: any) {
       console.error(error);
       return NextResponse.json({ error: error.message }, { status: 500 });
@@ -38,7 +69,8 @@ export const GET = async (request: NextRequest) => {
         include: {
           adjective: true,
           noun: true,
-          verb: true
+          verb: true,
+          derivative: true
         }
       });
 
