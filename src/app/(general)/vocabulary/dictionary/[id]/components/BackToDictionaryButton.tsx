f9d9433ed778c2507fd99a89b94a7c04c5b94e4b
neinja007 <anton.siligan@gmail.com>
2024-09-01 16:32:17 +0200

    extract back to dictionary button component

---
+++ src/app/(general)/vocabulary/dictionary/[id]/components/BackToDictionaryButton.tsx
@@ -0,0 +1,20 @@
+import clsx from 'clsx';
+import ui from '@/styles/ui.module.css';
+import { ArrowLeft } from 'lucide-react';
+import Link from 'next/link';
+
+const BackToDictionaryButton = () => {
+  return (
+    <Link
+      className={clsx(
+        ui.basic,
+        'hover w-fit bg-gray-100 text-center transition-colors hover:bg-gray-200 md:!pl-2 dark:bg-gray-900 hover:dark:bg-gray-800'
+      )}
+      href={'/vocabulary/dictionary'}
+    >
+      <ArrowLeft size={20} className='inline align-text-top' /> Zurück zum Wörterbuch
+    </Link>
+  );
+};
+
+export default BackToDictionaryButton;
--- src/app/(general)/vocabulary/dictionary/[id]/components/Header.tsx
+++ src/app/(general)/vocabulary/dictionary/[id]/components/Header.tsx
@@ -1,26 +1,15 @@
 import Heading from '@/components/Heading';
 import Badge from '@/components/Badge';
-import Link from 'next/link';
-import ui from '@/styles/ui.module.css';
-import { clsx } from 'clsx';
 import { Word } from '@/types/word';
 import { getLexicalForm } from '@/utils/word/getLexicalForm';
-import { ArrowLeft } from 'lucide-react';
+import BackToDictionaryButton from './BackToDictionaryButton';
 
 type HeaderProps = { word: Word };
 
 const Header = ({ word }: HeaderProps) => {
   return (
     <div className='min-h-12 grid-cols-2 justify-center sm:flex sm:gap-4 md:grid lg:grid-cols-3'>
-      <Link
-        className={clsx(
-          ui.basic,
-          'hover w-fit bg-gray-100 text-center transition-colors hover:bg-gray-200 md:!pl-2 dark:bg-gray-900 hover:dark:bg-gray-800'
-        )}
-        href={'/vocabulary/dictionary'}
-      >
-        <ArrowLeft size={20} className='inline align-text-top' /> Zurück zum Wörterbuch
-      </Link>
+      <BackToDictionaryButton />
       <Heading>
         <span className='mt-4 flex justify-center sm:mt-0 sm:justify-end lg:justify-center'>
           {word.name} {getLexicalForm(word)}
