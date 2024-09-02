27e91c740751b4b7613a33d7320a542e4ff7c358
neinja007 <anton.siligan@gmail.com>
2024-09-02 16:31:01 +0200

    fix correctValue being undefined

--- src/app/(general)/vocabulary/trainer/components/test/PropertyInputs.tsx
+++ src/app/(general)/vocabulary/trainer/components/test/PropertyInputs.tsx
@@ -1,11 +1,11 @@
 import PropertyInput from './PropertyInput';
 import { WordProperty } from '@/types/appConstants';
-import { Word } from '@/types/word';
+import { AnyWord } from '@/types/word';
 import { Dispatch, SetStateAction } from 'react';
 
 type PropertyInputsProps = {
   wordPropertiesToCheck: WordProperty[];
-  activeWord: Word;
+  activeWord: AnyWord;
   inputValues: Record<WordProperty, string>;
   setInputValues: Dispatch<SetStateAction<Record<WordProperty, string>>>;
   stage: 'test' | 'review';
@@ -21,7 +21,22 @@ const PropertyInputs = ({
   return (
     <div className='mt-4 grid gap-4 md:grid-cols-2'>
       {wordPropertiesToCheck.map((key, i) => {
-        let correctValue = (activeWord as any)[key];
+        let correctValue: string;
+        try {
+          const keyToProperty: 'noun' | 'verb' | 'adjective' = activeWord.type.toLowerCase() as
+            | 'noun'
+            | 'verb'
+            | 'adjective';
+          const typeSpecificData: Partial<{ [key in WordProperty]: string }> = activeWord[keyToProperty];
+          const value = typeSpecificData[key as keyof typeof typeSpecificData];
+          if (value === undefined) {
+            throw new Error(`Property ${key} not found in word ${activeWord}`);
+          } else {
+            correctValue = value;
+          }
+        } catch (error) {
+          throw new Error(`Property ${key} not found in word ${activeWord}`);
+        }
 
         return (
           <PropertyInput
