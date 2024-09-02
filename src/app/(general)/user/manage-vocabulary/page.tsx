2bd2a19f3c1ca17e860f019251098180bc4a3910
neinja007 <anton.siligan@gmail.com>
2024-09-02 07:03:41 +0200

    add basic visuals and fetching

--- src/app/(general)/user/manage-vocabulary/page.tsx
+++ src/app/(general)/user/manage-vocabulary/page.tsx
@@ -1,10 +1,54 @@
+'use client';
+
 import Heading from '@/components/Heading';
+import Hr from '@/components/Hr';
+import Skeleton from '@/components/Skeleton';
+import { Collection, List } from '@prisma/client';
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+import Cell from './components/Cell';
+import CellContainer from './components/CellContainer';
 
 const Page = () => {
+  const collectionQuery = useQuery<Collection[]>({
+    queryKey: ['collections'],
+    queryFn: () => axios.get('/api/collection', { params: { saved: true } }).then((res) => res.data)
+  });
+
+  const listQuery = useQuery<List[]>({
+    queryKey: ['lists'],
+    queryFn: () => axios.get('/api/list', { params: { saved: true } }).then((res) => res.data)
+  });
+
   return (
     <div>
       <Heading>Wortschatz</Heading>
-      Dieses Feature ist noch nicht verfügbar.
+      Ihre Kollektionen (z.B. Schulbücher):
+      <div className='mt-4 h-44 min-h-44'>
+        {!collectionQuery.isFetched && <Skeleton pulse />}
+        {collectionQuery.isError && <div>Beim Laden der Kollektionen ist ein Fehler aufgetreten.</div>}
+        {collectionQuery.isSuccess && (
+          <CellContainer>
+            {collectionQuery.data.map((collection) => (
+              <Cell key={collection.id}>{collection.name}</Cell>
+            ))}
+          </CellContainer>
+        )}
+      </div>
+      <Hr className='my-5' />
+      Ihre Listen (z.B. Lektionen eines Schulbuchs):
+      <div className='mt-4 h-44 min-h-44'>
+        {!listQuery.isFetched && <Skeleton pulse />}
+        {listQuery.isError && <div>Beim Laden der Kollektionen ist ein Fehler aufgetreten.</div>}
+        {listQuery.isSuccess && (
+          <CellContainer>
+            {listQuery.data.map((list) => (
+              <Cell key={list.id}>{list.name}</Cell>
+            ))}
+          </CellContainer>
+        )}
+      </div>
+      <div></div>
     </div>
   );
 };
