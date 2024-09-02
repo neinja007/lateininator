4d183eed269f6a600e8dee02ee4071ea1304cdc6
neinja007 <anton.siligan@gmail.com>
2024-09-02 16:33:20 +0200

    make word info text white on dark mode

--- src/components/WordInfo.tsx
+++ src/components/WordInfo.tsx
@@ -2,7 +2,7 @@ type WordInfoProps = { info: string };
 
 const WordInfo = ({ info }: WordInfoProps) => {
   return (
-    <p className='float-end rounded-lg border border-gray-600 bg-gray-100 p-1 px-3 text-black dark:bg-gray-900 dark:text-gray-400'>
+    <p className='float-end rounded-lg border border-gray-600 bg-gray-100 p-1 px-3 text-black dark:bg-gray-900 dark:text-white'>
       {info}
     </p>
   );
