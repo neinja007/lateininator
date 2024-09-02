00975915a6ca6574de2827eb54d3c2deb9675f79
neinja007 <anton.siligan@gmail.com>
2024-09-02 12:48:15 +0200

    accept type

--- src/app/(api)/utils/getIncludedDataObject.ts
+++ src/app/(api)/utils/getIncludedDataObject.ts
@@ -1,13 +1,13 @@
-export const getIncludedDataObject = (
+export const getIncludedDataObject = <Type>(
   includeParamData: string[] | null,
-  allowedItems: string[]
+  allowedItems: (keyof Type)[]
 ): { [key: string]: boolean } | false => {
-  if (!includeParamData || !includeParamData.every((item) => allowedItems.includes(item))) {
+  if (!includeParamData || !includeParamData.every((item) => allowedItems.includes(item as keyof Type))) {
     return false;
   }
 
-  return allowedItems.reduce((acc: { [key: string]: boolean }, item: string) => {
-    acc[item] = includeParamData.includes(item);
+  return allowedItems.reduce((acc: { [key: string]: boolean }, item: keyof Type) => {
+    acc[item as string] = includeParamData.includes(item as string);
     return acc;
   }, {});
 };
