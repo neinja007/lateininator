3005b659ea980cb8c5976cd2a5e02f8e34280327
neinja007 <anton.siligan@gmail.com>
2024-09-02 13:59:53 +0200

    remove list query and restyle cell content

--- src/app/(general)/user/manage-vocabulary/page.tsx
+++ src/app/(general)/user/manage-vocabulary/page.tsx
@@ -1,30 +1,23 @@
 'use client';
-
 import Heading from '@/components/Heading';
-import Hr from '@/components/Hr';
 import Skeleton from '@/components/Skeleton';
-import { Collection, List } from '@prisma/client';
+import { Collection, List, User } from '@prisma/client';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 import Cell from './components/Cell';
 import CellContainer from './components/CellContainer';
-import { Word } from '@/types/word';
 
 const Page = () => {
-  const collectionQuery = useQuery<(Collection & { lists: List[] })[]>({
+  const collectionQuery = useQuery<(Collection & { lists: List[]; owner: User })[]>({
     queryKey: ['collections'],
-    queryFn: () => axios.get('/api/collection', { params: { saved: true, include: ['lists'] } }).then((res) => res.data)
-  });
-
-  const listQuery = useQuery<(List & { words: Word[] })[]>({
-    queryKey: ['lists'],
-    queryFn: () => axios.get('/api/list', { params: { saved: true, include: ['words'] } }).then((res) => res.data)
+    queryFn: () =>
+      axios.get('/api/collection', { params: { saved: true, include: ['lists', 'owner'] } }).then((res) => res.data)
   });
 
   return (
     <div>
       <Heading>Wortschatz</Heading>
-      Ihre Kollektionen (z.B. Schulbücher):
+      <h3 className='text-center font-bold'>Ihre Kollektionen (z.B. Schulbücher)</h3>
       <div className='mt-4 h-44 resize-y overflow-y-scroll'>
         {!collectionQuery.isFetched && <Skeleton pulse />}
         {collectionQuery.isError && <div>Beim Laden der Kollektionen ist ein Fehler aufgetreten.</div>}
@@ -32,24 +25,15 @@ const Page = () => {
           <CellContainer>
             {collectionQuery.data.map((collection) => (
               <Cell key={collection.id}>
-                <h3>
-                  <i>{collection.name}</i> ({collection.lists.length})
+                <h3 className='text-center'>
+                  <span className='text-2xl font-medium'>{collection.name}</span>
                 </h3>
-              </Cell>
-            ))}
-          </CellContainer>
-        )}
-      </div>
-      <Hr className='my-5' />
-      Ihre Listen (z.B. Lektionen eines Schulbuchs):
-      <div className='mt-4 h-44 resize-y overflow-y-scroll'>
-        {!listQuery.isFetched && <Skeleton pulse />}
-        {listQuery.isError && <div>Beim Laden der Listen ist ein Fehler aufgetreten.</div>}
-        {listQuery.isSuccess && (
-          <CellContainer>
-            {listQuery.data.map((list) => (
-              <Cell key={list.id}>
-                {list.name} ({list.words.length})
+                <p className='mt-3 flex items-end justify-between'>
+                  <span className='mt-2'>Listen: {collection.lists.length}</span>
+                  <span>
+                    von <span className='font-medium text-yellow-500'>{collection.owner.name}</span>
+                  </span>
+                </p>
               </Cell>
             ))}
           </CellContainer>
