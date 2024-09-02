1d803277f8500139b51956bde2cbb24b4966ea22
neinja007 <anton.siligan@gmail.com>
2024-09-02 13:00:05 +0200

    display collection list count and list word count

--- src/app/(general)/user/manage-vocabulary/page.tsx
+++ src/app/(general)/user/manage-vocabulary/page.tsx
@@ -8,30 +8,33 @@ import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 import Cell from './components/Cell';
 import CellContainer from './components/CellContainer';
+import { Word } from '@/types/word';
 
 const Page = () => {
-  const collectionQuery = useQuery<Collection[]>({
+  const collectionQuery = useQuery<(Collection & { lists: List[] })[]>({
     queryKey: ['collections'],
-    queryFn: () => axios.get('/api/collection', { params: { saved: true } }).then((res) => res.data)
+    queryFn: () => axios.get('/api/collection', { params: { saved: true, include: ['lists'] } }).then((res) => res.data)
   });
 
-  const listQuery = useQuery<List[]>({
+  const listQuery = useQuery<(List & { words: Word[] })[]>({
     queryKey: ['lists'],
-    queryFn: () => axios.get('/api/list', { params: { saved: true } }).then((res) => res.data)
+    queryFn: () => axios.get('/api/list', { params: { saved: true, include: ['words'] } }).then((res) => res.data)
   });
 
   return (
     <div>
       <Heading>Wortschatz</Heading>
       Ihre Kollektionen (z.B. Schulbücher):
-      <div className='mt-4 h-44 overflow-y-scroll'>
+      <div className='mt-4 h-44 resize-y overflow-y-scroll'>
         {!collectionQuery.isFetched && <Skeleton pulse />}
         {collectionQuery.isError && <div>Beim Laden der Kollektionen ist ein Fehler aufgetreten.</div>}
         {collectionQuery.isSuccess && (
           <CellContainer>
             {collectionQuery.data.map((collection) => (
               <Cell key={collection.id}>
-                <h3 className='font-bold'>{collection.name}</h3>
+                <h3>
+                  <i>{collection.name}</i> ({collection.lists.length})
+                </h3>
               </Cell>
             ))}
           </CellContainer>
@@ -39,13 +42,15 @@ const Page = () => {
       </div>
       <Hr className='my-5' />
       Ihre Listen (z.B. Lektionen eines Schulbuchs):
-      <div className='mt-4 h-44 overflow-y-scroll'>
+      <div className='mt-4 h-44 resize-y overflow-y-scroll'>
         {!listQuery.isFetched && <Skeleton pulse />}
         {listQuery.isError && <div>Beim Laden der Listen ist ein Fehler aufgetreten.</div>}
         {listQuery.isSuccess && (
           <CellContainer>
             {listQuery.data.map((list) => (
-              <Cell key={list.id}>{list.name}</Cell>
+              <Cell key={list.id}>
+                {list.name} ({list.words.length})
+              </Cell>
             ))}
           </CellContainer>
         )}
