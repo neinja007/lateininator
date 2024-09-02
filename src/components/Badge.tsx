e848a106320942e14f49270c795e72caf940e317
neinja007 <anton.siligan@gmail.com>
2024-09-02 16:32:47 +0200

    make border dark for dark mode and use clsx

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -1,11 +1,17 @@
 import { COLORS } from '@/constants/other';
 import { Color } from '@/types/other';
+import clsx from 'clsx';
 
 type BadgeProps = { color?: Color; text: string };
 
 const Badge = ({ color = 'default', text }: BadgeProps) => {
   return (
-    <span className={`rounded-full border p-1 px-2 align-middle text-xs font-medium uppercase ${COLORS[color]}`}>
+    <span
+      className={clsx(
+        'rounded-full border p-1 px-2 align-middle text-xs font-medium uppercase dark:border-gray-700',
+        COLORS[color]
+      )}
+    >
       {text}
     </span>
   );
