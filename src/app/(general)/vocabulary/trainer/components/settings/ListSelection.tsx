1485e1ae14df9f8d5eda887735aef2aab6fa5cc9
neinja007 <anton.siligan@gmail.com>
2024-09-02 15:16:34 +0200

    fetch lists and change everything accordingly

--- src/app/(general)/vocabulary/trainer/components/settings/ListSelection.tsx
+++ src/app/(general)/vocabulary/trainer/components/settings/ListSelection.tsx
@@ -1,25 +1,34 @@
 import Button from '@/components/Button';
-import { lists } from '@/data/lists';
-import { List } from '@/types/other';
+import LinkToSupportEmail from '@/components/LinkToSupportEmail';
+import Skeleton from '@/components/Skeleton';
+import { Word } from '@/types/word';
+import { List } from '@prisma/client';
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
 import { Book } from 'lucide-react';
 import { Dispatch, SetStateAction, useEffect, useState } from 'react';
 
 type ListSelectionProps = {
-  selectedIds: number[];
-  setSelectedIds: Dispatch<SetStateAction<number[]>>;
+  selectedWords: Word[];
+  setSelectedWords: Dispatch<SetStateAction<Word[]>>;
 };
 
-const ListSelection = ({ selectedIds, setSelectedIds }: ListSelectionProps) => {
-  const [selectedLists, setSelectedLists] = useState<List[]>([]);
+const ListSelection = ({ selectedWords, setSelectedWords }: ListSelectionProps) => {
+  const { data: lists, status } = useQuery<(List & { words: Word[] })[]>({
+    queryKey: ['lists'],
+    queryFn: () => axios.get('/api/list', { params: { include: ['words'] } }).then((res) => res.data)
+  });
+
+  const [selectedLists, setSelectedLists] = useState<(List & { words: Word[] })[]>([]);
 
   useEffect(() => {
-    let ids: number[] = [];
-    selectedLists.forEach((list) => {
-      ids = ids.concat(list.words);
-    });
-
-    setSelectedIds(ids);
-  }, [selectedLists, setSelectedIds]);
+    setSelectedWords(
+      selectedLists.reduce((acc: Word[], cur) => {
+        acc.push(...cur.words);
+        return acc;
+      }, [])
+    );
+  }, [selectedLists, setSelectedWords]);
 
   return (
     <>
@@ -27,34 +36,49 @@ const ListSelection = ({ selectedIds, setSelectedIds }: ListSelectionProps) => {
         <p className='mb-3 lg:col-span-2'>Wähle aus, welche Lektionen du lernen möchtest:</p>
         <div className='grid grid-cols-2 gap-x-4'>
           <Button
-            color={selectedLists.length === lists.length ? 'blue' : 'default'}
-            onClick={() => setSelectedLists(lists)}
+            color={status === 'success' && selectedLists.length === lists.length ? 'blue' : 'default'}
+            onClick={() => setSelectedLists(status === 'success' ? lists : [])}
+            disabled={status !== 'success'}
           >
             Alle auswählen
           </Button>
-          <Button color={selectedLists.length === 0 ? 'blue' : 'default'} onClick={() => setSelectedLists([])}>
+          <Button
+            color={selectedLists.length === 0 ? 'blue' : 'default'}
+            onClick={() => setSelectedLists([])}
+            disabled={status !== 'success'}
+          >
             Alle abwählen
           </Button>
         </div>
       </div>
       <div className='grid h-44 max-h-96 min-h-44 resize-y grid-cols-2 gap-2 overflow-hidden overflow-y-scroll scroll-smooth py-5 pr-4 sm:grid-cols-4 md:grid-cols-5 md:gap-4'>
-        {[...lists, ...lists, ...lists].map((list, i) => (
-          <Button
-            key={i}
-            color={selectedLists.includes(list) ? 'blue' : 'default'}
-            onClick={() =>
-              setSelectedLists((prev) => (prev.includes(list) ? prev.filter((t) => t !== list) : [...prev, list]))
-            }
-            className='flex items-center gap-x-2'
-          >
-            <Book className='w-4' />
-            {list.name}
-          </Button>
-        ))}
+        {status === 'pending' && [...Array(15)].map((_, i) => <Skeleton pulse key={i} />)}
+        {status === 'error' && (
+          <span>
+            Fehler beim Laden der Listen. Versuchen Sie es bitte noch einmal. Sollte dieses Problem öfter auftreten, so
+            informieren Sie bitte unseren <LinkToSupportEmail />.
+          </span>
+        )}
+        {status === 'success' &&
+          lists.map((list) => (
+            <Button
+              key={list.id}
+              color={selectedLists.map((list) => list.id).includes(list.id) ? 'blue' : 'default'}
+              onClick={() =>
+                setSelectedLists((prev) => (prev.includes(list) ? prev.filter((t) => t !== list) : [...prev, list]))
+              }
+              className='flex items-center gap-x-2'
+            >
+              <Book className='w-4' />
+              {list.name}
+            </Button>
+          ))}
       </div>
-      <p>
-        Es wurden <b className='text-blue-500'>{selectedIds.length} Wörter</b> ausgewählt.
-      </p>
+      {status === 'success' && (
+        <p>
+          Es wurden <b className='text-blue-500'>{selectedWords.length} Wörter</b> ausgewählt.
+        </p>
+      )}
     </>
   );
 };
