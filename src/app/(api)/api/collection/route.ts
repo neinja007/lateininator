13ec48aee9f0b2c15c8fa089c0da57a11c6f1a46
neinja007 <anton.siligan@gmail.com>
2024-09-02 06:28:37 +0200

    add collection route

---
+++ src/app/(api)/api/collection/route.ts
@@ -0,0 +1,28 @@
+import { prisma } from '@/utils/other/client';
+import { currentUser } from '@clerk/nextjs/server';
+import { NextResponse } from 'next/server';
+
+export const GET = async () => {
+  const user = await currentUser();
+
+  if (!user) {
+    return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+  }
+
+  try {
+    const collections = await prisma.collection.findMany({
+      where: {
+        savedBy: {
+          some: {
+            id: user.id
+          }
+        }
+      }
+    });
+
+    return NextResponse.json(collections, { status: 200 });
+  } catch (error: any) {
+    console.error(error);
+    return NextResponse.json({ error: 'Internal server error' }, { status: 500 });
+  }
+};
