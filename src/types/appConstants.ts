3180909b4ea5ca3476901c54df92ecfc75333b85
neinja007 <anton.siligan@gmail.com>
2024-09-02 15:18:03 +0200

    add main word with other type

--- src/types/appConstants.ts
+++ src/types/appConstants.ts
@@ -2,6 +2,7 @@ import { APP_CONSTANTS } from '@/constants/appConstants';
 
 export type WordType = (typeof APP_CONSTANTS.wordTypes)[number];
 export type MainWordType = (typeof APP_CONSTANTS.mainWordTypes)[number];
+export type MainWordTypeWithOther = (typeof APP_CONSTANTS.mainWordTypesWithOther)[number];
 export type WordProperty = (typeof APP_CONSTANTS.allWordProperties)[number];
 export type OtherProperty = (typeof APP_CONSTANTS.otherProperties)[number];
 export type WordPropertiesUsingSelectInput = (typeof APP_CONSTANTS.wordPropertiesUsingSelectInput)[number];
