a2d492544f1ce97709d3f0f9953abd07cd496714
neinja007 <anton.siligan@gmail.com>
2024-09-02 14:10:30 +0200

    ad outline style and restyle bg

--- src/app/(general)/user/manage-vocabulary/components/Cell.tsx
+++ src/app/(general)/user/manage-vocabulary/components/Cell.tsx
@@ -1,9 +1,23 @@
+import clsx from 'clsx';
+
 type CellProps = {
   children?: React.ReactNode;
+  outlined?: boolean;
 };
 
-const Cell = ({ children }: CellProps) => {
-  return <div className='rounded-lg border bg-blue-200 px-4 py-2 dark:bg-blue-950'>{children}</div>;
+const Cell = ({ children, outlined }: CellProps) => {
+  return (
+    <div
+      className={clsx(
+        'cursor-pointer rounded-lg border border-gray-500 px-4 py-2 transition-colors hover:border-gray-700 dark:hover:border-gray-300',
+        outlined
+          ? 'border-dashed hover:border-solid hover:bg-gray-200 dark:hover:bg-gray-900'
+          : 'bg-blue-200 dark:bg-blue-950'
+      )}
+    >
+      {children}
+    </div>
+  );
 };
 
 export default Cell;
