f9787bb9b58fd772c4a68597b2416d07f2fcdd06
neinja007 <anton.siligan@gmail.com>
2024-09-03 07:03:25 +0200

    add max height classes

--- src/app/(general)/user/manage-vocabulary/components/CellContainer.tsx
+++ src/app/(general)/user/manage-vocabulary/components/CellContainer.tsx
@@ -3,7 +3,7 @@ type CellContainerProps = {
 };
 
 const CellContainer = ({ children }: CellContainerProps) => {
-  return <div className='grid grid-cols-3 gap-3'>{children}</div>;
+  return <div className='grid h-fit max-h-64 grid-cols-3 gap-3 overflow-y-scroll'>{children}</div>;
 };
 
 export default CellContainer;
