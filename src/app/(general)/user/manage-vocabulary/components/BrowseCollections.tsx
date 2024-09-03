584b60ab359f854e36db8b0d4e1199105c1ea1de
neinja007 <anton.siligan@gmail.com>
2024-09-03 08:18:21 +0200

    add browse collections component

---
+++ src/app/(general)/user/manage-vocabulary/components/BrowseCollections.tsx
@@ -0,0 +1,87 @@
+import { Collection, List, User } from '@prisma/client';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import axios from 'axios';
+import CellContainer from './CellContainer';
+import FailToLoad from '@/components/FailToLoad';
+import Skeleton from '@/components/Skeleton';
+import Cell from './Cell';
+import { useState } from 'react';
+import Button from '@/components/Button';
+import clsx from 'clsx';
+
+type BrowseCollectionsProps = {
+  hideBrowseCollections: () => void;
+};
+
+const BrowseCollections = ({ hideBrowseCollections }: BrowseCollectionsProps) => {
+  const [activeCollection, setActiveCollection] = useState<Collection & { lists: List[]; owner: User }>();
+
+  const queryClient = useQueryClient();
+
+  const { status, data: collections } = useQuery<(Collection & { lists: List[]; owner: User })[]>({
+    queryKey: ['collections', { saved: false }],
+    queryFn: () =>
+      axios.get('/api/collection', { params: { saved: false, include: ['lists', 'owner'] } }).then((res) => res.data)
+  });
+
+  const {
+    mutate,
+    variables,
+    error,
+    status: mutationStatus
+  } = useMutation({
+    mutationFn: (collectionId: number) => axios.patch('/api/collection', undefined, { params: { id: collectionId } }),
+    onSettled: () => queryClient.invalidateQueries({ queryKey: ['collections'] }),
+    onSuccess: hideBrowseCollections
+  });
+
+  return (
+    <div className='mt-5'>
+      <div
+        className={clsx(
+          'flex items-baseline justify-between',
+          status === 'success' && collections.length > 0 && 'mb-3'
+        )}
+      >
+        {status === 'pending' || (status === 'success' && collections.length > 0) ? (
+          <p className='mb-2'>Alle öffentliche Kollektionen:</p>
+        ) : (
+          <p>Es konnten keine öffentliche Kollektionen gefunden werden, die Sie noch nicht gespeichert haben.</p>
+        )}
+        <Button onClick={hideBrowseCollections}>Schließen</Button>
+      </div>
+      {status === 'error' && <FailToLoad />}
+      {status === 'pending' &&
+        [...Array(3)].map((_, i) => <Skeleton key={i} pulse customSize className='h-24 w-full' />)}
+      {status === 'success' && (
+        <>
+          <CellContainer>
+            {collections.map((collection) => (
+              <Cell
+                key={collection.id}
+                onClick={() =>
+                  setActiveCollection((prev) => (prev && prev.id === collection.id ? undefined : collection))
+                }
+                buttonOnClick={() => mutate(collection.id)}
+                className={
+                  variables && variables === collection.id && mutationStatus === 'pending'
+                    ? 'animate-pulse opacity-50'
+                    : undefined
+                }
+                buttonLabel='Lektion Hinzufügen'
+                buttonVisible={activeCollection?.id === collection.id}
+                lists={collection.lists.length}
+                name={collection.name}
+                owner={collection.owner.name}
+                buttonColor='green'
+              />
+            ))}
+          </CellContainer>
+        </>
+      )}
+      {error && error.message}
+    </div>
+  );
+};
+
+export default BrowseCollections;
