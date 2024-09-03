86539257b7e06bdec3586b9258b8dfe544f651af
neinja007 <anton.siligan@gmail.com>
2024-09-03 05:50:40 +0200

    add fixed height

--- src/app/(general)/user/manage-vocabulary/components/Cell.tsx
+++ src/app/(general)/user/manage-vocabulary/components/Cell.tsx
@@ -9,7 +9,7 @@ const Cell = ({ children, outlined }: CellProps) => {
   return (
     <div
       className={clsx(
-        'cursor-pointer rounded-lg border border-gray-500 px-4 py-2 transition-colors hover:border-gray-700 dark:hover:border-gray-300',
+        'h-24 cursor-pointer rounded-lg border border-gray-500 px-4 py-2 transition-colors hover:border-gray-700 dark:hover:border-gray-300',
         outlined
           ? 'border-dashed hover:border-solid hover:bg-gray-200 dark:hover:bg-gray-900'
           : 'bg-blue-200 dark:bg-blue-950'
