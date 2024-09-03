b96c572526021e0fc1175b5534b2f90447719805
neinja007 <anton.siligan@gmail.com>
2024-09-03 08:18:06 +0200

    render content in cell (not in parent child body)

--- src/app/(general)/user/manage-vocabulary/components/Cell.tsx
+++ src/app/(general)/user/manage-vocabulary/components/Cell.tsx
@@ -1,12 +1,33 @@
+import Button from '@/components/Button';
+import { Color } from '@/types/other';
 import clsx from 'clsx';
+import { Plus } from 'lucide-react';
 
 type CellProps = {
-  children?: React.ReactNode;
+  className?: string & React.CSSProperties;
   outlined?: boolean;
   onClick?: () => void;
+  name?: string;
+  lists?: number;
+  owner?: string;
+  buttonVisible?: boolean;
+  buttonLabel?: string;
+  buttonOnClick?: () => void;
+  buttonColor?: Color;
 };
 
-const Cell = ({ children, outlined, onClick }: CellProps) => {
+const Cell = ({
+  outlined,
+  onClick,
+  className,
+  buttonLabel,
+  lists,
+  name,
+  owner,
+  buttonVisible,
+  buttonOnClick,
+  buttonColor
+}: CellProps) => {
   return (
     <button
       onClick={onClick}
@@ -14,10 +35,35 @@ const Cell = ({ children, outlined, onClick }: CellProps) => {
         'min-h-24 rounded-lg border border-gray-500 px-4 py-2 transition-colors hover:border-gray-700 dark:hover:border-gray-300',
         outlined
           ? 'border-dashed hover:border-solid hover:bg-gray-200 dark:hover:bg-gray-900'
-          : 'bg-blue-200 dark:bg-blue-950'
+          : 'bg-blue-200 dark:bg-blue-950',
+        className
       )}
     >
-      {children}
+      {outlined ? (
+        <div className='flex h-full items-center justify-center text-gray-500 dark:text-gray-300'>
+          <div className='text-center'>
+            <Plus strokeWidth={1} className='m-0 mx-auto h-12 w-12' />
+            Neue Kollektion anlegen
+          </div>
+        </div>
+      ) : (
+        <>
+          <h3 className='text-center'>
+            <span className='text-2xl font-medium'>{name}</span>
+          </h3>
+          <p className='mt-3 flex items-end justify-between'>
+            <span className='mt-2'>Listen: {lists}</span>
+            <span>
+              von <span className='font-medium text-yellow-500'>{owner}</span>
+            </span>
+          </p>
+          {buttonVisible && (
+            <Button className='mt-3 w-full' onClick={buttonOnClick} color={buttonColor}>
+              {buttonLabel}
+            </Button>
+          )}
+        </>
+      )}
     </button>
   );
 };
