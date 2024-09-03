13f37e790d984b660439a17f70fe51a373d4ceea
neinja007 <anton.siligan@gmail.com>
2024-09-03 18:48:08 +0200

    fix icon css

--- src/components/LoadingHeading.tsx
+++ src/components/LoadingHeading.tsx
@@ -7,14 +7,12 @@ type LoadingHeadingProps = {
 };
 
 const LoadingHeading = ({ children, done }: LoadingHeadingProps) => {
+  const Icon = done ? Check : LoaderPinwheel;
+
   return (
-    <h1 className={clsx('mb-4 text-center text-4xl', done && 'animate-pulse')}>
+    <h1 className={clsx('mb-4 text-center text-4xl', !done && 'animate-pulse')}>
       <span className='inline-block'>
-        {done ? (
-          <Check className='mr-3 h-8 w-8' />
-        ) : (
-          <LoaderPinwheel className='mr-2 inline-block h-8 w-8 animate-spin align-middle' />
-        )}
+        <Icon className={clsx('mr-2 inline-block h-8 w-8 align-middle', !done && 'animate-spin')} />
         <span className='align-middle'>{children}</span>
       </span>
     </h1>
