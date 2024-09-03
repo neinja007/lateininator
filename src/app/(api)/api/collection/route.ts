6c45d0151cbd8a689ee281c17651d473ebafbc15
neinja007 <anton.siligan@gmail.com>
2024-09-03 07:03:04 +0200

    add DELETE handler

--- src/app/(api)/api/collection/route.ts
+++ src/app/(api)/api/collection/route.ts
@@ -37,3 +37,77 @@ export const GET = async (request: NextRequest) => {
     return NextResponse.json({ error: 'Internal server error' }, { status: 500 });
   }
 };
+
+export const DELETE = async (request: NextRequest) => {
+  const user = await currentUser();
+
+  if (!user) {
+    return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+  }
+
+  const searchParams = request.nextUrl.searchParams;
+  const collectionId: number = parseInt(searchParams.get('id') || '');
+
+  if (!collectionId) {
+    return NextResponse.json({ error: 'Missing collection id' }, { status: 400 });
+  }
+
+  try {
+    const collection = await prisma.collection.findUnique({
+      where: {
+        id: collectionId
+      },
+      include: {
+        savedBy: true,
+        owner: true
+      }
+    });
+
+    if (!collection) {
+      return NextResponse.json({ error: 'Collection not found' }, { status: 404 });
+    }
+
+    if (collection.owner.id === user.id) {
+      try {
+        const deletedCollection = await prisma.collection.delete({
+          where: {
+            id: collectionId,
+            owner: {
+              id: user.id
+            }
+          }
+        });
+
+        return NextResponse.json(deletedCollection, { status: 200 });
+      } catch (error: any) {
+        console.error(error);
+        return NextResponse.json({ error: 'Internal server error' }, { status: 500 });
+      }
+    } else if (collection.savedBy.find((u) => u.id === user.id)) {
+      try {
+        const updatedCollection = await prisma.collection.update({
+          where: {
+            id: collectionId
+          },
+          data: {
+            savedBy: {
+              disconnect: {
+                id: user.id
+              }
+            }
+          }
+        });
+
+        return NextResponse.json(updatedCollection, { status: 200 });
+      } catch (error: any) {
+        console.error(error);
+        return NextResponse.json({ error: 'Internal server error' }, { status: 500 });
+      }
+    } else {
+      return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+    }
+  } catch (error: any) {
+    console.error(error);
+    return NextResponse.json({ error: 'Internal server error' }, { status: 500 });
+  }
+};
