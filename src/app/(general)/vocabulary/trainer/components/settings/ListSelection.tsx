e556bb3b345c27a7d3a964dbc1219ec2765079b2
neinja007 <anton.siligan@gmail.com>
2024-09-03 08:47:55 +0200

    account for saved collections

--- src/app/(general)/vocabulary/trainer/components/settings/ListSelection.tsx
+++ src/app/(general)/vocabulary/trainer/components/settings/ListSelection.tsx
@@ -2,10 +2,11 @@ import Button from '@/components/Button';
 import FailToLoad from '@/components/FailToLoad';
 import Skeleton from '@/components/Skeleton';
 import { Word } from '@/types/word';
-import { List } from '@prisma/client';
+import { Collection, List } from '@prisma/client';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 import { Book } from 'lucide-react';
+import Link from 'next/link';
 import { Dispatch, SetStateAction, useEffect, useState } from 'react';
 
 type ListSelectionProps = {
@@ -14,13 +15,29 @@ type ListSelectionProps = {
 };
 
 const ListSelection = ({ selectedWords, setSelectedWords }: ListSelectionProps) => {
-  const { data: lists, status } = useQuery<(List & { words: Word[] })[]>({
+  const { data: lists, status } = useQuery<(List & { words: Word[]; collection: Collection })[]>({
     queryKey: ['lists'],
     queryFn: () =>
       axios
-        .get('/api/list', { params: { include: ['words'], wordInclude: ['noun', 'verb', 'adjective'] } })
+        .get('/api/list', { params: { include: ['words', 'collection'], wordInclude: ['noun', 'verb', 'adjective'] } })
         .then((res) => res.data)
   });
+
+  const [collections, setCollections] = useState<Collection[]>([]);
+  const [selectedCollection, setSelectedCollection] = useState<number>();
+
+  useEffect(() => {
+    if (status === 'success') {
+      setCollections(
+        lists.reduce((acc: Collection[], curr) => {
+          if (!acc.find((collection) => collection.id === curr.collection.id)) {
+            acc.push(curr.collection);
+          }
+          return acc;
+        }, [])
+      );
+    }
+  }, [lists, status]);
 
   const [selectedLists, setSelectedLists] = useState<(List & { words: Word[] })[]>([]);
 
@@ -33,10 +50,12 @@ const ListSelection = ({ selectedWords, setSelectedWords }: ListSelectionProps) 
     );
   }, [selectedLists, setSelectedWords]);
 
+  const filteredLists = lists?.filter((list) => selectedCollection === list.collection.id) || [];
+
   return (
     <>
       <div className='grid-cols-2 md:grid lg:grid-cols-3'>
-        <p className='mb-3 lg:col-span-2'>Wähle aus, welche Lektionen du lernen möchtest:</p>
+        <p className='mb-3 lg:col-span-2'>Wähle aus, welche Wörter du lernen möchtest:</p>
         <div className='grid grid-cols-2 gap-x-4'>
           <Button
             color={status === 'success' && selectedLists.length === lists.length ? 'blue' : 'default'}
@@ -54,24 +73,71 @@ const ListSelection = ({ selectedWords, setSelectedWords }: ListSelectionProps) 
           </Button>
         </div>
       </div>
-      <div className='grid h-fit max-h-80 grid-cols-2 gap-3 overflow-hidden overflow-y-scroll scroll-smooth py-5 sm:grid-cols-4 md:grid-cols-5'>
-        {status === 'pending' &&
-          [...Array(15)].map((_, i) => <Skeleton pulse key={i} customSize className='h-9 w-full' />)}
+      <div className='mt-5'>
+        <p className='mb-2'>
+          Gespeicherte Kollektionen
+          {(status !== 'success' || collections.length > 0) && (
+            <span>
+              {' '}
+              (können bei der{' '}
+              <Link href={'/manage-vocabulary'} className='font-medium text-blue-500 hover:underline'>
+                Wortschatz-Verwaltung
+              </Link>{' '}
+              bearbeitet werden)
+            </span>
+          )}
+          :
+        </p>
         {status === 'error' && <FailToLoad />}
-        {status === 'success' &&
-          lists.map((list) => (
-            <Button
-              key={list.id}
-              color={selectedLists.map((list) => list.id).includes(list.id) ? 'blue' : 'default'}
-              onClick={() =>
-                setSelectedLists((prev) => (prev.includes(list) ? prev.filter((t) => t !== list) : [...prev, list]))
-              }
-              className='flex items-center gap-x-2'
-            >
-              <Book className='w-4' />
-              {list.name}
-            </Button>
-          ))}
+        <div className='grid gap-4 sm:grid-cols-2 lg:grid-cols-3'>
+          {status === 'pending' &&
+            [...Array(3)].map((_, i) => <Skeleton pulse key={i} customSize className='h-9 w-full' />)}
+          {status === 'success' &&
+            (collections.length > 0 ? (
+              collections.map((collection) => (
+                <Button
+                  key={collection.id}
+                  color={selectedCollection === collection.id ? 'blue' : 'default'}
+                  onClick={() => setSelectedCollection((prev) => (prev === collection.id ? undefined : collection.id))}
+                >
+                  {collection.name}
+                </Button>
+              ))
+            ) : (
+              <div className='col-span-full'>
+                Keine gespeicherten Kollektionen gefunden. Sie können bei der{' '}
+                <Link href={'/manage-vocabulary'} className='font-medium text-blue-500 hover:underline'>
+                  Wortschatz-Verwaltung
+                </Link>{' '}
+                ein paar hinzufügen.
+              </div>
+            ))}
+        </div>
+      </div>
+      <div className='mt-3'>
+        {(status !== 'success' || filteredLists.length !== 0) && <p className='mb-2'>Listen:</p>}
+        {status === 'error' && <FailToLoad />}
+        <div className='grid h-fit max-h-80 grid-cols-2 gap-3 overflow-hidden overflow-y-scroll scroll-smooth sm:grid-cols-4 md:grid-cols-5'>
+          {status === 'pending' &&
+            [...Array(15)].map((_, i) => <Skeleton pulse key={i} customSize className='h-9 w-full' />)}
+          {status === 'success' && filteredLists.length > 0 ? (
+            filteredLists.map((list) => (
+              <Button
+                key={list.id}
+                color={selectedLists.map((list) => list.id).includes(list.id) ? 'blue' : 'default'}
+                onClick={() =>
+                  setSelectedLists((prev) => (prev.includes(list) ? prev.filter((t) => t !== list) : [...prev, list]))
+                }
+                className='flex items-center gap-x-2'
+              >
+                <Book className='w-4' />
+                {list.name}
+              </Button>
+            ))
+          ) : (
+            <div className='col-span-full'>Wähle eine Kollektion aus, um Listen anzuzeigen</div>
+          )}
+        </div>
       </div>
       {status === 'success' && (
         <p>
