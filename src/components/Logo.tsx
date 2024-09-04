bf2f501b87f5a1162d8722843b3064d5f1722008
neinja007 <anton.siligan@gmail.com>
2024-09-04 06:01:41 +0200

    use priority tag and use smaller width

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,11 +1,17 @@
 import Link from 'next/link';
 import Image from 'next/image';
-import LogoImage from '../../public/logo_wide.png';
 
 const Logo = () => {
   return (
     <Link href={'/'}>
-      <Image className='absolute start-1 top-1 h-14 w-auto' priority src={LogoImage} alt='Logo Wide' />
+      <Image
+        className='absolute start-1 top-1'
+        width={245}
+        height={56}
+        priority
+        src={'/logo_1000x.png'}
+        alt='Logo Wide'
+      />
     </Link>
   );
 };
