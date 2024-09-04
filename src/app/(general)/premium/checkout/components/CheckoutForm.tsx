090b334133aa3da713aad231876deb7f81132f74
neinja007 <anton.siligan@gmail.com>
2024-09-04 16:14:43 +0200

    fetch from payment-subscription endpoint

--- src/app/(general)/premium/checkout/components/CheckoutForm.tsx
+++ src/app/(general)/premium/checkout/components/CheckoutForm.tsx
@@ -11,7 +11,7 @@ const CheckoutForm = () => {
   const elements = useElements();
 
   useEffect(() => {
-    fetch('/api/create-payment-intent', {
+    fetch('/api/create-subscription', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
