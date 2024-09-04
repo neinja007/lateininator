52573afde46302290132a829405b761999e55b37
neinja007 <anton.siligan@gmail.com>
2024-09-04 10:34:28 +0200

    fix button cannot be a descendant of button

--- src/app/(general)/user/manage-vocabulary/components/Cell.tsx
+++ src/app/(general)/user/manage-vocabulary/components/Cell.tsx
@@ -29,42 +29,44 @@ const Cell = ({
   buttonColor
 }: CellProps) => {
   return (
-    <button
-      onClick={onClick}
-      className={clsx(
-        'min-h-24 rounded-lg border px-4 py-2 transition-colors',
-        outlined
-          ? 'border-dashed hover:border-solid hover:bg-gray-200 dark:hover:bg-gray-900'
-          : 'border-blue-500 bg-blue-400 hover:border-blue-900 hover:bg-blue-500 dark:bg-blue-950 dark:hover:border-blue-300 dark:hover:bg-blue-800',
-        className
-      )}
-    >
-      {outlined ? (
-        <div className='flex h-full items-center justify-center text-gray-500 dark:text-gray-300'>
-          <div className='text-center'>
-            <Plus strokeWidth={1} className='m-0 mx-auto h-12 w-12' />
-            Neue Kollektion anlegen
+    <div className='min-h-24'>
+      <button
+        onClick={onClick}
+        className={clsx(
+          'min-h-24 w-full rounded-lg border px-4 py-2 transition-colors',
+          outlined
+            ? 'border-dashed hover:border-solid hover:bg-gray-200 dark:hover:bg-gray-900'
+            : 'border-blue-500 bg-blue-400 hover:border-blue-900 hover:bg-blue-500 dark:bg-blue-950 dark:hover:border-blue-300 dark:hover:bg-blue-800',
+          className
+        )}
+      >
+        {outlined ? (
+          <div className='flex h-full items-center justify-center text-gray-500 dark:text-gray-300'>
+            <div className='text-center'>
+              <Plus strokeWidth={1} className='m-0 mx-auto h-12 w-12' />
+              Neue Kollektion anlegen
+            </div>
           </div>
-        </div>
-      ) : (
-        <>
-          <h3 className='text-center'>
-            <span className='text-2xl font-medium'>{name}</span>
-          </h3>
-          <p className='mt-3 block items-end justify-between sm:flex'>
-            <span className='mt-2 block'>{lists} Listen</span>
-            <span>
-              von <span className='font-medium text-yellow-400 dark:text-yellow-500'>{owner}</span>
-            </span>
-          </p>
-          {buttonVisible && (
-            <Button className='mt-3 w-full' onClick={buttonOnClick} color={buttonColor}>
-              {buttonLabel}
-            </Button>
-          )}
-        </>
+        ) : (
+          <>
+            <h3 className='text-center'>
+              <span className='text-2xl font-medium'>{name}</span>
+            </h3>
+            <p className='mt-3 block items-end justify-between sm:flex'>
+              <span className='mt-2 block'>{lists} Listen</span>
+              <span>
+                von <span className='font-medium text-yellow-400 dark:text-yellow-500'>{owner}</span>
+              </span>
+            </p>
+          </>
+        )}
+      </button>
+      {buttonVisible && (
+        <Button className={clsx('mt-2 w-full', className)} onClick={buttonOnClick} color={buttonColor}>
+          {buttonLabel}
+        </Button>
       )}
-    </button>
+    </div>
   );
 };
 
