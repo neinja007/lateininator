9890fc4429881ccbe560d4f3aed732efc8c81022
neinja007 <anton.siligan@gmail.com>
2024-09-04 16:16:52 +0200

    account for util rename

--- src/app/(api)/api/user/route.ts
+++ src/app/(api)/api/user/route.ts
@@ -2,7 +2,7 @@ import { currentUser } from '@clerk/nextjs/server';
 import { NextRequest, NextResponse } from 'next/server';
 import { getIncludedData } from '../../utils/getIncludedData';
 import { createUser } from './services/createUser';
-import { getUser } from './services/getUser';
+import { getUserById } from './services/getUserById';
 
 export const GET = async (request: NextRequest) => {
   const clerkUser = await currentUser();
@@ -20,7 +20,7 @@ export const GET = async (request: NextRequest) => {
   }
 
   try {
-    const user = await getUser(clerkUser.id, includedDataObject);
+    const user = await getUserById(clerkUser.id, includedDataObject);
     return NextResponse.json(user, { status: 200 });
   } catch (error: any) {
     console.error(error);
@@ -42,7 +42,7 @@ export const POST = async () => {
       return NextResponse.json({ status: 400, body: { message: 'Email or username not found' } });
     }
 
-    const userExists = await getUser(user.id);
+    const userExists = await getUserById(user.id);
 
     if (userExists) {
       return NextResponse.json(userExists, { status: 200 });
