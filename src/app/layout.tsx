bc02f722d69d83b8df72b11fe8d265ecb521013a
neinja007 <anton.siligan@gmail.com>
2024-09-05 08:37:22 +0200

    move react query logic to query client wrapper

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,43 +1,28 @@
-import type { Metadata } from 'next';
 import { Rubik } from 'next/font/google';
 import Navbar from '@/components/Navbar';
 import { ClerkProvider } from '@clerk/nextjs';
 import { deDE } from '@clerk/localizations';
 import { Analytics } from '@vercel/analytics/react';
 import '@/app/globals.css';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { QueryClientWrapper } from '@/components/QueryClientWrapper';
 
 const rubik = Rubik({ subsets: ['latin'] });
 
-export const metadata: Metadata = {
+export const metadata = {
   title: 'Lateininator',
-  description: 'Die #1 Latein-Lern-App'
+  description: 'Die #1 Latein-App zum Latein lernen'
 };
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 1000 * 60 * 5
-    }
-  }
-});
-
-const RootLayout = ({
-  children
-}: Readonly<{
-  children: React.ReactNode;
-}>) => {
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <ClerkProvider localization={deDE}>
       <html lang='en'>
         <body className={rubik.className + ' dark:bg-black dark:text-gray-300'}>
           <div className='min-h-screen px-4 pt-24'>
-            <QueryClientProvider client={queryClient}>
+            <QueryClientWrapper>
               <Navbar />
               <div className='container mx-auto max-w-[1024px] pb-16'>{children}</div>
-            </QueryClientProvider>
-            <ReactQueryDevtools initialIsOpen={false} />
+            </QueryClientWrapper>
           </div>
           <Analytics />
         </body>
