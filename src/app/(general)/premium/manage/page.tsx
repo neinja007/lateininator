d397b2efbf5e9135946e16a4096e8cefe43a80e2
neinja007 <anton.siligan@gmail.com>
2024-09-05 08:50:50 +0200

    add subscription management route

--- src/app/(general)/premium/manage/page.tsx
+++ src/app/(general)/premium/manage/page.tsx
@@ -1,14 +1,36 @@
+'use client';
+
 import Heading from '@/components/Heading';
+import { RedirectToSignIn, useUser } from '@clerk/nextjs';
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
 
 export const Page = () => {
+  const { data: stripeUser, isLoading } = useQuery({
+    queryKey: ['stripeUser'],
+    queryFn: () => axios.get('/api/stripe-user').then((res) => res.data)
+  });
+
+  const user = useUser();
+
+  if (user.isLoaded && !user.isSignedIn) {
+    return <RedirectToSignIn />;
+  }
+
+  if (isLoading) {
+    return (
+      <div>
+        <Heading>Abo Verwalten</Heading>
+        <div>Wird geladen...</div>
+      </div>
+    );
+  }
+
   return (
     <div>
       <Heading>Abo Verwalten</Heading>
       <div>
-        <div>
-          <p>Abo</p>
-          <p>Status</p>
-        </div>
+        <div>{JSON.stringify(stripeUser)}</div>
       </div>
     </div>
   );
