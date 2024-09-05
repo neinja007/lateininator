8ef35a4a235142e44b5cfdb2234aa1a2e5834691
neinja007 <anton.siligan@gmail.com>
2024-09-05 10:49:00 +0200

    add subscription management page

--- src/app/(general)/premium/manage/page.tsx
+++ src/app/(general)/premium/manage/page.tsx
@@ -1,36 +1,82 @@
 'use client';
-
 import Heading from '@/components/Heading';
 import { RedirectToSignIn, useUser } from '@clerk/nextjs';
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import axios from 'axios';
+import dayjs from 'dayjs';
+import { useRouter } from 'next/navigation';
+import { Card } from './components/Card';
 
 export const Page = () => {
-  const { data: stripeUser, isLoading } = useQuery({
-    queryKey: ['stripeUser'],
-    queryFn: () => axios.get('/api/stripe-user').then((res) => res.data)
+  const queryClient = useQueryClient();
+
+  const { data: subscription, isLoading } = useQuery({
+    queryKey: ['subscription'],
+    queryFn: () => axios.get('/api/subscription').then((res) => res.data.data)
+  });
+
+  const router = useRouter();
+
+  const { mutate: pauseSubscription, isPending: isPausePending } = useMutation({
+    mutationFn: () => axios.patch('/api/subscription', { cancel: true }).then((res) => res.data),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['subscription'] });
+      queryClient.invalidateQueries({ queryKey: ['dbUser'] });
+      router.push('/premium/overview');
+    }
   });
 
   const user = useUser();
+
+  if (!isLoading && !subscription) {
+    router.push('/premium/overview');
+    return;
+  }
 
   if (user.isLoaded && !user.isSignedIn) {
     return <RedirectToSignIn />;
   }
 
-  if (isLoading) {
-    return (
-      <div>
-        <Heading>Abo Verwalten</Heading>
-        <div>Wird geladen...</div>
-      </div>
-    );
-  }
+  const status = subscription && subscription.status;
+  const active = status === 'active' || status === 'trialing';
+
+  console.log(status);
+
+  const nextPaymentDate = active && dayjs.unix(subscription.current_period_end).format('DD.MM.YYYY');
+  const nextPaymentDays = active && dayjs.unix(subscription.current_period_end).diff(dayjs(), 'days');
+  const nextPaymentAmount = active && subscription.plan && (subscription.plan.amount / 100).toFixed(2);
+  const trialEnd = active && dayjs.unix(subscription.trial_end).format('DD.MM.YYYY');
 
   return (
     <div>
       <Heading>Abo Verwalten</Heading>
-      <div>
-        <div>{JSON.stringify(stripeUser)}</div>
+      <div className='flex justify-center'>
+        {isLoading ? (
+          <Card
+            loading
+            active
+            isPausePending={false}
+            nextPaymentAmount={0}
+            nextPaymentDays={0}
+            nextPaymentDate=''
+            trialEnd=''
+            status=''
+            pauseSubscription={() => {}}
+          />
+        ) : nextPaymentDays && nextPaymentDate && trialEnd ? (
+          <Card
+            active={active}
+            nextPaymentAmount={nextPaymentAmount}
+            nextPaymentDays={nextPaymentDays}
+            nextPaymentDate={nextPaymentDate}
+            trialEnd={trialEnd}
+            status={status}
+            pauseSubscription={pauseSubscription}
+            isPausePending={isPausePending}
+          />
+        ) : (
+          <div>Ein Fehler ist aufgetreten</div>
+        )}
       </div>
     </div>
   );
