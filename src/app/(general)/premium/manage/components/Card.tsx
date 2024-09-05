a25763ddf51de3b294c8628dcd85cd3bf3d65bc1
neinja007 <anton.siligan@gmail.com>
2024-09-05 10:48:05 +0200

    add card component

---
+++ src/app/(general)/premium/manage/components/Card.tsx
@@ -0,0 +1,72 @@
+import Button from '@/components/Button';
+import Hr from '@/components/Hr';
+import Skeleton from '@/components/Skeleton';
+
+type CardProps = {
+  active: boolean;
+  nextPaymentAmount: number;
+  nextPaymentDays: number;
+  nextPaymentDate: string;
+  trialEnd: string;
+  status: string;
+  pauseSubscription: () => void;
+  isPausePending: boolean;
+  loading?: boolean;
+};
+
+export const Card = ({
+  active,
+  nextPaymentAmount,
+  nextPaymentDays,
+  nextPaymentDate,
+  trialEnd,
+  status,
+  pauseSubscription,
+  isPausePending,
+  loading
+}: CardProps) => {
+  return (
+    <div className='w-full max-w-[400px] rounded-lg bg-blue-950 p-4'>
+      <div className='w-full text-center'>
+        {active ? (
+          <>
+            <p className='text-xl font-medium'>Nächste Abrechnung</p>
+            {loading ? (
+              <Skeleton customSize className='h-6 w-full' />
+            ) : (
+              <p className='h-6'>
+                <b>{nextPaymentAmount}€</b> in{' '}
+                <b>
+                  {nextPaymentDays} {nextPaymentDays === 1 ? 'Tag' : 'Tagen'}
+                </b>{' '}
+                ({nextPaymentDate})
+              </p>
+            )}
+          </>
+        ) : (
+          <span className='break-words text-red-400'>Ihr Abo wurde pausiert.</span>
+        )}
+      </div>
+      {!loading && status === 'trialing' && (
+        <>
+          <Hr className='my-5' />
+          <p className='text-center text-xl font-medium text-yellow-500'>Sie befinden sich im Testlauf</p>
+          <p className='w-full break-words text-center'>
+            Der Testlauf endet am <b>{trialEnd}</b>.
+          </p>
+        </>
+      )}
+      <Hr className='my-5' />
+      <Button
+        onClick={() => {
+          pauseSubscription();
+        }}
+        disabled={isPausePending || loading}
+        color='red'
+        className='w-full'
+      >
+        Abo beenden
+      </Button>
+    </div>
+  );
+};
