fd5a0c1c56d3e049f961934bdaf14b256bf45322
neinja007 <anton.siligan@gmail.com>
2024-09-05 08:39:03 +0200

    create manage subscription route

---
+++ src/app/(general)/premium/manage/page.tsx
@@ -0,0 +1,17 @@
+import Heading from '@/components/Heading';
+
+export const Page = () => {
+  return (
+    <div>
+      <Heading>Abo Verwalten</Heading>
+      <div>
+        <div>
+          <p>Abo</p>
+          <p>Status</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
