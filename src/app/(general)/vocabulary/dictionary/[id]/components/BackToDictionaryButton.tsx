ac81c0c049f6df00c0787407f1802fd9ce70ce1d
neinja007 <anton.siligan@gmail.com>
2024-09-06 14:56:26 +0200

    fix text alignment

--- src/app/(general)/vocabulary/dictionary/[id]/components/BackToDictionaryButton.tsx
+++ src/app/(general)/vocabulary/dictionary/[id]/components/BackToDictionaryButton.tsx
@@ -8,7 +8,7 @@ const BackToDictionaryButton = () => {
     <Link
       className={clsx(
         ui.basic,
-        'hover w-fit bg-gray-100 text-center transition-colors hover:bg-gray-200 md:!pl-2 dark:bg-gray-900 hover:dark:bg-gray-800'
+        'hover flex w-fit items-center bg-gray-100 text-center transition-colors hover:bg-gray-200 md:!pl-2 dark:bg-gray-900 hover:dark:bg-gray-800'
       )}
       href='/vocabulary/dictionary'
     >
