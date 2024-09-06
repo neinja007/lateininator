9f7c626e9e586026912b71adedaafa05f45d3437
neinja007 <anton.siligan@gmail.com>
2024-09-06 15:07:20 +0200

    implement individual input component and fix label

--- src/app/(general)/flexion/noun/components/Test.tsx
+++ src/app/(general)/flexion/noun/components/Test.tsx
@@ -3,7 +3,6 @@ import WordDisplay from '@/components/WordDisplay';
 import { Dispatch, SetStateAction, useEffect, useState } from 'react';
 import TableInput from './test/TableInput';
 import { IndividualInputForm, SetTableInputValues, TableInputValues } from '../types';
-import TrainerInput from '@/components/TrainerInput';
 import { MAPPER } from '@/utils/other/mapper';
 import { useTestForm } from '@/hooks/useTestForm';
 import Hr from '@/components/Hr';
@@ -12,6 +11,7 @@ import { WordCase } from '@/types/wordConstants';
 import { getRandomItem } from '@/utils/helpers/getRandomItem';
 import { getForm } from '@/utils/word/getForm';
 import { isNoun } from '@/utils/typeguards/isNoun';
+import { IndividualTrainerInput } from '../../components/IndividualTrainerInput';
 
 type TestProps = {
   activeWord: Noun;
@@ -55,18 +55,20 @@ const Test = ({
 
   const { submit } = useTestForm(handleContinue);
 
+  const correctIndividualInputValue = getForm(activeWord, individualInputForm);
+
   return (
     <>
       <WordDisplay word={activeWord} />
       <Hr />
       <form onSubmit={submit} className='space-y-8'>
         {individualInputForm && testingType === 'individual' ? (
-          <TrainerInput
-            label={`${MAPPER.extended.declension[activeWord.noun.declension]} ${MAPPER.extended.numerus[individualInputForm.numerus]}`}
-            handleChange={setIndividualInputValue}
-            value={individualInputValue}
-            correctValue={getForm(activeWord, individualInputForm)}
+          <IndividualTrainerInput
+            label={`${MAPPER.extended.wordCase[individualInputForm.wordCase]} ${MAPPER.extended.numerus[individualInputForm.numerus]}`}
+            correctValue={correctIndividualInputValue}
             stage={stage}
+            value={individualInputValue}
+            setValue={setIndividualInputValue}
           />
         ) : (
           testingType === 'table' && (
