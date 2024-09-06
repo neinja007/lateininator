5cbb1198ad6aa0851ea9c3924339789da75fcd02
neinja007 <anton.siligan@gmail.com>
2024-09-06 10:48:17 +0200

    add loading state to header

--- src/app/(general)/vocabulary/dictionary/[id]/components/Header.tsx
+++ src/app/(general)/vocabulary/dictionary/[id]/components/Header.tsx
@@ -4,18 +4,27 @@ import { Word } from '@/types/word';
 import { getLexicalForm } from '@/utils/word/getLexicalForm';
 import BackToDictionaryButton from './BackToDictionaryButton';
 import { MAPPER } from '@/utils/other/mapper';
+import Skeleton from '@/components/Skeleton';
 
-type HeaderProps = { word: Word };
+type HeaderProps = { word: Word; loading: false };
 
-const Header = ({ word }: HeaderProps) => {
+type HeaderLoadingProps = { word?: undefined; loading: true };
+
+const Header = ({ word, loading }: HeaderProps | HeaderLoadingProps) => {
   return (
     <div className='min-h-12 grid-cols-2 justify-center sm:flex sm:gap-4 md:grid lg:grid-cols-3'>
       <BackToDictionaryButton />
       <Heading>
         <span className='mt-4 flex justify-center sm:mt-0 sm:justify-end lg:justify-center'>
-          {word.name} {getLexicalForm(word)}
+          {loading ? (
+            <Skeleton customSize className='h-8 w-full' />
+          ) : (
+            <span>
+              {word.name} {getLexicalForm(word)}
+            </span>
+          )}
           <div className='ml-3 md:text-right'>
-            <Badge text={MAPPER.extended.type.singular[word.type]} />
+            <Badge text={loading ? '' : MAPPER.extended.type.singular[word.type]} />
           </div>
         </span>
       </Heading>
