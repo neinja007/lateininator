b1e01e2670cf8d5393642744bcee2f4246cc56f5
neinja007 <anton.siligan@gmail.com>
2024-09-07 11:31:49 +0200

    change return type

--- src/app/(api)/api/user-settings/route.ts
+++ src/app/(api)/api/user-settings/route.ts
@@ -1,5 +1,6 @@
 import { prisma } from '@/utils/other/client';
 import { currentUser } from '@clerk/nextjs/server';
+import { UserSetting } from '@prisma/client';
 import { NextResponse } from 'next/server';
 
 export const GET = async () => {
@@ -16,13 +17,43 @@ export const GET = async () => {
 
   const defaultSettings = await prisma.defaultSetting.findMany();
 
-  const combinedSettings = defaultSettings.map((setting) => {
-    const userSetting = userSettings.find((userSetting) => userSetting.settingKey === setting.settingKey);
-    return {
-      ...setting,
-      settingValue: userSetting?.settingValue || setting.defaultValue
-    };
-  });
+  const combinedSettings: Omit<UserSetting, 'id' | 'userId' | 'createdAt' | 'updatedAt'>[] = defaultSettings.map(
+    (setting) => {
+      const userSetting = userSettings.find((userSetting) => userSetting.settingKey === setting.settingKey);
+      return {
+        settingKey: setting.settingKey,
+        settingValue: userSetting?.settingValue || setting.defaultValue
+      };
+    }
+  );
 
   return NextResponse.json(combinedSettings);
+};
+
+export const PATCH = async (req: Request) => {
+  const { settingKey, settingValue } = await req.json();
+
+  const user = await currentUser();
+  if (!user) {
+    return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+  }
+
+  const userSetting = await prisma.userSetting.upsert({
+    where: {
+      userId_settingKey: {
+        userId: user.id,
+        settingKey
+      }
+    },
+    update: {
+      settingValue
+    },
+    create: {
+      userId: user.id,
+      settingKey,
+      settingValue
+    }
+  });
+
+  return NextResponse.json(userSetting);
 };
