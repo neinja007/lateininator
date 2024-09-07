88eb7c04ce4a22dd6d9b1f9a83c3671faf456737
neinja007 <anton.siligan@gmail.com>
2024-09-07 12:08:07 +0200

    use useSettings hook to conditionally render vocative row in noun table

--- src/app/(general)/vocabulary/dictionary/[id]/components/tables/NounTable.tsx
+++ src/app/(general)/vocabulary/dictionary/[id]/components/tables/NounTable.tsx
@@ -3,10 +3,16 @@ import table from '@/styles/table.module.css';
 import { Noun } from '@/types/word';
 import { getForm } from '@/utils/word/getForm';
 import { WORD_CONSTANTS } from '@/constants/wordConstants';
+import { useSettings } from '@/hooks/database/useSettings';
 
 type NounTableProps = { word: Noun };
 
 const NounTable = ({ word }: NounTableProps) => {
+  const { settings, status } = useSettings();
+
+  const showVocative =
+    settings?.find((setting) => setting.settingKey === 'DICTIONARY_VOCATIVE')?.settingValue === 'true';
+
   return (
     <div className={table.container}>
       <table className={table.table}>
@@ -21,16 +27,18 @@ const NounTable = ({ word }: NounTableProps) => {
           </tr>
         </thead>
         <tbody>
-          {WORD_CONSTANTS.wordCase.map((wordCase) => (
-            <tr key={wordCase} className={table.tr}>
-              <th className={table.th}>{MAPPER.extended.wordCase[wordCase]}</th>
-              {WORD_CONSTANTS.numerus.map((numerus) => (
-                <td key={numerus} className={table.td}>
-                  {getForm(word, { wordCase: wordCase, numerus: numerus })}
-                </td>
-              ))}
-            </tr>
-          ))}
+          {WORD_CONSTANTS.wordCase
+            .filter((wordCase) => (status === 'success' && showVocative) || wordCase !== '6')
+            .map((wordCase) => (
+              <tr key={wordCase} className={table.tr}>
+                <th className={table.th}>{MAPPER.extended.wordCase[wordCase]}</th>
+                {WORD_CONSTANTS.numerus.map((numerus) => (
+                  <td key={numerus} className={table.td}>
+                    {getForm(word, { wordCase: wordCase, numerus: numerus })}
+                  </td>
+                ))}
+              </tr>
+            ))}
         </tbody>
       </table>
     </div>
