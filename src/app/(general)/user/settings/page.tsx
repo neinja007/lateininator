93de0598862bc68300927c5cd0a71da071a95e83
neinja007 <anton.siligan@gmail.com>
2024-09-07 10:50:05 +0200

    add settings query

--- src/app/(general)/user/settings/page.tsx
+++ src/app/(general)/user/settings/page.tsx
@@ -1,13 +1,26 @@
+'use client';
+
+import { useQuery } from '@tanstack/react-query';
 import FancyLink from '@/components/FancyLink';
 import Heading from '@/components/Heading';
 import { IsPremium } from '@/components/IsPremium';
 import { Gem, LogOut, UserIcon } from 'lucide-react';
+import axios from 'axios';
+import { DefaultSetting } from '@prisma/client';
+import Setting from './components/Setting';
 
 const Page = () => {
+  const { data: settings, isLoading } = useQuery({
+    queryKey: ['user-settings'],
+    queryFn: () => axios.get('/api/user-settings')
+  });
+
   return (
     <div>
       <Heading>Einstellungen</Heading>
-      <div className='h-44'>Bis jetzt gibt es noch keine Einstellungen.</div>
+      <div className='h-44'>
+        {settings?.data.map((setting: DefaultSetting) => <Setting key={setting.id} setting={setting} />)}
+      </div>
       <p className='mb-2 text-center text-lg font-bold'>Konto-Einstellungen</p>
       <div className='items-center justify-between gap-x-2 sm:flex'>
         <FancyLink
