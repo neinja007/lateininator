edf91f2aae056d1f7b447cd98f8b1ae32d29f7a0
neinja007 <anton.siligan@gmail.com>
2024-09-07 13:38:48 +0200

    use db user

--- src/components/IsPremium.tsx
+++ src/components/IsPremium.tsx
@@ -1,14 +1,8 @@
 'use client';
-
-import { User } from '@prisma/client';
-import { useQuery } from '@tanstack/react-query';
-import axios from 'axios';
+import { useDbUser } from '@/hooks/database/useDbUser';
 
 export const IsPremium = ({ children, not }: { children: React.ReactNode; not?: boolean }) => {
-  const { data: dbUser, isLoading } = useQuery<User>({
-    queryKey: ['dbUser'],
-    queryFn: () => axios.get('/api/user').then((res) => res.data)
-  });
+  const { dbUser, isLoading } = useDbUser();
 
   if (isLoading || !dbUser?.premium) {
     return not ? children : null;
