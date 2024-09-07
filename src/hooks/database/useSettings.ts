b8704a3335a3174b3af56a40b7a179734ec979e5
neinja007 <anton.siligan@gmail.com>
2024-09-07 12:05:50 +0200

    create useSettings hook

---
+++ src/hooks/database/useSettings.ts
@@ -0,0 +1,12 @@
+import { UserSetting } from '@prisma/client';
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+
+export const useSettings = (): {} => {
+  const { data: settings, status } = useQuery<UserSetting[]>({
+    queryKey: ['user-settings'],
+    queryFn: () => axios.get('/api/user-settings').then((res) => res.data)
+  });
+
+  return { settings, status };
+};
