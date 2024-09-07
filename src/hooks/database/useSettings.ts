bfe1962fc0fa841ca50ae8f225d057c9a576f535
neinja007 <anton.siligan@gmail.com>
2024-09-07 12:08:26 +0200

    remove explicit return statement

--- src/hooks/database/useSettings.ts
+++ src/hooks/database/useSettings.ts
@@ -2,7 +2,7 @@ import { UserSetting } from '@prisma/client';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 
-export const useSettings = (): {} => {
+export const useSettings = () => {
   const { data: settings, status } = useQuery<UserSetting[]>({
     queryKey: ['user-settings'],
     queryFn: () => axios.get('/api/user-settings').then((res) => res.data)
