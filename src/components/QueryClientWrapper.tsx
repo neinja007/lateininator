3761ecfe176d4fa483f32dcaf43c93e68b11861d
neinja007 <anton.siligan@gmail.com>
2024-09-07 12:03:19 +0200

    add default timers

--- src/components/QueryClientWrapper.tsx
+++ src/components/QueryClientWrapper.tsx
@@ -4,7 +4,18 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 export const QueryClientWrapper = ({ children }: { children: React.ReactNode }) => {
-  const queryClient = new QueryClient();
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 5 * 60 * 1000,
+        gcTime: 10 * 60 * 1000
+      }
+    }
+  });
+
+  queryClient.setQueryDefaults(['dbUser'], {
+    staleTime: 0
+  });
 
   return (
     <QueryClientProvider client={queryClient}>
