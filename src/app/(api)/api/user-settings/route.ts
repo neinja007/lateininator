9b6f01f76fbb4423b472db19caa5fcb8543866a8
neinja007 <anton.siligan@gmail.com>
2024-09-07 10:48:11 +0200

    add API route for retrieving settings

---
+++ src/app/(api)/api/user-settings/route.ts
@@ -0,0 +1,28 @@
+import { prisma } from '@/utils/other/client';
+import { currentUser } from '@clerk/nextjs/server';
+import { NextResponse } from 'next/server';
+
+export const GET = async () => {
+  const user = await currentUser();
+  if (!user) {
+    return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+  }
+
+  const userSettings = await prisma.userSetting.findMany({
+    where: {
+      userId: user.id
+    }
+  });
+
+  const defaultSettings = await prisma.defaultSetting.findMany();
+
+  const combinedSettings = defaultSettings.map((setting) => {
+    const userSetting = userSettings.find((userSetting) => userSetting.settingKey === setting.settingKey);
+    return {
+      ...setting,
+      value: userSetting?.settingValue || setting.defaultValue
+    };
+  });
+
+  return NextResponse.json({ combinedSettings });
+};
