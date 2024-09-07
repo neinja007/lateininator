705f494b5446433da221b80ea7a0369cc7661577
neinja007 <anton.siligan@gmail.com>
2024-09-07 06:29:05 +0200

    always display browse collections

--- src/app/(general)/user/manage-vocabulary/page.tsx
+++ src/app/(general)/user/manage-vocabulary/page.tsx
@@ -1,27 +1,16 @@
 'use client';
-
 import Heading from '@/components/Heading';
-import { useState } from 'react';
 import Hr from '@/components/Hr';
 import BrowseCollections from './components/BrowseCollections';
 import ManageCollections from './components/ManageCollections';
 
 const Page = () => {
-  const [browseCollections, setBrowseCollections] = useState<boolean>(false);
-
   return (
     <div>
       <Heading>Wortschatz</Heading>
-      <ManageCollections
-        browseCollections={browseCollections}
-        enableBrowseCollections={() => setBrowseCollections(true)}
-      />
-      {browseCollections && (
-        <>
-          <Hr className='my-5' />
-          <BrowseCollections hideBrowseCollections={() => setBrowseCollections(false)} />
-        </>
-      )}
+      <ManageCollections />
+      <Hr className='my-5' />
+      <BrowseCollections />
     </div>
   );
 };
