da07cd45dfd4ace94a58920ce5c391512244894b
neinja007 <anton.siligan@gmail.com>
2024-09-08 12:17:58 +0200

    include vocative in table input values type

--- src/app/(general)/flexion/noun/types/index.ts
+++ src/app/(general)/flexion/noun/types/index.ts
@@ -1,7 +1,7 @@
 import { Numerus, WordCase } from '@/types/wordConstants';
 import { Dispatch, SetStateAction } from 'react';
 
-export type TableInputValues = Record<Numerus, Record<Exclude<WordCase, '6'>, string>>;
+export type TableInputValues = Record<Numerus, Record<WordCase, string>>;
 export type SetTableInputValues = Dispatch<SetStateAction<TableInputValues>>;
 export type IndividualInputForm = {
   numerus: Numerus;
