39b2e6b8c7aa069c66d069b31d2d627218858ac3
neinja007 <anton.siligan@gmail.com>
2024-09-08 11:19:29 +0200

    accept query parameter

--- src/hooks/database/useWords.ts
+++ src/hooks/database/useWords.ts
@@ -2,10 +2,14 @@ import { Word } from '@/types/word';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 
-export const useWords = (id?: number, include: string[] = []) => {
+export const useWords = (id?: number, include: string[] = [], query?: string) => {
+  if (query && id) {
+    throw new Error('Invalid searchParams (cannot read both id and query)');
+  }
+
   const { data: words, status } = useQuery({
-    queryKey: ['words', id, include],
-    queryFn: () => axios.get('/api/words', { params: { id, include } }).then((res) => res.data)
+    queryKey: ['words', query, id, include],
+    queryFn: () => axios.get('/api/words', { params: { query, id, include } }).then((res) => res.data)
   });
 
   if (id) {
