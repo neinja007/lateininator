89686e1c4842d32276f76252a8850c7df3bcefcb
neinja007 <anton.siligan@gmail.com>
2024-09-08 11:22:21 +0200

    fix word fetching if query is undefined

--- src/hooks/database/useWords.ts
+++ src/hooks/database/useWords.ts
@@ -2,10 +2,12 @@ import { Word } from '@/types/word';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 
-export const useWords = (id?: number, include: string[] = [], query?: string) => {
+export const useWords = (id?: number, include: string[] = [], query?: string | undefined) => {
   if (query && id) {
     throw new Error('Invalid searchParams (cannot read both id and query)');
   }
+
+  query = query || undefined;
 
   const { data: words, status } = useQuery({
     queryKey: ['words', query, id, include],
