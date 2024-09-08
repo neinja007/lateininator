759ede3e3b5805d83842ae632482d51098705533
neinja007 <anton.siligan@gmail.com>
2024-09-08 11:35:52 +0200

    rename settings constants and add disabled property

--- src/constants/settings.ts
+++ src/constants/settings.ts
@@ -6,6 +6,7 @@ export const settings: {
     options?: { [key: string]: string };
     name: string;
     description: string;
+    disabled?: boolean;
   };
 } = {
   DICTIONARY_VOCATIVE: {
@@ -25,6 +26,7 @@ export const settings: {
       en: 'Englisch'
     },
     name: 'Sprache',
-    description: 'Wählt die Sprache des gesamten Lateininators aus.'
+    description: 'Wählt die Sprache des gesamten Lateininators aus.',
+    disabled: true
   }
 };
