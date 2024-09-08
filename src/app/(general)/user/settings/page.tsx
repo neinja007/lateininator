24dce9c026c05f1d6ab28f11d0db2f6a2de02194
neinja007 <anton.siligan@gmail.com>
2024-09-08 12:10:55 +0200

    adjust for new settings object and setting component props

--- src/app/(general)/user/settings/page.tsx
+++ src/app/(general)/user/settings/page.tsx
@@ -7,6 +7,7 @@ import { Gem, LogOut, UserIcon } from 'lucide-react';
 import Setting from './components/Setting';
 import Skeleton from '@/components/Skeleton';
 import { useSettings } from '@/hooks/database/useSettings';
+import { SettingKey } from '@prisma/client';
 
 const Page = () => {
   const { settings, status } = useSettings();
@@ -17,7 +18,11 @@ const Page = () => {
       <div className='mb-5'>
         {status === 'pending' &&
           [...Array(3)].map((_, index) => <Skeleton key={index} pulse customSize className='my-2 h-20 w-full' />)}
-        {status === 'success' && settings && settings.map((setting) => <Setting key={setting.id} setting={setting} />)}
+        {status === 'success' &&
+          settings &&
+          Object.entries(settings).map(([key, value]) => (
+            <Setting key={key} settingKey={key as SettingKey} settingValue={value} />
+          ))}
       </div>
       <p className='mb-2 text-center text-lg font-bold'>Konto-Einstellungen</p>
       <div className='items-center justify-between gap-x-2 sm:flex'>
