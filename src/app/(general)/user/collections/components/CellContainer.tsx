0bc0340ee3b0372b12cfc8e8dc3bdbb657daa756
neinja007 <anton.siligan@gmail.com>
2024-09-09 17:58:50 +0200

    add wide property

--- src/app/(general)/user/collections/components/CellContainer.tsx
+++ src/app/(general)/user/collections/components/CellContainer.tsx
@@ -1,9 +1,21 @@
+import clsx from 'clsx';
+
 type CellContainerProps = {
   children?: React.ReactNode;
+  wide?: boolean;
 };
 
-const CellContainer = ({ children }: CellContainerProps) => {
-  return <div className='grid h-fit max-h-64 grid-cols-2 gap-3 overflow-y-scroll lg:grid-cols-3'>{children}</div>;
+const CellContainer = ({ children, wide }: CellContainerProps) => {
+  return (
+    <div
+      className={clsx(
+        'grid h-fit max-h-64 gap-3 overflow-y-scroll',
+        wide ? 'grid-cols-2 lg:grid-cols-3' : 'grid-cols-1 lg:grid-cols-2'
+      )}
+    >
+      {children}
+    </div>
+  );
 };
 
 export default CellContainer;
