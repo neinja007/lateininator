09a92bea79790a2cd6ab12bb3e50aabe4bbed476
neinja007 <anton.siligan@gmail.com>
2024-09-10 15:57:38 +0200

    implement extractions

--- src/app/(general)/user/collections/edit/components/EditCollection.tsx
+++ src/app/(general)/user/collections/edit/components/EditCollection.tsx
@@ -1,15 +1,15 @@
 'use client';
-
-import Button from '@/components/Button';
 import Input from '@/components/Input';
 import { useId, useState } from 'react';
-import ui from '@/styles/ui.module.css';
 import clsx from 'clsx';
-import { Check, Edit, Minus, Plus, X } from 'lucide-react';
+import { Minus, Plus } from 'lucide-react';
 import Hr from '@/components/Hr';
-import { List } from '@prisma/client';
+import { List as ListType } from '@prisma/client';
 import { Word } from '@/types/word';
 import { useWords } from '@/hooks/database/queries/useWords';
+import BasicDataEditor from './BasicDataEditor';
+import ListAddForm from './ListAddForm';
+import List from './List';
 
 type EditCollectionProps = {
   collectionId: number | undefined;
@@ -17,7 +17,7 @@ type EditCollectionProps = {
 
 const EditCollection = ({ collectionId }: EditCollectionProps) => {
   const [name, setName] = useState('');
-  const [lists, setLists] = useState<Omit<List, 'createdAt' | 'updatedAt'>[]>([]);
+  const [lists, setLists] = useState<Omit<ListType, 'createdAt' | 'updatedAt'>[]>([]);
   const [listName, setListName] = useState('');
   const [description, setDescription] = useState('');
 
@@ -44,83 +44,23 @@ const EditCollection = ({ collectionId }: EditCollectionProps) => {
   return (
     <div>
       <Input className='w-full' label='Name der Kollektion' value={name} onChange={setName} />
-      <div className='mt-4'>
-        <label htmlFor={id} className='block'>
-          Beschreibung
-        </label>
-        <textarea
-          id={id}
-          className={clsx(ui.shape, 'mt-1 h-24 w-full')}
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-        />
-      </div>
+      <BasicDataEditor name={name} description={description} setName={setName} setDescription={setDescription} />
       <Hr className='my-4' />
       <div>
-        <form onSubmit={handleListSubmit} className='flex items-end'>
-          <div className='flex items-baseline'>
-            <span className='mr-4'>Ihre Kollektion hat bereits {lists.length} Listen.</span>
-            <Input className='w-full max-w-64' value={listName} onChange={setListName} />
-          </div>
-          <Button
-            className='ml-4'
-            type='submit'
-            disabled={
-              listName.trim().length === 0 || listName.length > 100 || lists.some((list) => list.name === listName)
-            }
-          >
-            {lists.some((list) => list.name === listName) ? 'Liste existiert bereits' : 'Liste hinzufügen'}
-          </Button>
-        </form>
+        <ListAddForm handleListSubmit={handleListSubmit} listName={listName} lists={lists} setListName={setListName} />
         <div className={lists.length > 0 ? 'mt-4' : undefined}>
           <div className='flex flex-wrap gap-x-4'>
             {lists.map((list) => (
-              <button
+              <List
                 key={list.id}
-                className={clsx(
-                  'flex items-center rounded-lg px-3 py-2 dark:border-gray-700',
-                  activeList === list.id ? 'bg-gray-500' : 'bg-gray-900'
-                )}
-                onClick={() => setActiveList(list.id)}
-              >
-                {editList === list.id ? (
-                  <Input
-                    className='w-full'
-                    value={list.name}
-                    onChange={(value) => setLists(lists.map((l) => (l.id === list.id ? { ...l, name: value } : l)))}
-                    onClick={(e) => {
-                      e.stopPropagation();
-                      setActiveList(undefined);
-                    }}
-                  />
-                ) : (
-                  list.name
-                )}
-                {editList !== list.id ? (
-                  <Edit
-                    onClick={(e) => {
-                      e.stopPropagation();
-                      setEditList(list.id);
-                    }}
-                    className='ml-2 h-5 w-5 cursor-pointer rounded-full bg-yellow-400 p-0.5 dark:bg-yellow-700'
-                  />
-                ) : (
-                  <Check
-                    onClick={(e) => {
-                      e.stopPropagation();
-                      setEditList(undefined);
-                    }}
-                    className='ml-2 h-5 w-5 cursor-pointer rounded-full bg-green-400 p-0.5 dark:bg-green-700'
-                  />
-                )}
-                <X
-                  onClick={(e) => {
-                    e.stopPropagation();
-                    setLists(lists.filter((l) => l.id !== list.id));
-                  }}
-                  className='ml-2 h-5 w-5 cursor-pointer rounded-full bg-red-400 p-0.5 dark:bg-red-700'
-                />
-              </button>
+                list={list}
+                activeList={activeList}
+                setActiveList={setActiveList}
+                editList={editList}
+                setEditList={setEditList}
+                setLists={setLists}
+                lists={lists}
+              />
             ))}
           </div>
         </div>
