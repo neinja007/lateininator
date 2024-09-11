8be47d272504313e228d78c2096c607725025a9d
neinja007 <anton.siligan@gmail.com>
2024-09-11 16:23:54 +0200

    add metadata

---
+++ src/app/(general)/grammar/summaries/layout.tsx
@@ -0,0 +1,10 @@
+type LayoutProps = { children: React.ReactNode };
+
+export const metadata = {
+  title: 'Zusammenfassungen',
+  description: 'Zusammenfassungen'
+};
+
+const Layout = ({ children }: LayoutProps) => children;
+
+export default Layout;
