fc2844c4c98072a9d310c64ca2d295b83dfa9ccb
neinja007 <anton.siligan@gmail.com>
2024-09-11 16:24:09 +0200

    add metadata

---
+++ src/app/(general)/user/settings/layout.tsx
@@ -0,0 +1,10 @@
+type LayoutProps = { children: React.ReactNode };
+
+export const metadata = {
+  title: 'Einstellungen',
+  description: 'Einstellungen'
+};
+
+const Layout = ({ children }: LayoutProps) => children;
+
+export default Layout;
