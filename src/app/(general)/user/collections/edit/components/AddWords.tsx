f4c71f095385e377a69f2cdf4fa73569602014f0
neinja007 <anton.siligan@gmail.com>
2024-09-11 18:03:24 +0200

    fix alignment

--- src/app/(general)/user/collections/edit/components/AddWords.tsx
+++ src/app/(general)/user/collections/edit/components/AddWords.tsx
@@ -1,6 +1,6 @@
 import Hr from '@/components/Hr';
 import Input from '@/components/Input';
-import { Dispatch, SetStateAction, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Word as WordType } from '@/types/word';
 import { useWords } from '@/hooks/database/queries/useWords';
 import WordContainer from './WordContainer';
@@ -8,7 +8,7 @@ import WordContainer from './WordContainer';
 type AddWordsProps = {
   listName: string;
   words: WordType[];
-  setWords: Dispatch<SetStateAction<WordType[]>>;
+  setWords: (words: WordType[]) => void;
 };
 
 const AddWords = ({ listName, words, setWords }: AddWordsProps) => {
@@ -29,10 +29,11 @@ const AddWords = ({ listName, words, setWords }: AddWordsProps) => {
       <div className='mt-4 grid grid-cols-2'>
         <div className='space-y-3 border-r pr-3'>
           <Input className='w-full' placeholder='Wort suchen...' value={query} onChange={setQuery} />
-          <WordContainer addedWords={words} words={data?.slice(0, 20) || []} setWords={setWords} />
+          <div className='overflow-hidden overflow-y-scroll'>
+            <WordContainer addedWords={words} words={data?.slice(0, 20) || []} setWords={setWords} />
+          </div>
         </div>
         <div className='pl-3'>
-          <h3 className='mb-1 flex h-12 items-center'>Schon hinzugefügte Wörter:</h3>
           <WordContainer addedWords={words} words={words} setWords={setWords} />
         </div>
       </div>
