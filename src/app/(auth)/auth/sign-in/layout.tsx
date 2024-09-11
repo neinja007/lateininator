45ac50e35d27f8f675fcae801a71d798a09f98aa
neinja007 <anton.siligan@gmail.com>
2024-09-11 16:23:23 +0200

    add metadata

---
+++ src/app/(auth)/auth/sign-in/layout.tsx
@@ -0,0 +1,10 @@
+type LayoutProps = { children: React.ReactNode };
+
+export const metadata = {
+  title: 'Anmelden',
+  description: 'Anmelden'
+};
+
+const Layout = ({ children }: LayoutProps) => children;
+
+export default Layout;
