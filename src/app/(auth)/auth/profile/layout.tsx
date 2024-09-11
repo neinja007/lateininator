f062921b7beb747f343ab3ecbce9146c0ff1cfda
neinja007 <anton.siligan@gmail.com>
2024-09-11 16:23:19 +0200

    add metadata

---
+++ src/app/(auth)/auth/profile/layout.tsx
@@ -0,0 +1,10 @@
+type LayoutProps = { children: React.ReactNode };
+
+export const metadata = {
+  title: 'Profil verwalten',
+  description: 'Profil verwalten'
+};
+
+const Layout = ({ children }: LayoutProps) => children;
+
+export default Layout;
