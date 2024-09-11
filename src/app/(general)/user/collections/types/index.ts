170136b776b9bfe783831368985c8d9b66be48d4
neinja007 <anton.siligan@gmail.com>
2024-09-11 18:46:48 +0200

    format

--- src/app/(general)/user/collections/types/index.ts
+++ src/app/(general)/user/collections/types/index.ts
@@ -1,4 +1,6 @@
 import { Word } from '@/types/word';
 import { List } from '@prisma/client';
 
-export type ListWithWords = Omit<List, 'createdAt' | 'updatedAt' | 'collectionId'> & { words: Word[] };
+export type ListWithWords = Omit<List, 'createdAt' | 'updatedAt' | 'collectionId'> & {
+  words: Word[];
+};
