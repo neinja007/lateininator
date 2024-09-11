49ce47ff03d4b89017eb1b466d41f7fb3a34fabb
neinja007 <anton.siligan@gmail.com>
2024-09-11 16:24:06 +0200

    add metadata

---
+++ src/app/(general)/user/collections/edit/layout.tsx
@@ -0,0 +1,10 @@
+type LayoutProps = { children: React.ReactNode };
+
+export const metadata = {
+  title: 'Kollektion bearbeiten',
+  description: 'Kollektion bearbeiten'
+};
+
+const Layout = ({ children }: LayoutProps) => children;
+
+export default Layout;
