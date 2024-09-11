9a1b5a516810d6fbb0f3e531bbd2db2df8446390
neinja007 <anton.siligan@gmail.com>
2024-09-11 16:23:30 +0200

    add metadata

---
+++ src/app/(general)/dashboard/layout.tsx
@@ -0,0 +1,10 @@
+type LayoutProps = { children: React.ReactNode };
+
+export const metadata = {
+  title: 'Übersicht',
+  description: 'Übersicht'
+};
+
+const Layout = ({ children }: LayoutProps) => children;
+
+export default Layout;
