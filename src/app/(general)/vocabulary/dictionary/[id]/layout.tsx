375e1954e89ee43ffa31fdaebcb9c576d0c7677e
neinja007 <anton.siligan@gmail.com>
2024-09-11 15:57:31 +0200

    add dynamic word metadata

---
+++ src/app/(general)/vocabulary/dictionary/[id]/layout.tsx
@@ -0,0 +1,29 @@
+import { prisma } from '@/utils/other/client';
+
+type LayoutProps = { children: React.ReactNode };
+
+type Props = {
+  params: { id: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+
+export async function generateMetadata({ params, searchParams }: Props) {
+  const id = params.id;
+
+  const word = await prisma.word.findUnique({
+    where: {
+      id: parseInt(id)
+    }
+  });
+
+  return {
+    title: `"${word?.name.toUpperCase()}"`,
+    description: `Formen und Informationen zu ${word?.name} im Lateininator Wörterbuch`
+  };
+}
+
+const Layout = ({ children }: LayoutProps) => {
+  return children;
+};
+
+export default Layout;
