656b5bbb8fd4da6ac49c3cbddde20e648692071b
neinja007 <anton.siligan@gmail.com>
2024-09-11 16:24:15 +0200

    add metadata

---
+++ src/app/(general)/vocabulary/dictionary/layout.tsx
@@ -0,0 +1,10 @@
+type LayoutProps = { children: React.ReactNode };
+
+export const metadata = {
+  title: 'Wörterbuch',
+  description: 'Wörterbuch'
+};
+
+const Layout = ({ children }: LayoutProps) => children;
+
+export default Layout;
