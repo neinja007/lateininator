e3e7411e4d604e6bb1b5f82071f4d52e20b2ecea
neinja007 <anton.siligan@gmail.com>
2024-09-11 17:19:00 +0200

    protect /user routes for user that are not signed in

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,12 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 import { NextResponse } from 'next/server';
 
 export default clerkMiddleware((auth, request) => {
+  if (isProtectedRoute(request)) auth().protect();
+
   if (request.nextUrl.pathname === '/') {
     return NextResponse.redirect(new URL('/dashboard', request.url));
   }
 });
+
+const isProtectedRoute = createRouteMatcher(['/user(.*)']);
