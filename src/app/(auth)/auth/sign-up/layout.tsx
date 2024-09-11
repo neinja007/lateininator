6bed3e51dd7bbea63987cf9a6a281eae30132024
neinja007 <anton.siligan@gmail.com>
2024-09-11 16:23:28 +0200

    add metadata

---
+++ src/app/(auth)/auth/sign-up/layout.tsx
@@ -0,0 +1,10 @@
+type LayoutProps = { children: React.ReactNode };
+
+export const metadata = {
+  title: 'Konto Erstellen',
+  description: 'Konto Erstellen'
+};
+
+const Layout = ({ children }: LayoutProps) => children;
+
+export default Layout;
