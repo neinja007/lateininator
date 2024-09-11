b6663dbda60b31c8d28f4ced4d53e986a7d57c0b
neinja007 <anton.siligan@gmail.com>
2024-09-11 16:24:17 +0200

    add metadata

---
+++ src/app/(general)/vocabulary/trainer/layout.tsx
@@ -0,0 +1,10 @@
+type LayoutProps = { children: React.ReactNode };
+
+export const metadata = {
+  title: 'Vokabeltrainer',
+  description: 'Vokabeltrainer'
+};
+
+const Layout = ({ children }: LayoutProps) => children;
+
+export default Layout;
