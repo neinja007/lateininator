9626150fd701a4e5c30bbff84ce8a2503e0e01f6
neinja007 <anton.siligan@gmail.com>
2024-09-11 16:24:01 +0200

    add metadata

---
+++ src/app/(general)/premium/success/layout.tsx
@@ -0,0 +1,10 @@
+type LayoutProps = { children: React.ReactNode };
+
+export const metadata = {
+  title: 'Vielen Dank für Ihren Einkauf!',
+  description: 'Vielen Dank für Ihren Einkauf!'
+};
+
+const Layout = ({ children }: LayoutProps) => children;
+
+export default Layout;
