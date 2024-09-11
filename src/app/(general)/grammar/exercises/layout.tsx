b4fb5da2bf330ee55479def09ba7176ee293f8a3
neinja007 <anton.siligan@gmail.com>
2024-09-11 16:23:52 +0200

    add metadata

---
+++ src/app/(general)/grammar/exercises/layout.tsx
@@ -0,0 +1,10 @@
+type LayoutProps = { children: React.ReactNode };
+
+export const metadata = {
+  title: 'Übungen',
+  description: 'Übungen'
+};
+
+const Layout = ({ children }: LayoutProps) => children;
+
+export default Layout;
