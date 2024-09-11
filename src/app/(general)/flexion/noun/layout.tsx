76d7287f0a08b1676ed5404bc843e7ff96c2db55
neinja007 <anton.siligan@gmail.com>
2024-09-11 16:23:37 +0200

    add metadata

---
+++ src/app/(general)/flexion/noun/layout.tsx
@@ -0,0 +1,10 @@
+type LayoutProps = { children: React.ReactNode };
+
+export const metadata = {
+  title: 'Nomen',
+  description: 'Nomen'
+};
+
+const Layout = ({ children }: LayoutProps) => children;
+
+export default Layout;
