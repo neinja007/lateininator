14095603f085894eae6d282887aca3147c2a64b3
neinja007 <anton.siligan@gmail.com>
2024-09-11 16:23:26 +0200

    add metadata

---
+++ src/app/(auth)/auth/sign-out/layout.tsx
@@ -0,0 +1,10 @@
+type LayoutProps = { children: React.ReactNode };
+
+export const metadata = {
+  title: 'Abmelden',
+  description: 'Abmelden'
+};
+
+const Layout = ({ children }: LayoutProps) => children;
+
+export default Layout;
