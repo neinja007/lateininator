01bb0b719129ee9eb65130060cd7c1af148ea988
neinja007 <anton.siligan@gmail.com>
2024-09-11 16:23:57 +0200

    add metadata

---
+++ src/app/(general)/premium/manage/layout.tsx
@@ -0,0 +1,10 @@
+type LayoutProps = { children: React.ReactNode };
+
+export const metadata = {
+  title: 'Premium-Version verwalten',
+  description: 'Premium-Version verwalten'
+};
+
+const Layout = ({ children }: LayoutProps) => children;
+
+export default Layout;
