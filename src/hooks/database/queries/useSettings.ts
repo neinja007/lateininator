0656893a79b06b261808eb6aedd70f0bb1ef4da1
neinja007 <anton.siligan@gmail.com>
2024-09-12 05:07:51 +0200

    change returned values

--- src/hooks/database/queries/useSettings.ts
+++ src/hooks/database/queries/useSettings.ts
@@ -4,20 +4,20 @@ import axios from 'axios';
 import { useMemo } from 'react';
 
 export const useSettings = () => {
-  const { data: settings, status } = useQuery<UserSetting[]>({
+  const query = useQuery<UserSetting[]>({
     queryKey: ['user-settings'],
     queryFn: () => axios.get('/api/user-settings').then((res) => res.data)
   });
 
   const settingsObject = useMemo(() => {
-    return settings?.reduce(
+    return query.data?.reduce(
       (acc, setting) => {
         acc[setting.settingKey] = setting.settingValue;
         return acc;
       },
       {} as { [S in SettingKey]: string }
     );
-  }, [settings]);
+  }, [query.data]);
 
-  return { settings: settingsObject, status };
+  return { settings: settingsObject, query };
 };
