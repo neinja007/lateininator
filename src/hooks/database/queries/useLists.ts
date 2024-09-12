93a83674fa3556e179fe124d6405863b6f09b7a6
neinja007 <anton.siligan@gmail.com>
2024-09-12 05:07:36 +0200

    use object as parameter

--- src/hooks/database/queries/useLists.ts
+++ src/hooks/database/queries/useLists.ts
@@ -1,19 +1,15 @@
 import { Word } from '@/types/word';
 import { Collection, List } from '@prisma/client';
-import { useQuery, QueryStatus } from '@tanstack/react-query';
+import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 
-export const useLists = (
-  include?: string[],
-  wordInclude?: string[]
-): {
-  lists: (List & { words: Word[]; collection: Collection })[] | undefined;
-  status: QueryStatus;
-} => {
-  const { data: lists, status } = useQuery<(List & { words: Word[]; collection: Collection })[]>({
+export const useLists = (parameters: { include?: string[]; wordInclude?: string[] }) => {
+  const { include, wordInclude } = parameters;
+
+  const query = useQuery<(List & { words: Word[]; collection: Collection })[]>({
     queryKey: ['lists', include, wordInclude],
     queryFn: () => axios.get('/api/list', { params: { include, wordInclude } }).then((res) => res.data)
   });
 
-  return { lists: lists, status };
+  return query;
 };
