58862fed93e42f9e734760c989c281d23c5196cd
neinja007 <anton.siligan@gmail.com>
2024-09-12 05:08:05 +0200

    return entire query

--- src/hooks/database/queries/useWords.ts
+++ src/hooks/database/queries/useWords.ts
@@ -2,21 +2,21 @@ import { Word } from '@/types/word';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 
-export const useWords = (id?: number, include: string[] = [], query?: string | undefined) => {
-  if (query && id) {
+export const useWords = (parameters: { id?: number; include: string[]; query?: string | undefined }) => {
+  const { id, include, query: searchQuery } = parameters;
+
+  if (searchQuery && id) {
     throw new Error('Invalid searchParams (cannot read both id and query)');
   }
 
-  query = query || undefined;
-
-  const { data: words, status } = useQuery({
-    queryKey: ['words', query, id, include],
-    queryFn: () => axios.get('/api/words', { params: { query, id, include } }).then((res) => res.data)
+  const query = useQuery({
+    queryKey: ['words', searchQuery, id, include],
+    queryFn: () => axios.get('/api/words', { params: { searchQuery, id, include } }).then((res) => res.data)
   });
 
   if (id) {
-    return { word: words as Word, status };
+    return { word: query.data as Word, ...query };
   } else {
-    return { words: words as Word[], status };
+    return { words: query.data as Word[], ...query };
   }
 };
