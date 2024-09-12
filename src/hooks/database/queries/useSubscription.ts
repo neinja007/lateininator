03b945b5e71d48b6e2993cfc7b7a9d28eaee8d28
neinja007 <anton.siligan@gmail.com>
2024-09-12 05:07:59 +0200

    return entire query

--- src/hooks/database/queries/useSubscription.ts
+++ src/hooks/database/queries/useSubscription.ts
@@ -2,10 +2,10 @@ import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 
 export const useSubscription = () => {
-  const { data: subscription, isLoading } = useQuery({
+  const query = useQuery({
     queryKey: ['subscription'],
     queryFn: () => axios.get('/api/subscription').then((res) => res.data.data)
   });
 
-  return { subscription, isLoading };
+  return query;
 };
