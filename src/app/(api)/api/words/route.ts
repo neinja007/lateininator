41b0b99a942a71fde67001302b553dbfe9c76643
neinja007 <anton.siligan@gmail.com>
2024-09-14 06:33:05 +0200

    use zod to validate params

--- src/app/(api)/api/words/route.ts
+++ src/app/(api)/api/words/route.ts
@@ -1,15 +1,17 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { getIncludedData } from '../../utils/getIncludedData';
-import { validateNumber } from '../../utils/validateNumber';
-import { validateString } from '../../utils/validateString';
 import { getWordById } from './services/getWordById';
 import { getWordsByQuery } from './services/getWordsByQuery';
 import { getWords } from './services/getWords';
+import { z } from 'zod';
 
 export const GET = async (request: NextRequest) => {
   const searchParams = request.nextUrl.searchParams;
-  const query = searchParams.get('query');
-  const id = searchParams.get('id');
+  const query = z.string().optional().parse(searchParams.get('query'));
+  const id = z
+    .number()
+    .optional()
+    .parse(parseInt(z.string().optional().parse(searchParams.get('id')) || ''));
   const includedDataObject = getIncludedData<['adjective', 'noun', 'verb', 'derivative', 'lists', 'base']>(
     searchParams.getAll('include[]'),
     ['adjective', 'noun', 'verb', 'derivative', 'lists', 'base']
@@ -23,14 +25,6 @@ export const GET = async (request: NextRequest) => {
     return NextResponse.json({ error: 'Invalid searchParams (cannot read both id and query)' }, { status: 400 });
   }
 
-  if (!validateNumber(id)) {
-    return NextResponse.json({ error: 'Invalid id (must be number)' }, { status: 400 });
-  }
-
-  if (!validateString(query)) {
-    return NextResponse.json({ error: 'Invalid query (must be string)' }, { status: 400 });
-  }
-
   if (query) {
     try {
       const words = await getWordsByQuery(query, includedDataObject);
@@ -42,7 +36,7 @@ export const GET = async (request: NextRequest) => {
     }
   } else if (id) {
     try {
-      const word = await getWordById(parseInt(id), includedDataObject);
+      const word = await getWordById(id, includedDataObject);
 
       if (!word) {
         return NextResponse.json({ error: 'Word not found' }, { status: 404 });
