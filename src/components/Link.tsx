0379c65e419197622257776afe7f1db18403fe3e
neinja007 <anton.siligan@gmail.com>
2024-09-14 17:52:49 +0200

    add all native link props to link component

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -4,11 +4,11 @@ import NextLink from 'next/link';
 type LinkProps = {
   children: React.ReactNode;
   href: string;
-};
+} & React.ComponentProps<typeof NextLink>;
 
-const Link = ({ children, href }: LinkProps) => {
+const Link = ({ children, href, ...props }: LinkProps) => {
   return (
-    <NextLink href={href} className='text-blue-500 hover:underline'>
+    <NextLink href={href} className='text-blue-500 hover:underline' {...props}>
       {children}
       <ExternalLink className='ml-1 inline-block h-4 w-4 align-text-top' />
     </NextLink>
