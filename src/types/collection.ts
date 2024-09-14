ad838e49f2d6ef9032c0631c06a0f3e509331e39
neinja007 <anton.siligan@gmail.com>
2024-09-14 08:02:06 +0200

    add savedby property

--- src/types/collection.ts
+++ src/types/collection.ts
@@ -4,6 +4,7 @@ import { Word } from './word';
 export type FullCollection = Collection & {
   lists: ListWithWords[];
   owner: User;
+  savedBy: User[];
 };
 
 export type ListWithWords = Omit<List, 'createdAt' | 'updatedAt' | 'collectionId'> & {
