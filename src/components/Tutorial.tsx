15d0d06cfe4e03258a52c31d75939b6550d9c5e3
neinja007 <anton.siligan@gmail.com>
2024-09-14 17:30:28 +0200

    change color

--- src/components/Tutorial.tsx
+++ src/components/Tutorial.tsx
@@ -5,7 +5,7 @@ type TutorialProps = {
 
 const Tutorial = ({ children, heading }: TutorialProps) => {
   return (
-    <div className='mb-4 rounded-lg border border-teal-200 bg-teal-50 p-4 dark:border-teal-700 dark:bg-teal-900'>
+    <div className='mb-4 rounded-lg border border-cyan-200 bg-cyan-50 p-4 dark:border-cyan-700 dark:bg-cyan-900'>
       <h2 className='mb-2 text-center text-xl font-bold'>{heading}</h2>
       {children}
     </div>
