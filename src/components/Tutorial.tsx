f81e980e0a107fd792b667f53805dace9e6d390c
neinja007 <anton.siligan@gmail.com>
2024-09-14 17:29:29 +0200

    add tutorial heading

--- src/components/Tutorial.tsx
+++ src/components/Tutorial.tsx
@@ -1,10 +1,12 @@
 type TutorialProps = {
   children: React.ReactNode;
+  heading: string;
 };
 
-const Tutorial = ({ children }: TutorialProps) => {
+const Tutorial = ({ children, heading }: TutorialProps) => {
   return (
-    <div className='mb-4 rounded-lg border border-gray-200 bg-gray-50 p-4 dark:border-gray-700 dark:bg-gray-900'>
+    <div className='mb-4 rounded-lg border border-teal-200 bg-teal-50 p-4 dark:border-teal-700 dark:bg-teal-900'>
+      <h2 className='mb-2 text-center text-xl font-bold'>{heading}</h2>
       {children}
     </div>
   );
