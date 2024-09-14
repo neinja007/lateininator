d75ee856dbf5ea0dbd6d70952d6d7a174ed5a842
neinja007 <anton.siligan@gmail.com>
2024-09-14 17:29:54 +0200

    add tutorial window

--- src/app/(general)/dashboard/page.tsx
+++ src/app/(general)/dashboard/page.tsx
@@ -4,17 +4,22 @@ import { routes } from '@/constants/routes';
 import FancyLinkContainer from './components/FancyLinkContainer';
 import StatusDependentRoutes from './components/StatusDependentRoutes';
 import Tutorial from '@/components/Tutorial';
+import { useUser } from '@clerk/nextjs';
 
 const Page = () => {
   const signInRoute = routes.find((route) => route.label === 'Anmelden');
   const premiumRoute = routes.find((route) => route.label === 'Premium');
   if (!signInRoute || !premiumRoute) throw new Error('Route not found');
 
+  const user = useUser();
+
   return (
     <div>
       <Heading className='mb-3 md:mb-6 lg:mb-12'>Übersicht</Heading>
       <div>
-        <Tutorial>Willkommen!</Tutorial>
+        <Tutorial heading={user.isSignedIn ? `Willkommen, ${user.user?.fullName}!` : 'Willkommen!'}>
+          Unten findest du alle Tools, die dir zur Verfügung stehen.
+        </Tutorial>
       </div>
       <div>
         <p className='mb-2 text-center text-lg font-bold'>Lernen</p>
