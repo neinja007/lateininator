1898a21408d1e060545ac908ddb7e732947af094
neinja007 <anton.siligan@gmail.com>
2024-09-14 06:36:40 +0200

    accept word include type

--- src/hooks/database/queries/useCollections.ts
+++ src/hooks/database/queries/useCollections.ts
@@ -6,15 +6,18 @@ export const useCollections = <T extends Collection | Collection[]>(parameters: 
   saved?: boolean;
   include?: ('lists' | 'owner' | 'savedBy')[];
   listInclude?: ('words' | 'collection')[];
+  wordInclude?: ('adjective' | 'noun' | 'verb' | 'derivative' | 'lists' | 'base')[];
   id?: number;
   enabled?: boolean;
 }) => {
-  const { saved, include, id, listInclude, enabled } = parameters;
+  const { saved, include, id, listInclude, wordInclude, enabled } = parameters;
 
   const query = useQuery<T>({
-    queryKey: ['collections', { id, saved, include, listInclude }],
+    queryKey: ['collections', { id, saved, include, listInclude, wordInclude }],
     queryFn: () =>
-      axios.get('/api/collection', { params: { id, saved, include, listInclude } }).then((res) => res.data),
+      axios
+        .get('/api/collection', { params: { id, saved, include, listInclude, wordInclude } })
+        .then((res) => res.data),
     enabled
   });
 
