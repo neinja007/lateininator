52092d25433462ba5216d09317f650d8a5a4e54c
neinja007 <anton.siligan@gmail.com>
2024-09-14 17:47:27 +0200

    improve styling and add space y

--- src/components/Tutorial.tsx
+++ src/components/Tutorial.tsx
@@ -5,9 +5,9 @@ type TutorialProps = {
 
 const Tutorial = ({ children, heading }: TutorialProps) => {
   return (
-    <div className='mb-4 rounded-lg border border-cyan-200 bg-cyan-50 p-4 dark:border-cyan-700 dark:bg-cyan-900'>
+    <div className='mb-5 rounded-lg border border-cyan-200 bg-cyan-50 p-4 dark:border-cyan-700 dark:bg-cyan-900'>
       <h2 className='mb-2 text-center text-xl font-bold'>{heading}</h2>
-      {children}
+      <div className='space-y-4'>{children}</div>
     </div>
   );
 };
