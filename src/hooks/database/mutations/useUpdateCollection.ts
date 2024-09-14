0949e8b81dd96a102bbbd6098df74dbc5afca491
neinja007 <anton.siligan@gmail.com>
2024-09-14 08:34:33 +0200

    invalidate collections after creating or editing a collection

--- src/hooks/database/mutations/useUpdateCollection.ts
+++ src/hooks/database/mutations/useUpdateCollection.ts
@@ -1,10 +1,15 @@
 import { CollectionSchema } from '@/schemas/collectionSchema';
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import axios from 'axios';
 
 export const useUpdateCollection = () => {
+  const queryClient = useQueryClient();
+
   const { mutate: updateCollection, status } = useMutation({
-    mutationFn: async (collection: CollectionSchema) => axios.put('/api/collection', collection)
+    mutationFn: async (collection: CollectionSchema) => axios.put('/api/collection', collection),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ predicate: (query) => query.queryKey?.includes('collections') });
+    }
   });
 
   return { updateCollection, status };
