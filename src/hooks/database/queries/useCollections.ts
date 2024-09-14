d16ca211b677f59d9d4bde2b7a0a753e46ba5ae4
neinja007 <anton.siligan@gmail.com>
2024-09-14 08:02:16 +0200

    use status param in fetching hook

--- src/hooks/database/queries/useCollections.ts
+++ src/hooks/database/queries/useCollections.ts
@@ -3,20 +3,20 @@ import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 
 export const useCollections = <T extends Collection | Collection[]>(parameters: {
-  saved?: boolean;
+  status: 'saved' | 'all' | 'owned';
   include?: ('lists' | 'owner' | 'savedBy')[];
   listInclude?: ('words' | 'collection')[];
   wordInclude?: ('adjective' | 'noun' | 'verb' | 'derivative' | 'lists' | 'base')[];
   id?: number;
   enabled?: boolean;
 }) => {
-  const { saved, include, id, listInclude, wordInclude, enabled } = parameters;
+  const { status, include, id, listInclude, wordInclude, enabled } = parameters;
 
   const query = useQuery<T>({
-    queryKey: ['collections', { id, saved, include, listInclude, wordInclude }],
+    queryKey: ['collections', { id, status, include, listInclude, wordInclude }],
     queryFn: () =>
       axios
-        .get('/api/collection', { params: { id, saved, include, listInclude, wordInclude } })
+        .get('/api/collection', { params: { id, status, include, listInclude, wordInclude } })
         .then((res) => res.data),
     enabled
   });
