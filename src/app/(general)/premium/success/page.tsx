be8018e1851220053c12c4f03c9e13e7ef3d0449
neinja007 <anton.siligan@gmail.com>
2024-09-15 16:05:25 +0200

    remove heading

--- src/app/(general)/premium/success/page.tsx
+++ src/app/(general)/premium/success/page.tsx
@@ -1,12 +1,9 @@
 'use client';
-
-import Heading from '@/components/Heading';
 import Link from '@/components/Link';
 
 const Page = () => {
   return (
     <div>
-      <Heading>Lateininator Premium</Heading>
       <p className='mt-5 text-center'>
         <span className='text-green-500'>
           Die Bezahlung war erfolgreich. Vielen Dank für Ihren Einkauf! Sie können von nun an alle Premium-Funktionen
