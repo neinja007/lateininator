47bf3f5cbc768c38233f3e948e9102f57272494d
neinja007 <anton.siligan@gmail.com>
2024-09-15 15:38:59 +0200

    accept additional classname prop

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,14 +1,16 @@
+import clsx from 'clsx';
 import { ExternalLink } from 'lucide-react';
 import NextLink from 'next/link';
 
 type LinkProps = {
   children: React.ReactNode;
   href: string;
+  className?: string;
 } & React.ComponentProps<typeof NextLink>;
 
-const Link = ({ children, href, ...props }: LinkProps) => {
+const Link = ({ children, href, className, ...props }: LinkProps) => {
   return (
-    <NextLink href={href} className='text-blue-500 hover:underline' {...props}>
+    <NextLink href={href} className={clsx('text-blue-500 hover:underline', className)} {...props}>
       {children}
       <ExternalLink className='ml-1 inline-block h-4 w-4 align-text-top' />
     </NextLink>
