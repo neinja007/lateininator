433e8110187348267be41b0ab8fe3b1ed15dfab7
neinja007 <anton.siligan@gmail.com>
2024-09-15 15:59:27 +0200

    use tutorial heading and add tutorial text

--- src/app/(general)/dashboard/page.tsx
+++ src/app/(general)/dashboard/page.tsx
@@ -1,9 +1,8 @@
 'use client';
-import Heading from '@/components/Heading';
+import TutorialHeading from '@/components/TutorialHeading';
 import { routes } from '@/constants/routes';
 import FancyLinkContainer from './components/FancyLinkContainer';
 import StatusDependentRoutes from './components/StatusDependentRoutes';
-import Tutorial from '@/components/Tutorial';
 import { useUser } from '@clerk/nextjs';
 
 const Page = () => {
@@ -15,14 +14,10 @@ const Page = () => {
 
   return (
     <div>
-      <Heading>Übersicht</Heading>
-      <Tutorial heading={user.isSignedIn ? `Willkommen, ${user.user?.fullName}!` : 'Willkommen!'}>
-        <p>
-          Hier finden Sie alle Tools, die Ihnen zur Verfügung stehen. Es gibt Übungen für <b>Endungen</b>,{' '}
-          <b>Vokabeln</b> und <b>Grammatik</b>. Außerdem können Sie Ihren Fortschritt verfolgen und Ihre Einstellungen
-          anpassen.
-        </p>
-      </Tutorial>
+      <TutorialHeading heading={user.isSignedIn ? `Willkommen, ${user.user?.fullName}!` : 'Willkommen!'}>
+        Hier finden Sie alle Tools, die Ihnen zur Verfügung stehen. Es gibt Übungen für <b>Endungen</b>, <b>Vokabeln</b>{' '}
+        und <b>Grammatik</b>. Außerdem können Sie Ihren Fortschritt verfolgen und Ihre Einstellungen anpassen.
+      </TutorialHeading>
       <div>
         <p className='mb-2 text-center text-lg font-bold'>Lernen</p>
         <div>
