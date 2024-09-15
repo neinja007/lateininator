5c8b362a10c1ca367a4dad3b9f479255605ca1e0
neinja007 <anton.siligan@gmail.com>
2024-09-15 16:37:39 +0200

    remove id from lists

--- src/schemas/collectionSchema.ts
+++ src/schemas/collectionSchema.ts
@@ -5,7 +5,7 @@ export const collectionSchema = z.object({
   name: z.string().min(1),
   description: z.string(),
   private: z.boolean(),
-  lists: z.array(z.object({ id: z.number().optional(), name: z.string().min(1), words: z.array(z.number()) }))
+  lists: z.array(z.object({ name: z.string().min(1), words: z.array(z.number()) }))
 });
 
 export type CollectionSchema = z.infer<typeof collectionSchema>;
