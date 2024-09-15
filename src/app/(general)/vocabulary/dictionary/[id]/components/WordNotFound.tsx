faf3354336f16f5127c6b60a95984c2ec33f3d59
neinja007 <anton.siligan@gmail.com>
2024-09-15 15:41:45 +0200

    refactor word not found information

--- src/app/(general)/vocabulary/dictionary/[id]/components/WordNotFound.tsx
+++ src/app/(general)/vocabulary/dictionary/[id]/components/WordNotFound.tsx
@@ -1,16 +1,13 @@
 import Link from '@/components/Link';
-import Heading from '@/components/Heading';
 import LinkToSupportEmail from '@/components/LinkToSupportEmail';
 
 const WordNotFound = () => {
   return (
-    <div>
-      <Heading>Das Wort wurde nicht gefunden.</Heading>
-      <div>
-        Wenn Sie denken, das dies ein Fehler ist, so melden Sie diesen bitte bei unserem <LinkToSupportEmail /> .
-        Andernfalls können Sie <Link href='/vocabulary/dictionary'>hier zum Wörterbuch zurückkehren</Link>.
-      </div>
-    </div>
+    <p>
+      <b>Das Wort wurde nicht gefunden.</b> Wenn Sie denken, das dies ein Fehler ist, so melden Sie diesen bitte bei
+      unserem <LinkToSupportEmail /> . Andernfalls können Sie{' '}
+      <Link href='/vocabulary/dictionary'>hier zum Wörterbuch zurückkehren</Link>.
+    </p>
   );
 };
 
