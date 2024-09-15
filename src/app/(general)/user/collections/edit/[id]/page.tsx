512783c6515d271322d96801d9a1db0bcbf29130
neinja007 <anton.siligan@gmail.com>
2024-09-15 16:10:07 +0200

    add tutorial to edit collection page

--- src/app/(general)/user/collections/edit/[id]/page.tsx
+++ src/app/(general)/user/collections/edit/[id]/page.tsx
@@ -1,12 +1,16 @@
 'use client';
 
-import Heading from '@/components/Heading';
+import TutorialHeading from '@/components/TutorialHeading';
 import EditCollection from '../../components/EditCollection';
+import Link from '@/components/Link';
 
 const Page = ({ params }: { params: { id: string } }) => {
   return (
     <div>
-      <Heading>Kollektion bearbeiten</Heading>
+      <TutorialHeading heading='Kollektion bearbeiten'>
+        Hier können Sie Ihre Kollektionen bearbeiten. Wenn Sie einige Wörter nicht finden, können Sie diese bei der{' '}
+        <Link href='/user/words'>Wort-Verwaltung</Link> hinzufügen.
+      </TutorialHeading>
       <EditCollection collectionId={parseInt(params.id)} />
     </div>
   );
