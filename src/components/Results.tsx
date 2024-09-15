912c859a62e4239ed2a0678561fc889ff7304e53
neinja007 <anton.siligan@gmail.com>
2024-09-15 06:15:21 +0200

    ensure that multiple requests are denied

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,5 +1,7 @@
 import Button from '@/components/Button';
+import { useAddPoints } from '@/hooks/database/mutations/useAddPoints';
 import { Stage } from '@/types/other';
+import { useState } from 'react';
 
 type ResultsProps = {
   handleContinue: (arg?: Stage) => void;
@@ -7,15 +9,29 @@ type ResultsProps = {
 };
 
 const Results = ({ handleContinue, points }: ResultsProps) => {
+  const { mutate: addPoints, status } = useAddPoints('increment');
+  const [saved, setSaved] = useState(false);
+
   return (
     <div className='flex flex-col items-center justify-center space-y-4'>
-      {points && (
+      {!!points && (
         <div className='text-center'>
           <h2 className='text-2xl font-semibold'>Ergebnisse</h2>
           <p className='my-2 text-lg'>
             Du hast {points} Punkt{points !== 1 ? 'e' : ''} erreicht!
           </p>
-          <Button color='purple'>Punkte speichern</Button>
+          {!saved && (
+            <Button
+              color='purple'
+              onClick={() => {
+                addPoints(points);
+                setSaved(true);
+              }}
+              disabled={status === 'pending'}
+            >
+              Punkte speichern
+            </Button>
+          )}
         </div>
       )}
       <Button onClick={() => handleContinue()} color='gray'>
