a5d571db3cd3a15288c10db4b5cc1b82f6a020f7
neinja007 <anton.siligan@gmail.com>
2024-09-15 15:59:11 +0200

    fully revamp dictionary word header

--- src/app/(general)/vocabulary/dictionary/[id]/components/Header.tsx
+++ src/app/(general)/vocabulary/dictionary/[id]/components/Header.tsx
@@ -1,10 +1,8 @@
-import Heading from '@/components/Heading';
-import Badge from '@/components/Badge';
+import TutorialHeading from '@/components/TutorialHeading';
 import { Word } from '@/types/word';
 import { getLexicalForm } from '@/utils/word/getLexicalForm';
 import BackToDictionaryButton from './BackToDictionaryButton';
 import { MAPPER } from '@/utils/other/mapper';
-import Skeleton from '@/components/Skeleton';
 
 type HeaderProps = { word: Word; loading: false };
 
@@ -12,22 +10,19 @@ type HeaderLoadingProps = { word?: undefined; loading: true };
 
 const Header = ({ word, loading }: HeaderProps | HeaderLoadingProps) => {
   return (
-    <div className='min-h-12 grid-cols-2 justify-center sm:flex sm:gap-4 md:grid lg:grid-cols-3'>
-      <BackToDictionaryButton />
-      <Heading>
-        <span className='mt-4 flex justify-center sm:mt-0 sm:justify-end lg:justify-center'>
-          {loading ? (
-            <Skeleton pulse customSize className='h-8 w-full' />
-          ) : (
-            <span>
-              {word.name} {getLexicalForm(word)}
-            </span>
-          )}
-          <div className='ml-3 md:text-right'>
-            <Badge text={loading ? '' : MAPPER.extended.type.singular[word.type]} />
-          </div>
-        </span>
-      </Heading>
+    <div>
+      <div className='absolute float-start'>
+        <BackToDictionaryButton />
+      </div>
+      <div className='flex w-full justify-center'>
+        <TutorialHeading
+          heading={
+            word ? `${MAPPER.extended.type.singular[word.type]}: ${word?.name} ${getLexicalForm(word)}` : 'Wörterbuch'
+          }
+        >
+          Hier findest du alle Informationen zum Wort &quot;{word?.name}&quot;.
+        </TutorialHeading>
+      </div>
     </div>
   );
 };
