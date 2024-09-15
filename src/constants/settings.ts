45416b982a80547a2b63c55987073c11b22c0a44
neinja007 <anton.siligan@gmail.com>
2024-09-15 06:38:57 +0200

    add client settings supporting async requests

--- src/constants/settings.ts
+++ src/constants/settings.ts
@@ -1,9 +1,13 @@
 import { SettingData } from '@/types/other';
 import { SettingKey } from '@prisma/client';
+import axios from 'axios';
 
-export const settings: {
-  [S in SettingKey]: SettingData;
-} = {
+type ClientSettings = 'RESET_POINTS';
+type Settings = {
+  [S in SettingKey | ClientSettings]: SettingData;
+};
+
+export const settings: Settings = {
   DICTIONARY_VOCATIVE: {
     type: 'boolean',
     name: 'Vokativ im Wörterbuch anzeigen',
@@ -23,5 +27,16 @@ export const settings: {
     name: 'Sprache',
     description: 'Wählt die Sprache des gesamten Lateininators aus.',
     disabled: true
+  },
+  RESET_POINTS: {
+    type: 'button',
+    name: 'Punkte zurücksetzen',
+    description: 'Setzt die Punkte auf 0 zurück.',
+    onClick: () => {
+      axios.post('/api/points', { points: 0, method: 'set' });
+    },
+    buttonText: 'Punkte zurücksetzen',
+    color: 'red',
+    invalidateQueries: 'points'
   }
 };
