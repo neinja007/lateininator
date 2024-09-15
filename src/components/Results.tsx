3b99ed6f0a95c5a6c13a803ed5299798effa5cc7
neinja007 <anton.siligan@gmail.com>
2024-09-15 05:48:51 +0200

    conditionally render points and results

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -9,10 +9,15 @@ type ResultsProps = {
 const Results = ({ handleContinue, points }: ResultsProps) => {
   return (
     <div className='flex flex-col items-center justify-center space-y-4'>
-      <div className='text-center'>
-        <h2 className='text-2xl font-semibold'>Ergebnisse</h2>
-        <p className='mt-2 text-lg'>Du hast {points} Punkte erreicht!</p>
-      </div>
+      {points && (
+        <div className='text-center'>
+          <h2 className='text-2xl font-semibold'>Ergebnisse</h2>
+          <p className='my-2 text-lg'>
+            Du hast {points} Punkt{points !== 1 ? 'e' : ''} erreicht!
+          </p>
+          <Button color='purple'>Punkte speichern</Button>
+        </div>
+      )}
       <Button onClick={() => handleContinue()} color='gray'>
         Neu Laden
       </Button>
