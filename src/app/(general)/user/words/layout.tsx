897e24c4123319b62c3f5d79adbd56154c26a696
neinja007 <anton.siligan@gmail.com>
2024-09-15 15:42:04 +0200

    use new title for word management

--- src/app/(general)/user/words/layout.tsx
+++ src/app/(general)/user/words/layout.tsx
@@ -1,8 +1,8 @@
 type LayoutProps = { children: React.ReactNode };
 
 export const metadata = {
-  title: { default: 'Wörter', template: '%s - Lateininator' },
-  description: 'Wörter'
+  title: { default: 'Wörter verwalten', template: '%s - Lateininator' },
+  description: 'Wörter verwalten'
 };
 
 const Layout = ({ children }: LayoutProps) => children;
