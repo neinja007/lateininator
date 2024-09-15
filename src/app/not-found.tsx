94e70e65db942417dcfa3a4ecb2f066d00c74c8f
neinja007 <anton.siligan@gmail.com>
2024-09-15 15:48:50 +0200

    add tutorial and use new tutorial heading component

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,16 +1,16 @@
-import Heading from '@/components/Heading';
+import TutorialHeading from '@/components/TutorialHeading';
 import Link from '@/components/Link';
 import LinkToSupportEmail from '@/components/LinkToSupportEmail';
 
 const Page = () => {
   return (
     <div>
-      <Heading>Diese Seite wurde nicht gefunden (Error 404).</Heading>
-      <p>
+      <TutorialHeading heading='Diese Seite wurde nicht gefunden (Error 404).'>
         Wenn Sie glauben, dass dies ein Fehler ist, bitten wir Sie, unseren <LinkToSupportEmail /> zu kontaktieren.
-      </p>
+        Ansonsten können Sie <Link href='/dashboard'>hier zurück zur Startseite</Link> gelangen.
+      </TutorialHeading>
       <p className='mt-2'>
-        Ansonsten können Sie <Link href='/dashboard'>hier zurück zur Homepage gelangen</Link>.
+        Ansonsten können Sie <Link href='/dashboard'>hier zurück zur Startseite gelangen</Link>.
       </p>
     </div>
   );
