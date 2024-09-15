db574db80de982592fdc99a3e5dd76148a2ced98
neinja007 <anton.siligan@gmail.com>
2024-09-15 05:06:54 +0200

    accept points prop

--- src/components/WordDisplay.tsx
+++ src/components/WordDisplay.tsx
@@ -2,18 +2,25 @@ import Badge from '@/components/Badge';
 import WordInfo from './WordInfo';
 import { MAPPER } from '@/utils/other/mapper';
 import { Word } from '@/types/word';
+import { Coins } from 'lucide-react';
 
 type WordDisplayProps = {
   word: Word;
+  points?: number;
 };
 
-const WordDisplay = ({ word }: WordDisplayProps) => {
+const WordDisplay = ({ word, points }: WordDisplayProps) => {
   return (
-    <div>
+    <div className='flex items-baseline justify-between'>
       {word.info && <WordInfo info={word.info} />}
       <p className='text-2xl font-medium'>
         {word.name} <Badge text={MAPPER.extended.type.singular[word.type]} />
       </p>
+      {points && (
+        <p className='flex items-center gap-1'>
+          <b>{points}</b> <Coins size={16} />
+        </p>
+      )}
     </div>
   );
 };
