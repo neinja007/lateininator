c059a136ceb0ce0fe52d970a34fc12b3954d85ae
neinja007 <anton.siligan@gmail.com>
2024-09-17 05:42:54 +0200

    reduce cols to only 1 on sm and below

--- src/app/(general)/user/collections/components/CellContainer.tsx
+++ src/app/(general)/user/collections/components/CellContainer.tsx
@@ -10,7 +10,7 @@ const CellContainer = ({ children, wide }: CellContainerProps) => {
     <div
       className={clsx(
         'grid h-fit gap-x-3 gap-y-6 overflow-y-scroll',
-        wide ? 'grid-cols-1 lg:grid-cols-2' : 'grid-cols-2 lg:grid-cols-3'
+        wide ? 'grid-cols-1 lg:grid-cols-2' : 'grid-cols-1 sm:grid-cols-2 lg:grid-cols-3'
       )}
     >
       {children}
