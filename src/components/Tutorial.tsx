4975aa23468685d5ba5b6f04b149d247fee3629f
neinja007 <anton.siligan@gmail.com>
2024-09-17 05:35:15 +0200

    make heading prop optional

--- src/components/Tutorial.tsx
+++ src/components/Tutorial.tsx
@@ -1,12 +1,12 @@
 type TutorialProps = {
   children: React.ReactNode;
-  heading: string;
+  heading?: string;
 };
 
 const Tutorial = ({ children, heading }: TutorialProps) => {
   return (
     <div className='mb-5 rounded-lg border border-cyan-200 bg-cyan-50 p-4 dark:border-cyan-700 dark:bg-cyan-900'>
-      <h2 className='mb-2 text-center text-xl font-bold'>{heading}</h2>
+      {heading && <h2 className='mb-2 text-center text-xl font-bold'>{heading}</h2>}
       <div className='space-y-4'>{children}</div>
     </div>
   );
