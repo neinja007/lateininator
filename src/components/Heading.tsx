8bbb6c4cb09839407ea133405264a38ab5ab0cb3
neinja007 <anton.siligan@gmail.com>
2024-09-21 06:47:04 +0200

    make heading responsive

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -11,13 +11,15 @@ type TutorialHeadingProps = {
 
 const Heading = ({ children, className, heading, ...props }: TutorialHeadingProps) => {
   return (
-    <h1 {...props} className={clsx('mb-5 flex items-center justify-center text-3xl', className)}>
-      <span className='font-bold text-blue-500'>{heading}</span>
-      {children && (
-        <Info size={5} heading={heading}>
-          {children}
-        </Info>
-      )}
+    <h1 {...props} className={clsx('mb-5 text-center text-3xl', className)}>
+      <span className='inline-block'>
+        <span className='font-bold text-blue-500'>{heading}</span>
+        {children && (
+          <Info size={5} heading={heading}>
+            {children}
+          </Info>
+        )}
+      </span>
     </h1>
   );
 };
