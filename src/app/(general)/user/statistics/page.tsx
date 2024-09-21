b7dbbfd855ef1da316487a445c3c3e1a01491822
neinja007 <anton.siligan@gmail.com>
2024-09-21 06:55:52 +0200

    add not available text

--- src/app/(general)/user/statistics/page.tsx
+++ src/app/(general)/user/statistics/page.tsx
@@ -4,6 +4,7 @@ const Page = () => {
   return (
     <div>
       <Heading heading='Statistiken'>Dieses Feature ist noch nicht verfügbar.</Heading>
+      Dieses Feature ist noch nicht verfügbar.
     </div>
   );
 };
