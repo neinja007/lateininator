7435c7c9274ec2413a13932b3d82d1512a69c25e
neinja007 <anton.siligan@gmail.com>
2024-09-21 07:42:35 +0200

    remove overflow scroll

--- src/app/(general)/user/collections/components/CellContainer.tsx
+++ src/app/(general)/user/collections/components/CellContainer.tsx
@@ -9,7 +9,7 @@ const CellContainer = ({ children, wide }: CellContainerProps) => {
   return (
     <div
       className={clsx(
-        'grid h-fit gap-6 overflow-y-scroll',
+        'grid h-fit gap-6',
         wide ? 'grid-cols-1 lg:grid-cols-2' : 'grid-cols-1 sm:grid-cols-2 lg:grid-cols-3'
       )}
     >
