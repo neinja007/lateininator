bb9c137d83f811feaa32e75290df52260add3e50
neinja007 <anton.siligan@gmail.com>
2024-09-21 05:37:37 +0200

    refactor cell component

--- src/app/(general)/user/collections/components/Cell.tsx
+++ src/app/(general)/user/collections/components/Cell.tsx
@@ -1,68 +1,101 @@
 import Button from '@/components/Button';
-import { Color } from '@/types/other';
+import Link from '@/components/Link';
 import clsx from 'clsx';
-import { Plus } from 'lucide-react';
+import { Edit, Plus } from 'lucide-react';
 
 type CellProps = {
   className?: string & React.CSSProperties;
   outlined?: boolean;
-  onClick?: () => void;
   name?: string;
   lists?: number;
   owner?: string;
-  buttonLabel?: string;
-  buttonColor?: Color;
   description?: string;
-  wide?: boolean;
+  active?: boolean;
+  onToggleActive?: () => void;
+  editable?: boolean;
+  onToggleEditable?: () => void;
+  owned?: boolean;
 };
 
 const Cell = ({
   outlined,
   className,
-  buttonLabel,
   lists,
   name,
   owner,
-  buttonColor,
-  onClick,
   description,
-  wide
+  active,
+  onToggleActive,
+  editable,
+  onToggleEditable,
+  owned
 }: CellProps) => {
   return (
-    <div className='min-h-24'>
-      <button
+    <div className='min-h-32'>
+      <div
         className={clsx(
-          'min-h-24 w-full rounded-lg px-4 py-2 transition-colors',
-          outlined
-            ? 'border border-dashed hover:border-solid hover:bg-gray-200 dark:hover:bg-gray-900'
-            : 'bg-blue-400 hover:bg-blue-500 dark:bg-blue-950 dark:hover:bg-blue-800',
+          'w-full px-4 py-2 transition-colors',
+          outlined ? 'h-[132px] rounded-lg border border-dashed' : 'h-24 rounded-t-lg bg-blue-400 dark:bg-blue-950',
           className
         )}
-        onClick={outlined ? onClick : undefined}
       >
         {outlined ? (
           <div className='flex h-full items-center justify-center text-gray-500 dark:text-gray-300'>
             <div className='text-center'>
               <Plus strokeWidth={1} className='m-0 mx-auto h-12 w-12' />
-              Neue Kollektion anlegen
+              <b>Aktivieren</b> Sie eine Kollektion, oder{' '}
+              <Link href='/user/collections/new'>erstellen Sie eine neue</Link>
             </div>
           </div>
         ) : (
           <>
-            <h3 className={clsx('text-2xl font-medium', wide ? 'text-left' : 'text-center')}>{name}</h3>
-            {wide && <p className='text-left'>{description}</p>}
+            <h3
+              className={clsx(
+                'flex text-2xl font-medium',
+                description || editable ? 'justify-between' : 'justify-center'
+              )}
+            >
+              {name}
+              {editable && (
+                <button
+                  onClick={
+                    onToggleEditable
+                      ? (e) => {
+                          e.stopPropagation();
+                          onToggleEditable();
+                        }
+                      : undefined
+                  }
+                  className='-mr-2 rounded-lg bg-sky-700 p-2 hover:bg-sky-600'
+                >
+                  <Edit className='h-4 w-4' />
+                </button>
+              )}
+            </h3>
+            {description && <p className='text-left'>{description}</p>}
             <p className='mt-3 block items-end justify-between sm:flex'>
               <span className='mt-2 block'>{lists} Listen</span>
-              <span>
-                von <span className='font-medium text-yellow-400 dark:text-yellow-500'>{owner}</span>
-              </span>
+              {owned ? (
+                <span className='font-medium text-sky-500'>Ihre Kollektion</span>
+              ) : (
+                <span>
+                  von <span className='font-medium text-yellow-400 dark:text-yellow-500'>{owner}</span>
+                </span>
+              )}
             </p>
           </>
         )}
-      </button>
-      <Button className={clsx('mt-2 w-full', className)} onClick={onClick} color={buttonColor}>
-        {buttonLabel}
-      </Button>
+      </div>
+      {!outlined && (
+        <Button
+          className={clsx('h-9 w-full rounded-b-lg', className)}
+          unstyled
+          onClick={onToggleActive}
+          color={active ? 'orange' : 'green'}
+        >
+          {active ? 'Kollektion deaktivieren' : 'Kollektion aktivieren'}
+        </Button>
+      )}
     </div>
   );
 };
