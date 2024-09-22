a9e3e616c51ea0021981540e04d7758f7bcae38b
neinja007 <anton.siligan@gmail.com>
2024-09-22 06:21:49 +0200

    use hook to fetch primary color

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,10 +1,8 @@
 'use client';
-
 import clsx from 'clsx';
 import Info from './Info';
-import { useSettings } from '@/hooks/database/queries/useSettings';
 import { COLORS } from '@/constants/other';
-import { AvailableColor } from '@/app/(general)/user/settings/components/ColorPicker';
+import { usePrimaryColor } from '@/hooks/database/queries/usePrimaryColor';
 
 type TutorialHeadingProps = {
   children?: React.ReactNode;
@@ -13,13 +11,12 @@ type TutorialHeadingProps = {
 } & React.ComponentProps<'h1'>;
 
 const Heading = ({ children, className, heading, ...props }: TutorialHeadingProps) => {
-  const { settings } = useSettings();
-  const primaryColor = settings?.PRIMARY_COLOR || 'blue';
+  const primaryColor = usePrimaryColor();
 
   return (
     <h1 {...props} className={clsx('mb-5 text-center text-3xl', className)}>
       <span className='inline-block'>
-        <span className={clsx('font-bold', COLORS[primaryColor as AvailableColor].text)}>{heading}</span>
+        <span className={clsx('font-bold', COLORS[primaryColor].text)}>{heading}</span>
         {children && (
           <Info size={5} heading={heading}>
             {children}
