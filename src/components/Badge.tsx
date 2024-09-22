7ae5af15540a84f12a63fc335d30c1062109f862
neinja007 <anton.siligan@gmail.com>
2024-09-22 07:19:56 +0200

    use inherit color border

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -6,12 +6,7 @@ type BadgeProps = { color?: Color; text: string };
 
 const Badge = ({ color = 'default', text }: BadgeProps) => {
   return (
-    <span
-      className={clsx(
-        'rounded-full border p-1 px-2 align-middle text-xs font-medium uppercase dark:border-gray-700',
-        COLORS[color].static
-      )}
-    >
+    <span className={clsx('rounded-full p-1 px-2 align-middle text-xs font-medium uppercase', COLORS[color].static)}>
       {text}
     </span>
   );
