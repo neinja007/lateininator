d63f3efb2e44513ddc46f959bb63240a60f02991
neinja007 <anton.siligan@gmail.com>
2024-09-22 11:02:18 +0200

    add option to use random color function

--- src/hooks/database/queries/usePrimaryColor.ts
+++ src/hooks/database/queries/usePrimaryColor.ts
@@ -1,15 +1,23 @@
+/* eslint-disable no-redeclare */
 import { PrimaryColor } from '@/types/other';
 import { useSettings } from './useSettings';
 import { availableColors } from '@/constants/other';
 
-export const usePrimaryColor = (): PrimaryColor => {
+export function usePrimaryColor(randomFn: true): () => PrimaryColor;
+export function usePrimaryColor(randomFn?: false): PrimaryColor;
+export function usePrimaryColor(randomFn?: boolean): (() => PrimaryColor) | PrimaryColor;
+export function usePrimaryColor(randomFn?: boolean) {
   const { settings } = useSettings();
 
   const primaryColor = (settings?.PRIMARY_COLOR as PrimaryColor | 'random') || 'blue';
 
   if (primaryColor === 'random') {
-    return availableColors[Math.floor(Math.random() * availableColors.length)];
+    if (randomFn) {
+      return () => availableColors[Math.floor(Math.random() * availableColors.length)];
+    } else {
+      return availableColors[Math.floor(Math.random() * availableColors.length)];
+    }
   }
 
   return primaryColor;
-};
+}
