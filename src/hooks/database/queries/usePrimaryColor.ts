578e1e77d55b798de8b85087ab61c49caaad4461
neinja007 <anton.siligan@gmail.com>
2024-09-22 10:32:12 +0200

    support random color

--- src/hooks/database/queries/usePrimaryColor.ts
+++ src/hooks/database/queries/usePrimaryColor.ts
@@ -1,10 +1,15 @@
 import { PrimaryColor } from '@/types/other';
 import { useSettings } from './useSettings';
+import { availableColors } from '@/constants/other';
 
-export const usePrimaryColor = () => {
+export const usePrimaryColor = (): PrimaryColor => {
   const { settings } = useSettings();
 
-  const primaryColor: PrimaryColor = (settings?.PRIMARY_COLOR as PrimaryColor) || 'blue';
+  const primaryColor = (settings?.PRIMARY_COLOR as PrimaryColor | 'random') || 'blue';
+
+  if (primaryColor === 'random') {
+    return availableColors[Math.floor(Math.random() * availableColors.length)];
+  }
 
   return primaryColor;
 };
