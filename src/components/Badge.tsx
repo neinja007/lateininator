f843546dd5476a60a65b33222a5010f4b90980f6
neinja007 <anton.siligan@gmail.com>
2024-09-22 06:02:05 +0200

    use static colors for badge

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -9,7 +9,7 @@ const Badge = ({ color = 'default', text }: BadgeProps) => {
     <span
       className={clsx(
         'rounded-full border p-1 px-2 align-middle text-xs font-medium uppercase dark:border-gray-700',
-        COLORS[color]
+        COLORS[color].static
       )}
     >
       {text}
