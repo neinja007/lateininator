a83791bb041220e21c2b768c7829fe0e2d70f3e4
neinja007 <anton.siligan@gmail.com>
2024-09-22 06:00:32 +0200

    use primary color for headings

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -2,6 +2,9 @@
 
 import clsx from 'clsx';
 import Info from './Info';
+import { useSettings } from '@/hooks/database/queries/useSettings';
+import { COLORS } from '@/constants/other';
+import { AvailableColor } from '@/app/(general)/user/settings/components/ColorPicker';
 
 type TutorialHeadingProps = {
   children?: React.ReactNode;
@@ -10,10 +13,13 @@ type TutorialHeadingProps = {
 } & React.ComponentProps<'h1'>;
 
 const Heading = ({ children, className, heading, ...props }: TutorialHeadingProps) => {
+  const { settings } = useSettings();
+  const primaryColor = settings?.PRIMARY_COLOR || 'blue';
+
   return (
     <h1 {...props} className={clsx('mb-5 text-center text-3xl', className)}>
       <span className='inline-block'>
-        <span className='font-bold text-blue-500'>{heading}</span>
+        <span className={clsx('font-bold', COLORS[primaryColor as AvailableColor].text)}>{heading}</span>
         {children && (
           <Info size={5} heading={heading}>
             {children}
