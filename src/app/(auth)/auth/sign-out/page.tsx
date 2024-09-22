f61b2dd5b63b5c5449e83ecee400116d6f125625
neinja007 <anton.siligan@gmail.com>
2024-09-22 06:45:33 +0200

    use primary color

--- src/app/(auth)/auth/sign-out/page.tsx
+++ src/app/(auth)/auth/sign-out/page.tsx
@@ -1,10 +1,14 @@
 'use client';
 
 import LoadingHeading from '@/components/LoadingHeading';
+import { COLORS } from '@/constants/other';
+import { usePrimaryColor } from '@/hooks/database/queries/usePrimaryColor';
 import { SignOutButton, useClerk } from '@clerk/nextjs';
+import clsx from 'clsx';
 
 const Page = () => {
   const { signOut } = useClerk();
+  const primaryColor = usePrimaryColor();
 
   signOut();
 
@@ -13,7 +17,7 @@ const Page = () => {
       <LoadingHeading>Sie werden abgemeldet...</LoadingHeading>
       Sollte dies zu lange dauern,{' '}
       <SignOutButton>
-        <button className='text-blue-500 hover:underline'>klicken sie bitte hier</button>
+        <button className={clsx('hover:underline', COLORS[primaryColor].text)}>klicken sie bitte hier</button>
       </SignOutButton>
       .
     </>
