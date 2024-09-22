faf41bb67785cb148deb92653d168ab3ae4e62a5
neinja007 <anton.siligan@gmail.com>
2024-09-22 06:16:12 +0200

    use primary color for navbar

--- src/components/NavbarDropdown.tsx
+++ src/components/NavbarDropdown.tsx
@@ -5,6 +5,9 @@ import navbar from '@/styles/navbar.module.css';
 import clsx from 'clsx';
 import { ChevronUp } from 'lucide-react';
 import { Route } from '@/constants/routes';
+import { COLORS } from '@/constants/other';
+import { AvailableColor } from '@/app/(general)/user/settings/components/ColorPicker';
+import { useSettings } from '@/hooks/database/queries/useSettings';
 
 type NavbarDropdownProps = {
   route: Route;
@@ -15,10 +18,13 @@ type NavbarDropdownProps = {
 };
 
 const NavbarDropdown = ({ route, children, open, handleOpen, active }: NavbarDropdownProps) => {
+  const { settings } = useSettings();
+  const primaryColor = settings?.PRIMARY_COLOR || 'blue';
+
   return (
     <div className='flex justify-start xl:justify-center'>
       <div
-        className={clsx(navbar.navlink, active ? navbar.active : navbar.inactive)}
+        className={clsx(navbar.navlink, active ? COLORS[primaryColor as AvailableColor].text : navbar.inactive)}
         onClick={() => handleOpen((prevOpen) => (prevOpen === route.label ? '' : route.label))}
       >
         <route.icon className='w-5' /> <span className='max-w-40 truncate'>{route.label}</span>
--- src/components/NavbarLink.tsx
+++ src/components/NavbarLink.tsx
@@ -4,6 +4,9 @@ import Link from 'next/link';
 import navbar from '@/styles/navbar.module.css';
 import clsx from 'clsx';
 import { Route } from '@/constants/routes';
+import { useSettings } from '@/hooks/database/queries/useSettings';
+import { AvailableColor } from '@/app/(general)/user/settings/components/ColorPicker';
+import { COLORS } from '@/constants/other';
 
 type NavbarLinkProps = {
   route: Route;
@@ -12,12 +15,15 @@ type NavbarLinkProps = {
 };
 
 const NavbarLink = ({ dropdown, route, active }: NavbarLinkProps) => {
+  const { settings } = useSettings();
+  const primaryColor = settings?.PRIMARY_COLOR || 'blue';
+
   return (
     <Link
       className={clsx(
         route.label === 'Premium' && 'text-pink-600 hover:text-pink-500 dark:text-pink-400',
         dropdown ? navbar.dropdown_navlink : navbar.navlink,
-        route.label !== 'Premium' && (active ? navbar.active : navbar.inactive)
+        route.label !== 'Premium' && (active ? COLORS[primaryColor as AvailableColor].text : navbar.inactive)
       )}
       href={route.href}
     >
