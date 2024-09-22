7632f2ce77f9533be0c02a87bfcbb180dedcc45f
neinja007 <anton.siligan@gmail.com>
2024-09-22 06:13:13 +0200

    dim info background

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -30,17 +30,21 @@ const Info = ({ children, heading, size = 4 }: InfoProps) => {
           <div className='absolute inset-0 flex items-center justify-center'>
             <div
               className={clsx(
-                'mx-4 w-full max-w-md rounded-lg border border-neutral-200 p-4 dark:border-neutral-700',
+                'relative mx-4 w-full max-w-md rounded-lg border border-neutral-200 p-4 dark:border-neutral-700',
                 COLORS[primaryColor as AvailableColor].static
               )}
             >
-              <div className='flex items-start justify-between'>
-                <h2 className='mb-2 line-clamp-2 text-2xl font-bold'>{heading || 'Information'}</h2>
-                <button onClick={() => setShowTutorial(false)} className='mt-1 inline-block'>
-                  <X className='size-6 text-gray-500 hover:text-gray-600 dark:text-gray-300 dark:hover:text-white' />
-                </button>
+              <div className='absolute inset-0 z-0 rounded-lg bg-inherit brightness-50'></div>
+
+              <div className='relative z-10'>
+                <div className='flex items-start justify-between'>
+                  <h2 className='mb-2 line-clamp-2 text-2xl font-bold'>{heading || 'Information'}</h2>
+                  <button onClick={() => setShowTutorial(false)} className='mt-1 inline-block'>
+                    <X className='size-6 text-gray-500 hover:text-gray-600 dark:text-gray-300 dark:hover:text-white' />
+                  </button>
+                </div>
+                <p className='break-words text-left text-base text-gray-700 dark:text-gray-300'>{children}</p>
               </div>
-              <p className='break-words text-left text-base text-gray-700 dark:text-gray-300'>{children}</p>
             </div>
           </div>
         </div>
