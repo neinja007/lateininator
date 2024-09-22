127382fa5b8b02b045a01b0c6a22eab8ed9900a9
neinja007 <anton.siligan@gmail.com>
2024-09-22 06:42:37 +0200

    use primary color for links

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,3 +1,7 @@
+'use client';
+
+import { COLORS } from '@/constants/other';
+import { usePrimaryColor } from '@/hooks/database/queries/usePrimaryColor';
 import clsx from 'clsx';
 import { ExternalLink } from 'lucide-react';
 import NextLink from 'next/link';
@@ -9,8 +13,10 @@ type LinkProps = {
 } & React.ComponentProps<typeof NextLink>;
 
 const Link = ({ children, href, className, ...props }: LinkProps) => {
+  const primaryColor = usePrimaryColor();
+
   return (
-    <NextLink href={href} className={clsx('text-blue-500 hover:underline', className)} {...props}>
+    <NextLink href={href} className={clsx('hover:underline', className, COLORS[primaryColor].text)} {...props}>
       {children}
       <ExternalLink className='mx-1 inline-block h-4 w-4 align-text-top' />
     </NextLink>
