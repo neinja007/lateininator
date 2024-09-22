6ce60a405859f881066f62cfe42a91394b2dc9f2
neinja007 <anton.siligan@gmail.com>
2024-09-22 10:19:15 +0200

    support info light mode

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -30,7 +30,7 @@ const Info = ({ children, heading, size = 4 }: InfoProps) => {
         >
           <div className='absolute inset-0 flex items-center justify-center'>
             <div className={clsx('relative mx-4 w-full max-w-lg rounded-lg p-4', COLORS[primaryColor].static)}>
-              <div className='absolute inset-0 z-0 rounded-lg bg-inherit brightness-50'></div>
+              <div className='absolute inset-0 z-0 rounded-lg bg-inherit brightness-125 dark:brightness-50'></div>
               <div className='relative z-10'>
                 <div className='mb-3 flex items-start justify-between'>
                   <h2 className='mb-2 line-clamp-2 text-2xl font-bold'>{heading || 'Information'}</h2>
@@ -38,7 +38,7 @@ const Info = ({ children, heading, size = 4 }: InfoProps) => {
                     <X className='size-6 text-gray-500 hover:text-gray-600 dark:text-gray-300 dark:hover:text-white' />
                   </button>
                 </div>
-                <p className='break-words text-left text-base text-gray-700 dark:text-gray-300'>{children}</p>
+                <p className='break-words text-left text-base text-black dark:text-gray-300'>{children}</p>
               </div>
             </div>
           </div>
