46b1f9b90baaa9ca3fa812e298b89bf828413e3a
neinja007 <anton.siligan@gmail.com>
2024-09-22 07:21:29 +0200

    add word type badge color to word display

--- src/components/WordDisplay.tsx
+++ src/components/WordDisplay.tsx
@@ -2,6 +2,7 @@ import Badge from '@/components/Badge';
 import WordInfo from './WordInfo';
 import { MAPPER } from '@/utils/other/mapper';
 import { Word } from '@/types/word';
+import { mapTypeToColor } from '@/constants/other';
 
 type WordDisplayProps = {
   word: Word;
@@ -11,7 +12,7 @@ const WordDisplay = ({ word }: WordDisplayProps) => {
   return (
     <div className='flex items-baseline justify-between'>
       <p className='text-2xl font-medium'>
-        {word.name} <Badge text={MAPPER.extended.type.singular[word.type]} />
+        {word.name} <Badge text={MAPPER.extended.type.singular[word.type]} color={mapTypeToColor[word.type]} />
       </p>
       {word.info && <WordInfo info={word.info} />}
     </div>
