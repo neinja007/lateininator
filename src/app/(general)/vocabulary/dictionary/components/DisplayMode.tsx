1c49e2a450950113a26b380588b49897afc4076f
neinja007 <anton.siligan@gmail.com>
2024-09-22 06:49:47 +0200

    use primary color

--- src/app/(general)/vocabulary/dictionary/components/DisplayMode.tsx
+++ src/app/(general)/vocabulary/dictionary/components/DisplayMode.tsx
@@ -1,19 +1,23 @@
+import { COLORS } from '@/constants/other';
+import { usePrimaryColor } from '@/hooks/database/queries/usePrimaryColor';
 import { List, RectangleHorizontal } from 'lucide-react';
 
 type DisplayModeProps = { view: 'cards' | 'list'; setView: (view: 'cards' | 'list') => void };
 
 const DisplayMode = ({ view, setView }: DisplayModeProps) => {
+  const primaryColor = usePrimaryColor();
+
   return (
     <div className='float-end mb-4 mr-2 hidden md:flex'>
       <div
-        className={`flex cursor-pointer select-none transition-colors ${view === 'cards' ? 'text-blue-500' : 'text-gray-400'}`}
+        className={`flex cursor-pointer select-none transition-colors ${view === 'cards' ? COLORS[primaryColor].text : 'text-gray-400'}`}
         onClick={() => setView('cards')}
       >
         Kartenansicht
         <RectangleHorizontal className='ml-2 mr-8' />
       </div>
       <div
-        className={`flex cursor-pointer select-none transition-colors ${view === 'list' ? 'text-blue-500' : 'text-gray-400'}`}
+        className={`flex cursor-pointer select-none transition-colors ${view === 'list' ? COLORS[primaryColor].text : 'text-gray-400'}`}
         onClick={() => setView('list')}
       >
         Listenansicht <List className='ml-2' />
