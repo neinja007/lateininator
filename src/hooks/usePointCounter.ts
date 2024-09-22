289a76431f89523b07d732352f449e111aa6cc94
neinja007 <anton.siligan@gmail.com>
2024-09-22 10:40:47 +0200

    add points while testing

--- src/hooks/usePointCounter.ts
+++ src/hooks/usePointCounter.ts
@@ -1,15 +1,19 @@
 import { Stage } from '@/types/other';
 import { useCallback, useState, useEffect } from 'react';
+import { useAddPoints } from './database/mutations/useAddPoints';
 
 export const usePointCounter = (stage: Stage) => {
   const [points, setPoints] = useState<number>(0);
   const [difference, setDifference] = useState<number>(0);
 
+  const { mutate: addDbPoints } = useAddPoints('increment');
+
   const addPoints = useCallback(
     (difference: number) => {
       setPoints((prev) => prev + difference);
+      addDbPoints(difference);
     },
-    [setPoints]
+    [setPoints, addDbPoints]
   );
 
   const addDifference = useCallback(
@@ -20,9 +24,9 @@ export const usePointCounter = (stage: Stage) => {
   );
 
   const addDifferenceToPoints = useCallback(() => {
-    setPoints((prev) => prev + difference);
+    addPoints(difference);
     setDifference(0);
-  }, [difference, setPoints, setDifference]);
+  }, [difference, addPoints, setDifference]);
 
   useEffect(() => {
     if (stage === 'test' && difference !== 0) {
