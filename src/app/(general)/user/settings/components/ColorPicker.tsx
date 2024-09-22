62d03d2da0450874680049c83e306f52a3b47c76
neinja007 <anton.siligan@gmail.com>
2024-09-22 05:50:35 +0200

    add current color display

--- src/app/(general)/user/settings/components/ColorPicker.tsx
+++ src/app/(general)/user/settings/components/ColorPicker.tsx
@@ -3,23 +3,41 @@ import { Color } from '@/types/other';
 import clsx from 'clsx';
 import { CheckCircle } from 'lucide-react';
 
+const availableColors = ['yellow', 'blue', 'green', 'purple', 'pink', 'orange'] as const;
+export type AvailableColor = (typeof availableColors)[number];
+
 type ColorPickerProps = {
   disabled: boolean;
-  value: Color;
+  value: AvailableColor;
   onChange: (color: Color) => void;
 };
 
-const availableColors = ['yellow', 'blue', 'green', 'purple', 'pink', 'orange'] as Color[];
+const colorMap: { [key in AvailableColor]: string } = {
+  yellow: 'Gelb',
+  blue: 'Blau',
+  green: 'Grün',
+  purple: 'Lila',
+  pink: 'Pink',
+  orange: 'Orange'
+};
 
 const ColorPicker = ({ disabled, value, onChange }: ColorPickerProps) => {
   return (
-    <div className='grid grid-cols-3'>
-      {availableColors.map((color) => (
-        <button key={color} onClick={() => onChange(color)} className='flex items-center justify-center'>
-          <div className={clsx('h-6 w-6', COLORS[color])} />
-          {value === color && <CheckCircle className='absolute h-4 w-4' />}
-        </button>
-      ))}
+    <div className='flex items-center'>
+      <span className={clsx('mr-4 rounded-md px-2', COLORS[value])}>{colorMap[value]}</span>
+      <div className='grid grid-cols-3'>
+        {availableColors.map((color) => (
+          <button
+            disabled={disabled}
+            key={color}
+            onClick={() => onChange(color)}
+            className='flex items-center justify-center'
+          >
+            <div className={clsx('h-6 w-6', COLORS[color])} />
+            {value === color && <CheckCircle className='absolute h-4 w-4' />}
+          </button>
+        ))}
+      </div>
     </div>
   );
 };
