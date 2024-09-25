cd3a6b0c2a713ffbe021db2e5dcd255fa74ff34d
neinja007 <anton.siligan@gmail.com>
2024-09-25 17:31:21 +0200

    fix mutation function argument type

--- src/hooks/database/mutations/useChangeUsername.ts
+++ src/hooks/database/mutations/useChangeUsername.ts
@@ -1,10 +1,9 @@
-import { AllSettingKey } from '@/types/other';
 import { useMutation } from '@tanstack/react-query';
 import axios from 'axios';
 
 export const useChangeUsername = () => {
   const query = useMutation({
-    mutationFn: (name: AllSettingKey) => axios.patch('/api/user', { name })
+    mutationFn: (name: string) => axios.patch('/api/user', { name })
   });
 
   return query;
