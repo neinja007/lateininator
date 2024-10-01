995cc582351722f3e9697e7dffcc10c5a957dae6
neinja007 <anton.siligan@gmail.com>
2024-10-01 19:38:45 +0200

    remove unnecessary ternaries

--- src/app/(general)/vocabulary/dictionary/[id]/components/Header.tsx
+++ src/app/(general)/vocabulary/dictionary/[id]/components/Header.tsx
@@ -1,30 +1,21 @@
 import Heading from '@/components/Heading';
 import { Word } from '@/types/word';
-import { getLexicalForm } from '@/utils/word/getLexicalForm';
 import BackToDictionaryButton from './BackToDictionaryButton';
 import { MAPPER } from '@/utils/other/mapper';
 
 type HeaderProps = { word: Word };
 
-type HeaderLoadingProps = { word?: undefined };
-
-const Header = ({ word }: HeaderProps | HeaderLoadingProps) => {
+const Header = ({ word }: HeaderProps) => {
   return (
     <div>
       <div className='float-start mb-2 sm:absolute'>
         <BackToDictionaryButton />
       </div>
       <div className='flex w-full justify-end lg:justify-center'>
-        <Heading
-          heading={
-            word
-              ? `${word?.name} ${getLexicalForm(word) || ''} (${MAPPER.extended.type.singular[word.type]})`
-              : 'Wörterbuch'
-          }
-        >
-          Hier findest du <b>alle Informationen</b> zum {word ? MAPPER.extended.type.singular[word.type] : 'Wort'}{' '}
+        <Heading heading={word.name}>
+          Hier findest du <b>alle Informationen</b> zum {MAPPER.extended.type.singular[word.type]}
           &quot;
-          {word?.name}
+          {word.name}
           &quot;.
         </Heading>
       </div>
