bf1394cdc9e9d7f6a5bea1989388e53239fc0238
neinja007 <anton.siligan@gmail.com>
2024-10-01 19:44:24 +0200

    improve big version

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -7,7 +7,7 @@ type BadgeProps = { color?: Color; text: string; big?: boolean };
 const Badge = ({ color = 'default', text, big }: BadgeProps) => {
   return (
     <div className={clsx('inline-flex items-center justify-center rounded-full p-1 px-2', COLORS[color].static)}>
-      <span className={clsx('font-medium uppercase leading-none', big ? 'text-sm' : 'text-xs')}>{text}</span>
+      <span className={clsx('font-medium', big ? 'text-xl' : 'text-xs uppercase leading-none')}>{text}</span>
     </div>
   );
 };
