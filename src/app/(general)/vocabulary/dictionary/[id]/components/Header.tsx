c180580629fbe06d4ff5298ec12ec5ccf7e7cce1
neinja007 <anton.siligan@gmail.com>
2024-10-01 19:44:40 +0200

    move word type display from heading to badge

--- src/app/(general)/vocabulary/dictionary/[id]/components/Header.tsx
+++ src/app/(general)/vocabulary/dictionary/[id]/components/Header.tsx
@@ -2,22 +2,30 @@ import Heading from '@/components/Heading';
 import { Word } from '@/types/word';
 import BackToDictionaryButton from './BackToDictionaryButton';
 import { MAPPER } from '@/utils/other/mapper';
+import Badge from '@/components/Badge';
+import { mapTypeToColor } from '@/constants/other';
 
 type HeaderProps = { word: Word };
 
 const Header = ({ word }: HeaderProps) => {
   return (
-    <div>
+    <div className='h-16'>
       <div className='float-start mb-2 sm:absolute'>
         <BackToDictionaryButton />
       </div>
-      <div className='flex w-full justify-end lg:justify-center'>
-        <Heading heading={word.name}>
-          Hier findest du <b>alle Informationen</b> zum {MAPPER.extended.type.singular[word.type]}
-          &quot;
-          {word.name}
-          &quot;.
-        </Heading>
+      <div className='grid w-full grid-cols-2 gap-4 lg:grid-cols-3'>
+        <div />
+        <div>
+          <Heading heading={word.name}>
+            Hier findest du <b>alle Informationen</b> zum {MAPPER.extended.type.singular[word.type]}
+            &quot;
+            {word.name}
+            &quot;.
+          </Heading>
+        </div>
+        <div className='text-right'>
+          <Badge color={mapTypeToColor[word.type]} text={MAPPER.extended.type.singular[word.type]} big />
+        </div>
       </div>
     </div>
   );
