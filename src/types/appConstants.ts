36bbf2e536800b94b4381682767cfb690aaa1d50
neinja007 <anton.siligan@gmail.com>
2024-10-02 08:54:13 +0200

    rename type to clearer name

--- src/types/appConstants.ts
+++ src/types/appConstants.ts
@@ -4,5 +4,5 @@ export type WordType = (typeof APP_CONSTANTS.wordTypes)[number];
 export type MainWordType = (typeof APP_CONSTANTS.mainWordTypes)[number];
 export type MainWordTypeWithOther = (typeof APP_CONSTANTS.mainWordTypesWithOther)[number];
 export type WordProperty = (typeof APP_CONSTANTS.allWordProperties)[number];
+export type WordPropertyUsingSelectInput = (typeof APP_CONSTANTS.wordPropertiesUsingSelectInput)[number];
 export type OtherProperty = (typeof APP_CONSTANTS.otherProperties)[number];
-export type WordPropertiesUsingSelectInput = (typeof APP_CONSTANTS.wordPropertiesUsingSelectInput)[number];
