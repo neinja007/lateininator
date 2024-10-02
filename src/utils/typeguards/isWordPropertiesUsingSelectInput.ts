0387b5cf864c21c1188eb60dc7c0b2bc345ef5cb
neinja007 <anton.siligan@gmail.com>
2024-10-02 08:54:18 +0200

    use new name

--- src/utils/typeguards/isWordPropertiesUsingSelectInput.ts
+++ src/utils/typeguards/isWordPropertiesUsingSelectInput.ts
@@ -1,6 +1,6 @@
 import { APP_CONSTANTS } from '@/constants/appConstants';
-import { WordProperty, WordPropertiesUsingSelectInput } from '@/types/appConstants';
+import { WordProperty, WordPropertyUsingSelectInput } from '@/types/appConstants';
 
-export const isWordPropertiesUsingSelectInput = (element: WordProperty): element is WordPropertiesUsingSelectInput => {
-  return APP_CONSTANTS.wordPropertiesUsingSelectInput.includes(element as WordPropertiesUsingSelectInput);
+export const isWordPropertiesUsingSelectInput = (element: WordProperty): element is WordPropertyUsingSelectInput => {
+  return APP_CONSTANTS.wordPropertiesUsingSelectInput.includes(element as WordPropertyUsingSelectInput);
 };
