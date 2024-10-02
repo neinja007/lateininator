07b3a82a140f9aace690007a6e8354284057df24
neinja007 <anton.siligan@gmail.com>
2024-10-02 09:32:50 +0200

    add id control prop and support native props

--- src/components/CheckboxWithLabel.tsx
+++ src/components/CheckboxWithLabel.tsx
@@ -5,12 +5,31 @@ import { Dispatch, SetStateAction, useId } from 'react';
 type CheckboxWithLabelProps = {
   checked: boolean;
   disabled?: boolean;
-  handleChange: Dispatch<SetStateAction<boolean>>;
+  handleChange?: Dispatch<SetStateAction<boolean>>;
   label: string;
+  noGeneratedId?: boolean;
 } & Omit<React.ComponentProps<'input'>, 'type'>;
 
-const CheckboxWithLabel = ({ checked, disabled, handleChange, label }: CheckboxWithLabelProps) => {
+const CheckboxWithLabel = ({
+  checked,
+  disabled,
+  handleChange,
+  label,
+  noGeneratedId,
+  ...props
+}: CheckboxWithLabelProps) => {
   const id = useId();
+
+  if ((!props.id && !noGeneratedId) || (props.id && noGeneratedId)) {
+    throw new Error('conflicting ids!');
+  }
+
+  const dynamicId = noGeneratedId ? props.id : id;
+
+  if (handleChange && props.onChange) {
+    throw new Error('conflicting onChange handlers!');
+  }
+
   const primaryColor = usePrimaryColor(true);
 
   return (
@@ -19,13 +38,14 @@ const CheckboxWithLabel = ({ checked, disabled, handleChange, label }: CheckboxW
         type='checkbox'
         disabled={disabled}
         style={{ accentColor: COLORS[primaryColor()].hex }}
-        id={id}
+        id={dynamicId}
         checked={checked}
-        onChange={(e) => handleChange(e.target.checked)}
+        onChange={handleChange ? (e) => handleChange(e.target.checked) : props.onChange}
         className='mr-1'
+        {...props}
       />
       <label
-        htmlFor={id}
+        htmlFor={dynamicId}
         className={
           checked ? COLORS[primaryColor()].text : disabled ? 'text-gray-400 dark:text-gray-600' : 'text-gray-500'
         }
