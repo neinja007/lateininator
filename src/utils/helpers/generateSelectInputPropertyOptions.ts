c482ab730c2fb6acb7b8775c3de10aa5c96002d2
neinja007 <anton.siligan@gmail.com>
2024-10-02 08:54:32 +0200

    narrow return type

--- src/utils/helpers/generateSelectInputPropertyOptions.ts
+++ src/utils/helpers/generateSelectInputPropertyOptions.ts
@@ -1,9 +1,9 @@
 import { WORD_CONSTANTS } from '@/constants/wordConstants';
-import { WordProperty } from '@/types/appConstants';
+import { WordPropertyUsingSelectInput } from '@/types/appConstants';
 import { MAPPER } from '../other/mapper';
 import { isWordPropertiesUsingSelectInput } from '../typeguards/isWordPropertiesUsingSelectInput';
 
-export const generateSelectInputPropertyOptions = (property: WordProperty) => {
+export const generateSelectInputPropertyOptions = (property: WordPropertyUsingSelectInput) => {
   return isWordPropertiesUsingSelectInput(property)
     ? WORD_CONSTANTS.optional[property].reduce((object: { [key: string]: string }, element) => {
         object[element] = (MAPPER.extended[property] as { [key: string]: string })[element];
