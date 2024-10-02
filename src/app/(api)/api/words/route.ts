1e7f28923b043c0f1a1c77db4f78cb1be9484934
neinja007 <anton.siligan@gmail.com>
2024-10-02 09:18:58 +0200

    add put handler to word route

--- src/app/(api)/api/words/route.ts
+++ src/app/(api)/api/words/route.ts
@@ -4,6 +4,10 @@ import { getWordById } from './services/getWordById';
 import { getWordsByQuery } from './services/getWordsByQuery';
 import { getWords } from './services/getWords';
 import { z } from 'zod';
+import { currentUser } from '@clerk/nextjs/server';
+import { wordSchema } from '@/schemas/word';
+import { prisma } from '@/utils/other/client';
+import { Comparison, Conjugation, Declension, Gender, Type } from '@prisma/client';
 
 export const GET = async (request: NextRequest) => {
   const searchParams = request.nextUrl.searchParams;
@@ -53,5 +57,99 @@ export const GET = async (request: NextRequest) => {
       console.error(error);
       return NextResponse.json({ error: error.message }, { status: 500 });
     }
+  }
+};
+
+export const PUT = async (request: NextRequest) => {
+  const user = await currentUser();
+
+  if (!user) {
+    return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+  }
+
+  const body = await request.json();
+
+  const parsed = wordSchema.safeParse(body);
+
+  if (!parsed.success) {
+    return NextResponse.json({ error: 'Invalid request body' }, { status: 400 });
+  }
+
+  const word = parsed.data;
+
+  if (Number(!!word.noun) + Number(!!word.verb) + Number(!!word.adjective) <= 1) {
+    return NextResponse.json({ error: 'Invalid request body' }, { status: 400 });
+  }
+
+  const rawWord = Object.assign(
+    {},
+    {
+      name: word.name,
+      type: word.type as Type,
+      translation: word.translation,
+      info: word.info
+    }
+  );
+
+  const nounData =
+    rawWord.type === 'NOUN' && word.noun
+      ? Object.assign(
+          {},
+          {
+            pluralOnly: word.noun.pluralOnly,
+            declension: word.noun.declension as Declension,
+            genitive: word.noun.genitive,
+            gender: word.noun.gender as Gender
+          }
+        )
+      : null;
+
+  const verbData =
+    rawWord.type === 'VERB' && word.verb
+      ? Object.assign(
+          {},
+          {
+            conjugation: word.verb.conjugation as Conjugation,
+            present: word.verb.present,
+            perfect: word.verb.perfect,
+            participle: word.verb.participle
+          }
+        )
+      : null;
+
+  const adjectiveData =
+    rawWord.type === 'ADJECTIVE' && word.adjective
+      ? Object.assign(
+          {},
+          {
+            comparison: word.adjective.comparison as Comparison,
+            femininum: word.adjective.femininum,
+            neutrum: word.adjective.neutrum
+          }
+        )
+      : null;
+
+  const derivativeId = word.derivativeId;
+
+  try {
+    const newWord = await prisma.word.create({
+      data: {
+        ...rawWord,
+        createdBy: {
+          connect: {
+            id: user.id
+          }
+        },
+        ...(nounData && { noun: { create: nounData } }),
+        ...(verbData && { verb: { create: verbData } }),
+        ...(adjectiveData && { adjective: { create: adjectiveData } }),
+        ...(derivativeId && { derivative: { connect: { id: derivativeId } } })
+      }
+    });
+
+    return NextResponse.json(newWord);
+  } catch (error: any) {
+    console.error(error);
+    return NextResponse.json({ error: error.message }, { status: 500 });
   }
 };
