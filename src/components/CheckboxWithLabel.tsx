183dc650c0bbe3dc21ea80248230277328bdda7d
neinja007 <anton.siligan@gmail.com>
2024-10-02 09:33:54 +0200

    support ref

--- src/components/CheckboxWithLabel.tsx
+++ src/components/CheckboxWithLabel.tsx
@@ -1,6 +1,6 @@
 import { COLORS } from '@/constants/other';
 import { usePrimaryColor } from '@/hooks/database/queries/usePrimaryColor';
-import { Dispatch, SetStateAction, useId } from 'react';
+import { Dispatch, SetStateAction, useId, forwardRef } from 'react';
 
 type CheckboxWithLabelProps = {
   checked: boolean;
@@ -10,50 +10,48 @@ type CheckboxWithLabelProps = {
   noGeneratedId?: boolean;
 } & Omit<React.ComponentProps<'input'>, 'type'>;
 
-const CheckboxWithLabel = ({
-  checked,
-  disabled,
-  handleChange,
-  label,
-  noGeneratedId,
-  ...props
-}: CheckboxWithLabelProps) => {
-  const id = useId();
+const CheckboxWithLabel = forwardRef<HTMLInputElement, CheckboxWithLabelProps>(
+  ({ checked, disabled, handleChange, label, noGeneratedId, ...props }, ref) => {
+    const id = useId();
 
-  if ((!props.id && !noGeneratedId) || (props.id && noGeneratedId)) {
-    throw new Error('conflicting ids!');
+    if ((!props.id && !noGeneratedId) || (props.id && noGeneratedId)) {
+      throw new Error('conflicting ids!');
+    }
+
+    const dynamicId = noGeneratedId ? props.id : id;
+
+    if (handleChange && props.onChange) {
+      throw new Error('conflicting onChange handlers!');
+    }
+
+    const primaryColor = usePrimaryColor(true);
+
+    return (
+      <div>
+        <input
+          type='checkbox'
+          disabled={disabled}
+          style={{ accentColor: COLORS[primaryColor()].hex }}
+          id={dynamicId}
+          checked={checked}
+          onChange={handleChange ? (e) => handleChange(e.target.checked) : props.onChange}
+          className='mr-1'
+          ref={ref}
+          {...props}
+        />
+        <label
+          htmlFor={dynamicId}
+          className={
+            checked ? COLORS[primaryColor()].text : disabled ? 'text-gray-400 dark:text-gray-600' : 'text-gray-500'
+          }
+        >
+          {label}
+        </label>
+      </div>
+    );
   }
+);
 
-  const dynamicId = noGeneratedId ? props.id : id;
-
-  if (handleChange && props.onChange) {
-    throw new Error('conflicting onChange handlers!');
-  }
-
-  const primaryColor = usePrimaryColor(true);
-
-  return (
-    <div>
-      <input
-        type='checkbox'
-        disabled={disabled}
-        style={{ accentColor: COLORS[primaryColor()].hex }}
-        id={dynamicId}
-        checked={checked}
-        onChange={handleChange ? (e) => handleChange(e.target.checked) : props.onChange}
-        className='mr-1'
-        {...props}
-      />
-      <label
-        htmlFor={dynamicId}
-        className={
-          checked ? COLORS[primaryColor()].text : disabled ? 'text-gray-400 dark:text-gray-600' : 'text-gray-500'
-        }
-      >
-        {label}
-      </label>
-    </div>
-  );
-};
+CheckboxWithLabel.displayName = 'CheckboxWithLabel';
 
 export default CheckboxWithLabel;
