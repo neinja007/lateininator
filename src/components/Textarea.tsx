cc121cdaae102d60e587375ab5b41abab827d7ed
neinja007 <anton.siligan@gmail.com>
2024-10-02 08:38:00 +0200

    add more specific textarea props

--- src/components/Textarea.tsx
+++ src/components/Textarea.tsx
@@ -1,27 +1,46 @@
 import { clsx } from 'clsx';
 import ui from '@/styles/ui.module.css';
-import { useId } from 'react';
+import { useId, forwardRef } from 'react';
 
 type TextareaProps = {
   value: string;
-  setValue: (description: string) => void;
-};
+  handleChange: (description: string) => void;
+  label?: string;
+  className?: React.CSSProperties;
+  useDisabledStyle?: boolean;
+  noGeneratedId?: boolean;
+} & Omit<React.ComponentProps<'textarea'>, 'value' | 'className'>;
 
-const Textarea = ({ value, setValue }: TextareaProps) => {
-  const id = useId();
-  return (
-    <div className='mt-4'>
-      <label htmlFor={id} className='block'>
-        Beschreibung
-      </label>
-      <textarea
-        id={id}
-        className={clsx(ui.shape, 'mt-1 h-24 w-full')}
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-      />
-    </div>
-  );
-};
+const Textarea = forwardRef<HTMLTextAreaElement, TextareaProps>(
+  ({ value, handleChange, label, className, noGeneratedId, useDisabledStyle, ...props }, ref) => {
+    const id = useId();
+
+    if ((noGeneratedId && !props.id) || (!noGeneratedId && props.id)) {
+      throw new Error('conflicting ids!');
+    }
+
+    const dynamicId = noGeneratedId ? props.id : id;
+
+    return (
+      <div className='inline'>
+        {label && (
+          <label htmlFor={dynamicId} className='block'>
+            {label}
+          </label>
+        )}
+        <textarea
+          id={dynamicId}
+          className={clsx(ui.shape, 'mt-1 h-24 w-full', useDisabledStyle && 'disabled:opacity-50')}
+          value={value}
+          onChange={(e) => handleChange(e.target.value)}
+          ref={ref}
+          {...props}
+        />
+      </div>
+    );
+  }
+);
+
+Textarea.displayName = 'Textarea';
 
 export default Textarea;
