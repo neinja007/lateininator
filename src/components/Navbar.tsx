fce931fab4cfcff0f80911403a5f8f9026aa3105
neinja007 <anton.siligan@gmail.com>
2024-10-23 14:24:40 +0200

    make route children status dependent

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -64,7 +64,10 @@ const Navbar = () => {
                   {route.children
                     .map((child) => ({ ...child, href: route.href + child.href }))
                     .map((child, i) => {
-                      return <NavbarLink dropdown key={i} route={child} active={pathname.startsWith(child.href)} />;
+                      return makeStatusDependent(
+                        <NavbarLink dropdown key={i} route={child} active={pathname.startsWith(child.href)} />,
+                        child.status
+                      );
                     })}
                 </NavbarDropdown>
               );
