b1734cb3378eab05c364f58b671ddddd933d15f7
neinja007 <anton.siligan@gmail.com>
2024-10-28 05:58:12 +0100

    run clerk codemon

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-export default clerkMiddleware((auth, request) => {
-  if (isProtectedRoute(request)) auth().protect();
+export default clerkMiddleware(async (auth, request) => {
+  if (isProtectedRoute(request)) await auth.protect();
 });
 
 const isProtectedRoute = createRouteMatcher(['/user(.*)', '/premium/manage(.*)']);
