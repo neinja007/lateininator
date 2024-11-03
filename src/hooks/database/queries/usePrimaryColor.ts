b2673f8ed5db0ce7655a064f9e4533fc714062b4
neinja007 <anton.siligan@gmail.com>
2024-11-03 11:48:54 +0100

    check existing data

--- src/hooks/database/queries/usePrimaryColor.ts
+++ src/hooks/database/queries/usePrimaryColor.ts
@@ -1,15 +1,24 @@
 /* eslint-disable no-redeclare */
-import { PrimaryColor } from '@/types/other';
+import { AllSettingKey, PrimaryColor } from '@/types/other';
 import { useSettings } from './useSettings';
 import { availableColors } from '@/constants/other';
+import { useQueryClient } from '@tanstack/react-query';
 
 export function usePrimaryColor(randomFn: true): () => PrimaryColor;
 export function usePrimaryColor(randomFn?: false): PrimaryColor;
 export function usePrimaryColor(randomFn?: boolean): (() => PrimaryColor) | PrimaryColor;
+
 export function usePrimaryColor(randomFn?: boolean) {
   const { settings } = useSettings();
 
-  const primaryColor = (settings?.PRIMARY_COLOR as PrimaryColor | 'random') || 'blue';
+  const queryClient = useQueryClient();
+  const queryData: { settingKey: AllSettingKey; settingValue: string }[] | undefined = queryClient.getQueryData([
+    'user-settings'
+  ]);
+
+  const primaryColorData = queryData?.find((setting) => setting.settingKey === 'PRIMARY_COLOR')?.settingValue;
+
+  const primaryColor = primaryColorData ?? (settings?.PRIMARY_COLOR as PrimaryColor | 'random') ?? 'blue';
 
   if (randomFn) {
     if (primaryColor === 'random') {
