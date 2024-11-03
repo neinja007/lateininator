769ade9aa7179c747d184a518bd20687315915b0
neinja007 <anton.siligan@gmail.com>
2024-11-03 12:38:22 +0100

    only generate query client once (HUGE FIX)

--- src/components/QueryClientWrapper.tsx
+++ src/components/QueryClientWrapper.tsx
@@ -2,15 +2,19 @@
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { useState } from 'react';
 
 export const QueryClientWrapper = ({ children }: { children: React.ReactNode }) => {
-  const queryClient = new QueryClient({
-    defaultOptions: {
-      queries: {
-        staleTime: 5 * 60 * 1000
-      }
-    }
-  });
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 5 * 60 * 1000
+          }
+        }
+      })
+  );
 
   queryClient.setQueryDefaults(['dbUser'], {
     staleTime: 0
