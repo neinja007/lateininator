6622fb686bb81b82f3becb34a906becfbf5c5e5a
neinja007 <anton.siligan@gmail.com>
2024-11-16 20:06:32 +0100

    add buttons

--- src/app/(general)/user/words/components/ExceptionEditor.tsx
+++ src/app/(general)/user/words/components/ExceptionEditor.tsx
@@ -4,18 +4,20 @@ import { WORD_CONSTANTS } from '@/constants/wordConstants';
 import { MainWordType, WordProperty, WordType } from '@/types/appConstants';
 import { MAPPER } from '@/utils/other/mapper';
 import clsx from 'clsx';
-import { Plus } from 'lucide-react';
+import { Check, Plus, X } from 'lucide-react';
 import Input from '@/components/Input';
 import table from '@/styles/table.module.css';
 import { getValueArrayFromObject } from '../utils/getValueArrayFromObject';
+import { useState } from 'react';
 
 type ExceptionEditorProps = {
   type: WordType | undefined;
   exception?: Object;
+  setExceptions?: (value: any) => void;
 };
 
 const ExceptionEditor = ({ type, exception }: ExceptionEditorProps) => {
-  const exceptions = getValueArrayFromObject(exception ?? {});
+  const [exceptions, setExceptions] = useState(getValueArrayFromObject(exception ?? {}));
 
   return (
     type !== 'OTHER' && (
@@ -30,7 +32,10 @@ const ExceptionEditor = ({ type, exception }: ExceptionEditorProps) => {
                     {MAPPER.extended.property.singular[property as WordProperty]}
                   </th>
                 ))}
-                <th>Ausnahme</th>
+                <th className={table.th}>Ausnahme</th>
+                <th className={clsx(table.th, 'w-12')}>
+                  <X />
+                </th>
               </tr>
             </thead>
             <tbody>
@@ -53,6 +58,13 @@ const ExceptionEditor = ({ type, exception }: ExceptionEditorProps) => {
                       )}
                     </td>
                   ))}
+                  <td className={table.td}>
+                    <div className='flex items-center'>
+                      <button className='rounded-lg border border-red-700 transition-colors hover:bg-red-500'>
+                        <X />
+                      </button>
+                    </div>
+                  </td>
                 </tr>
               ))}
               <tr className={table.tr}>
@@ -62,6 +74,32 @@ const ExceptionEditor = ({ type, exception }: ExceptionEditorProps) => {
                 >
                   <div className='flex justify-center gap-x-2'>
                     <Plus /> Neue Ausnahme hinzufügen
+                  </div>
+                </td>
+              </tr>
+              <tr className={table.tr}>
+                {[...Array(exceptionStructure[type as MainWordType].length + 1)].map((value, index) => (
+                  <td className='border p-0 dark:border-gray-500' key={index}>
+                    {index == exceptionStructure[type as MainWordType].length ? (
+                      <Input type='text' className={clsx(table.input, 'w-full')} value={value} />
+                    ) : (
+                      <Select
+                        className={clsx(table.select, 'm-0 h-8 w-full !border-none px-1 focus:outline-none')}
+                        value={value}
+                        options={
+                          WORD_CONSTANTS[
+                            exceptionStructure[type as MainWordType][index] as keyof typeof WORD_CONSTANTS
+                          ] as string[]
+                        }
+                      />
+                    )}
+                  </td>
+                ))}
+                <td className={table.td}>
+                  <div className='flex items-center'>
+                    <button className='rounded-lg border border-green-700 transition-colors hover:bg-green-500'>
+                      <Check />
+                    </button>
                   </div>
                 </td>
               </tr>
