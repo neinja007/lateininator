b16dcd58c1835de75bf54a02d1d295726cfc4e9a
neinja007 <anton.siligan@gmail.com>
2024-11-18 19:40:53 +0100

    remove comment

--- src/app/(general)/vocabulary/dictionary/page.tsx
+++ src/app/(general)/vocabulary/dictionary/page.tsx
@@ -35,7 +35,6 @@ const Page = () => {
     <div className='space-y-5'>
       <Heading heading='Wörterbuch'>
         Das ist das Lateininator Wörterbuch. Hier kannst du alle Wörter sehen, die es auf dem Lateininator gibt.
-        {/* Wenn es ein Wort noch nicht gibt, kannst du es unter <Link href='/user/words'>Wörter verwalten</Link> hinzufügen. */}
       </Heading>
       <SearchBar query={query} setQuery={setQuery} />
       {status === 'error' ? (
