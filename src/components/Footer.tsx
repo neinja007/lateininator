b1ac73dc64999b723ddb3f1ec804182960c3c497
neinja007 <anton.siligan@gmail.com>
2025-05-04 07:10:57 +0200

    update copyright notice and add link to dev homepage

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,12 @@
+import Link from './Link';
 import LinkToSupportEmail from './LinkToSupportEmail';
 
 const Footer = () => {
   return (
     <div className='justify-between text-center md:flex'>
-      <span>&copy; 2024 Lateininator</span>
+      <span>
+        &copy; 2025 Lateininator (von <Link href='https://neinja.dev'>neinja.dev</Link>)
+      </span>
       <div className='mt-5 md:mt-0'>
         Fehler, Vorschläge, und Feedback: <LinkToSupportEmail>support@lateininator.com</LinkToSupportEmail>
       </div>
