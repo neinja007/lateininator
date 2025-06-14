929f459bb339055e1e08d838c7cebd06083812ad
Anton Siligan <anton.siligan@neinja.dev>
2025-06-14 11:05:05 +0200

    make big badges wider

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -6,7 +6,13 @@ type BadgeProps = { color?: Color; text: string; big?: boolean };
 
 const Badge = ({ color = 'default', text, big }: BadgeProps) => {
   return (
-    <div className={clsx('inline-flex items-center justify-center rounded-full p-1 px-2', COLORS[color].static)}>
+    <div
+      className={clsx(
+        'inline-flex items-center justify-center rounded-full py-1',
+        big ? 'px-4' : 'px-2',
+        COLORS[color].static
+      )}
+    >
       <span className={clsx('font-medium', big ? 'text-xl' : 'text-xs uppercase leading-none')}>{text}</span>
     </div>
   );
