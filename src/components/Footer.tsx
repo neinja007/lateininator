effad522aa1cad4f942535be844c213efe28b946
Anton Siligan <anton.siligan@neinja.dev>
2026-01-30 16:19:29 +0100

    include legal page links in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,12 +3,19 @@ import LinkToSupportEmail from './LinkToSupportEmail';
 
 const Footer = () => {
   return (
-    <div className='justify-between text-center md:flex'>
-      <span>
-        &copy; {new Date().getFullYear()} Lateininator (von <Link href='https://neinja.dev'>neinja.dev</Link>)
-      </span>
-      <div className='mt-5 md:mt-0'>
-        Fehler, Vorschläge, und Feedback: <LinkToSupportEmail>support@lateininator.com</LinkToSupportEmail>
+    <div className='flex flex-col'>
+      <div className='justify-between text-center md:flex'>
+        <span>
+          &copy; {new Date().getFullYear()} Lateininator (von <Link href='https://neinja.dev'>neinja.dev</Link>)
+        </span>
+        <div className='mt-5 md:mt-0'>
+          Fehler, Vorschläge, und Feedback: <LinkToSupportEmail>support@lateininator.com</LinkToSupportEmail>
+        </div>
+      </div>
+      <div className='flex flex-row justify-center gap-4'>
+        <Link href='/impressum'>Impressum</Link>
+        <Link href='/privacy-policy'>Datenschutzerklärung</Link>
+        <Link href='/terms-of-service'>Nutzungsbedingungen</Link>
       </div>
     </div>
   );
