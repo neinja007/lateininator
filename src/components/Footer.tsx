073633c7732000b92309ba21c92bacdeed9170dc
Anton Siligan <anton.siligan@neinja.dev>
2026-01-30 16:17:58 +0100

    dynamic date

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <div className='justify-between text-center md:flex'>
       <span>
-        &copy; 2025 Lateininator (von <Link href='https://neinja.dev'>neinja.dev</Link>)
+        &copy; {new Date().getFullYear()} Lateininator (von <Link href='https://neinja.dev'>neinja.dev</Link>)
       </span>
       <div className='mt-5 md:mt-0'>
         Fehler, Vorschläge, und Feedback: <LinkToSupportEmail>support@lateininator.com</LinkToSupportEmail>
